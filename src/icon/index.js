6a80a945ba6f3e86dadacb850b2a4e89117d6513
Shannon Moeller <icebox@uber.com>
2018-05-28 11:55:07 -0600

    feat: create icon component

---
+++ src/icon/index.js
@@ -0,0 +1,6 @@
+// @flow
+
+export {default as CloudUploadIcon} from './cloud-upload-icon';
+export {default as MagnifyingGlassIcon} from './magnifying-glass-icon';
+export {default as RightArrowIcon} from './right-arrow-icon';
+export {default as XIcon} from './x-icon';
