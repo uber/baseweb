fe0ca7fe25a3a91991471f47a07c6176e1585bae
Todd Levin <todd.levin@uber.com>
2018-05-29 09:55:35 -0700

    test: add tests for styled and themeprovider

---
+++ src/utils/test-utils.js
@@ -0,0 +1,24 @@
+// @flow
+import React from 'react';
+import {Provider as StyletronProvider} from 'styletron-react';
+import {Client as Styletron} from 'styletron-engine-atomic';
+import {ThemeProvider} from '../styles';
+import DEFAULT_THEME from '../themes/light-theme';
+
+const engine = new Styletron();
+
+export const withStyletronProvider = (Component: any) => props => {
+  return (
+    <StyletronProvider value={engine}>
+      <Component {...props} />
+    </StyletronProvider>
+  );
+};
+
+export const withThemeProvider = (Component: any) => props => {
+  return (
+    <ThemeProvider theme={DEFAULT_THEME}>
+      <Component {...props} />
+    </ThemeProvider>
+  );
+};
