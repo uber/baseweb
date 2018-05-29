178f3f4ea35e8801021570ae03b5cac280b7fa26
Todd Levin <todd.levin@uber.com>
2018-05-29 09:55:35 -0700

    test: add tests for styled and themeprovider

---
+++ src/styles/styled.test.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+import React from 'react';
+import Enzyme, {mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import styled from './styled';
+import {withStyletronProvider, withThemeProvider} from '../utils/test-utils';
+import {LightTheme} from '../themes';
+
+Enzyme.configure({adapter: new Adapter()});
+
+test('styled', () => {
+  const StyledMockButton = styled('button', ({theme}) => ({
+    backgroundColor: theme.colors.primary400,
+  }));
+
+  const TestComponent = withStyletronProvider(
+    withThemeProvider(() => <StyledMockButton id="testButton" />)
+  );
+  const wrapper = mount(<TestComponent />);
+  const button = wrapper
+    .find('#testButton')
+    .children()
+    .at(0);
+  expect(button.props().theme).toBe(LightTheme);
+});
