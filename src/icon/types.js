7d238fdf92dbd9089d68f8a2abb9827a75567df9
Shannon Moeller <icebox@uber.com>
2018-05-29 17:01:59 -0600

    feat: move icon props to types file

--- src/icon/types.js
+++ src/icon/types.js
@@ -1,6 +1,12 @@
 // @flow
 
+import type {ThemeT} from '../styles/types';
+
 export type IconProps = {
   alt?: ?string,
   children?: any,
+};
+
+export type StyledIconProps = IconProps & {
+  theme: ThemeT,
 };
