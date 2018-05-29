b08042a1d88f97d78d2032ce29643aa5207da1be
Todd Levin <todd.levin@uber.com>
2018-05-29 10:57:05 -0700

    fix: fallback to createReactContext

--- src/styles/theme-provider.js
+++ src/styles/theme-provider.js
@@ -1,10 +1,11 @@
 // @flow
 import * as React from 'react';
+import createReactContext from 'create-react-context';
 import LIGHT_THEME from '../themes/light-theme';
 
 import type {ThemeT} from './types';
 
-export const ThemeContext = React.createContext(LIGHT_THEME);
+export const ThemeContext = createReactContext(LIGHT_THEME);
 
 const ThemeProvider = (props: {theme: ThemeT, children: ?React.Node}) => {
   return (
