bf591252dc9c91c5d53e38f00e9f444402ad8320
Todd Levin <todd.levin@uber.com>
2018-05-29 17:29:37 -0700

    chore: add animation variables

--- src/themes/light-theme.js
+++ src/themes/light-theme.js
@@ -203,6 +203,11 @@ export default {
     overlay600: 'inset 0 0 0 100rem hsla(0, 0%, 0%, 0.24)',
   },
   animation: {
-    timing400: '0.2s',
+    timing100: '0.25s',
+    timing400: '0.4s',
+    timing700: '0.6s',
+    easeOutCurve: 'cubic-bezier(.2, .8, .4, 1)',
+    easeInCurve: 'cubic-bezier(.8, .2, .6, 1)',
+    easeInOutCurve: 'cubic-bezier(0.4, 0, 0.2, 1)',
   },
 };
