b571d535b5a2b6e921852ab753d060f53b97d8e6
Nadiia <nadiia.dmytrenko@gmail.com>
2018-06-19 10:42:13 -0700

    Add missing theme values, withProps helper, change the theme prop to $theme (#27)

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,10 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': 0,
     'import/no-extraneous-dependencies': 0,
+    'prettier/prettier': ['error', {
+      singleQuote: true,
+      trailingComma: 'all',
+      bracketSpacing: false
+    }]
   },
 };
