ed50968d6539d5c2a7b2f679fac151012ef2e513
Oleg Gomozov <gomozov@uber.com>
2018-06-25 15:33:27 -0700

    feat: add info plugin

--- src/button/index.js
+++ src/button/index.js
@@ -1,15 +1,15 @@
 /* @flow */
 
-import React from 'react';
+import * as React from 'react';
 /* eslint react/require-default-props: ["off"]*/
 
 type Props = {
   onClick: () => void,
-  children?: any,
+  children?: React.Node,
 };
 
-const Button = ({onClick, children = undefined}: Props) => (
-  <button onClick={onClick}>{children}</button>
+const Button = ({onClick, children}: Props) => (
+  <button onClick={onClick}>{children || 'it is a button!'}</button>
 );
 
 Button.displayName = 'Button';
