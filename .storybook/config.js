6c3d09dd62eb9a194f18dc07064c3b2294b08cda
Oleg Gomozov <blackswanny@gmail.com>
2018-06-26 09:56:46 -0700

    Show info and source code with live changing props (#33)
    * Show info and source code with live changing props
    
    * feat: add info plugin

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import {configure, addDecorator} from '@storybook/react';
+import {configure, addDecorator, setAddon} from '@storybook/react';
 import {Provider as StyletronProvider} from 'styletron-react';
 import {Client as Styletron} from 'styletron-engine-atomic';
 import {ThemeProvider} from '../src/styles';
 import DEFAULT_THEME from '../src/themes/light-theme';
+import {withInfo} from '@storybook/addon-info';
+import {withKnobs, text, boolean, number} from '@storybook/addon-knobs';
 
 const engine = new Styletron();
 
@@ -12,6 +14,13 @@ const req = require.context('../src', true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+// this should be first decorator to avoid extra code to be parsed here
+addDecorator(story => {
+  return withInfo()(story)({});
+});
+
+addDecorator(withKnobs);
 
 // Add providers for theme and styletron
 addDecorator(story => {
