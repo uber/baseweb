5a919429ee3bdace18943c3577681c332bacdd9b
Oleg Gomozov <blackswanny@gmail.com>
2018-06-26 09:56:46 -0700

    Show info and source code with live changing props (#33)
    * Show info and source code with live changing props
    
    * feat: add info plugin

--- src/button/index.js
+++ src/button/index.js
@@ -1,11 +1,17 @@
 /* @flow */
 
-import React from 'react';
+import * as React from 'react';
+/* eslint react/require-default-props: ["off"]*/
 
 type Props = {
   onClick: () => void,
+  children?: React.Node,
 };
 
-export default ({onClick}: Props) => (
-  <button onClick={onClick}>it is a button!</button>
+const Button = ({onClick, children}: Props) => (
+  <button onClick={onClick}>{children || 'it is a button!'}</button>
 );
+
+Button.displayName = 'Button';
+
+export default Button;
--- src/button/stories.js
+++ src/button/stories.js
@@ -3,9 +3,10 @@ import React from 'react';
 
 import {storiesOf} from '@storybook/react';
 import {action} from '@storybook/addon-actions';
+import {text} from '@storybook/addon-knobs';
 
 import Button from './index';
 
 storiesOf('Button', module).add('demo button', () => (
-  <Button onClick={action('clicked')}>Hello Button</Button>
+  <Button onClick={action('clicked')}>{text('Label', 'Hello Button')}</Button>
 ));
