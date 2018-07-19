4e989ba33e7b97c45a2dc6e3f8ccb27a3b8aa9b1
DianaSuvorova <diana.suvorova@gmail.com>
2018-07-19 09:19:13 -0700

    test: e2e set up with rollup for popover (#61)

---
+++ src/popover/e2e.js
@@ -0,0 +1,18 @@
+// @flow
+import e2e from '../utils/e2e-test-utils';
+
+const {run, By, runBrowserAccecibilityTest, location} = e2e;
+
+run((driver, browser) => {
+  describe('Popover Test Suite', function() {
+    beforeEach(function() {
+      runBrowserAccecibilityTest(driver, 'label');
+    });
+
+    it('Checked state', async function() {
+      await driver.get(location);
+      const popover = await driver.findElement(By.id('stateless%20popover'));
+      popover.click();
+    });
+  });
+});
