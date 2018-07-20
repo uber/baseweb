4e6b733948b7bad3048a83b142e436f9dfd4d83f
DianaSuvorova <diana.suvorova@gmail.com>
2018-07-20 09:12:03 -0700

    chore: exporting components (#72)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 // @flow
+import * as Checkbox from './checkbox';
 import * as TextInput from './input';
+import * as Popover from './popover';
 
 export {styled, ThemeProvider} from './styles';
 export {LightTheme} from './themes';
 export {withProps} from './helpers';
+export {Checkbox};
 export {TextInput};
+export {Popover};
