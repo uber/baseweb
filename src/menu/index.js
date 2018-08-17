954acc9b18a24816bfb4a9d98cbee4f30b127c4f
Anh Mai <abmai91@gmail.com>
2018-08-17 12:27:24 -0700

    chore: add exports

--- src/menu/index.js
+++ src/menu/index.js
@@ -6,12 +6,24 @@ LICENSE file in the root directory of this source tree.
 */
 // @flow
 export {default as StatefulMenu} from './stateful-menu';
+export {default as StatefulMenuProfile} from './stateful-menu-profile';
 export {default as StatefulContainer} from './stateful-container';
 export {default as Menu} from './menu';
+export {default as MenuProfile} from './menu-profile';
 // Constants
 export {KEY_STRINGS, STATE_CHANGE_TYPES} from './constants';
 // Styled elements
 export {
   List as StyledList,
   ListItem as StyledListItem,
+  ListProfile as StyledListProfile,
+  ListItemProfile as StyledListItemProfile,
+  ProfileImgContainer as StyledProfileImgContainer,
+  ProfileImg as StyledProfileImg,
+  ProfileLabelsContainer as StyledProfileLabelsContainer,
+  ProfileTitle as StyledProfileTitle,
+  ProfileSubtitle as StyledProfileSubtitle,
+  ProfileBody as StyledProfileBody,
 } from './styled-components';
+// Flow
+export * from './types';
