71ef54aa491ed1fcd8bd259876fc1c10a7bbfeef
Gergely Nemeth <gergelyke@users.noreply.github.com>
2018-08-30 13:58:30 -0700

    docs(storybook): move readme to the first tab

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,5 +1,5 @@
+import 'storybook-readme/register';
 import '@storybook/addon-options/register';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-knobs/register';
-import 'storybook-readme/register';
