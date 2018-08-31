7dd8747e3bb638bdae089d458a208146ba0fb069
KahWee Teng <262105+kahwee@users.noreply.github.com>
2018-08-31 13:33:30 -0700

    docs(rfc): buttons

--- src/menu/index.js
+++ src/menu/index.js
@@ -1,3 +1,9 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
 // @flow
 export {default as StatefulMenu} from './stateful-menu';
 export {default as StatefulContainer} from './stateful-container';
