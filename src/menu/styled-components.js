38e0582d79079db836b0506914d65eed27ccf2ae
Anh Mai <abmai91@gmail.com>
2018-09-02 23:44:47 -0700

    fix: rebase master; reconcile with select

--- src/menu/styled-components.js
+++ src/menu/styled-components.js
@@ -17,41 +17,43 @@ type StyledListItemPropsT = {
   $isHighlighted: boolean,
 } & StyledPropsT;
 
-export const List = styled('ul', ({$theme}: StyledPropsT) => ({
+export const getListStyles = ({$theme}: StyledPropsT) => ({
   position: 'relative',
   margin: 0,
   padding: 0,
   background: $theme.colors.white,
   borderRadius: $theme.borders.radius300,
   boxShadow: $theme.lighting.shadow600,
-}));
+});
+export const List = styled('ul', getListStyles);
 
-export const ListItem = styled(
-  'li',
-  ({$theme, $isHighlighted}: StyledListItemPropsT) => ({
-    ...$theme.typography.font300,
-    position: 'relative',
-    display: 'block',
-    color: $isHighlighted ? $theme.colors.primary : $theme.colors.black,
-    cursor: 'pointer',
-    transitionProperty: 'color',
-    transitionDuration: $theme.animation.timing100,
-    transitionTimingFunction: $theme.animation.easeOutCurve,
-    ':hover': {
-      color: $theme.colors.primary400,
-    },
-    paddingTop: $theme.sizing.scale300,
-    paddingBottom: $theme.sizing.scale300,
-    paddingRight: $theme.sizing.scale600,
-    paddingLeft: $theme.sizing.scale600,
-    ':first-child': {
-      paddingTop: $theme.sizing.scale600,
-    },
-    ':last-child': {
-      paddingBottom: $theme.sizing.scale600,
-    },
-  }),
-);
+export const getListItemStyles = ({
+  $theme,
+  $isHighlighted,
+}: StyledListItemPropsT) => ({
+  ...$theme.typography.font300,
+  position: 'relative',
+  display: 'block',
+  color: $isHighlighted ? $theme.colors.primary : $theme.colors.black,
+  cursor: 'pointer',
+  transitionProperty: 'color',
+  transitionDuration: $theme.animation.timing100,
+  transitionTimingFunction: $theme.animation.easeOutCurve,
+  ':hover': {
+    color: $theme.colors.primary400,
+  },
+  paddingTop: $theme.sizing.scale300,
+  paddingBottom: $theme.sizing.scale300,
+  paddingRight: $theme.sizing.scale600,
+  paddingLeft: $theme.sizing.scale600,
+  ':first-child': {
+    paddingTop: $theme.sizing.scale600,
+  },
+  ':last-child': {
+    paddingBottom: $theme.sizing.scale600,
+  },
+});
+export const ListItem = styled('li', getListItemStyles);
 
 export const ListItemProfile = styled('li', ({$theme}: StyledPropsT) => ({
   position: 'relative',
@@ -70,6 +72,41 @@ export const ListItemProfile = styled('li', ({$theme}: StyledPropsT) => ({
   },
 }));
 
-export const List = styled('ul', getListStyles);
+export const ProfileImgContainer = styled('div', {
+  width: '60px',
+  height: '60px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
 
-export const ListItem = styled('li', getListItemStyles);
+export const ProfileImg = styled('img', {
+  width: '100%',
+  height: '100%',
+  borderRadius: '50%',
+});
+
+export const ProfileLabelsContainer = styled('div', ({$theme}) => ({
+  marginLeft: $theme.sizing.scale600,
+  alignSelf: 'flex-start',
+  display: 'flex',
+  flexDirection: 'column',
+}));
+
+export const ProfileTitle = styled('h6', ({$theme}) => ({
+  ...$theme.typography.font450,
+  color: $theme.colors.black,
+  margin: 0,
+}));
+
+export const ProfileSubtitle = styled('p', ({$theme}) => ({
+  ...$theme.typography.font300,
+  color: $theme.colors.black,
+  margin: 0,
+}));
+
+export const ProfileBody = styled('p', ({$theme}) => ({
+  ...$theme.typography.font200,
+  margin: 0,
+  color: $theme.colors.mono800,
+}));
