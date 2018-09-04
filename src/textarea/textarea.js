f8507513fd468d33e3fcf6067b5ecef7aeb0dbda
Gergely Nemeth <gergelyke@users.noreply.github.com>
2018-09-04 10:59:33 -0700

    chore(license): change to short MIT license in source files

--- src/textarea/textarea.js
+++ src/textarea/textarea.js
@@ -1,3 +1,9 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
 // @flow
 import * as React from 'react';
 import type {TextareaPropsT} from './types';
