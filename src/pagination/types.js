e86dd356d732345282790b93888ad5cc1e8255f0
Anh Mai <abmai91@gmail.com>
2018-09-10 01:23:49 -0700

    feat(pagination): initial component implementation (part 1)

---
+++ src/pagination/types.js
@@ -0,0 +1,62 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+import type {OverrideT} from '../helpers/overrides';
+import {STATE_CHANGE_TYPE} from './constants';
+
+export type StateReducerFnT = (
+  changeType: $Keys<typeof STATE_CHANGE_TYPE>,
+  changes: StatefulContainerStateT,
+  currentState: StatefulContainerStateT,
+) => StatefulContainerStateT;
+
+export type OverridesT = {
+  Root?: OverrideT<*>,
+  PrevButton?: OverrideT<*>,
+  NextButton?: OverrideT<*>,
+  MaxLabel?: OverrideT<*>,
+  DropdownContainer?: OverrideT<*>,
+  DropdownButton?: OverrideT<*>,
+  DropdownMenu?: OverrideT<*>,
+};
+
+export type PaginationPropsT = {
+  numPages: number,
+  currentPage: number,
+  prepositionLabel?: string,
+  overrides?: OverridesT,
+  onPrevClick?: (event: *) => *,
+  onNextClick?: (event: *) => *,
+  onPageChange?: (nextPage: number, prevPage: number) => *,
+};
+
+export type PaginationStateT = {
+  isMenuOpen: boolean,
+};
+
+export type StatefulPaginationPropsT = {
+  numPages: number,
+  stateReducer?: StateReducerFnT,
+  initialState?: StatefulContainerStateT,
+  onPageChange?: (nextPage: number, prevPage: number) => *,
+  overrides?: OverridesT,
+  prepositionLabel?: string,
+  onPrevClick?: (event: *) => *,
+  onNextClick?: (event: *) => *,
+};
+
+export type StatefulContainerPropsT = {
+  children: *,
+  numPages: number,
+  stateReducer?: StateReducerFnT,
+  initialState?: StatefulContainerStateT,
+  onPageChange?: (nextPage: number, prevPage: number) => *,
+};
+
+export type StatefulContainerStateT = {
+  currentPage: number,
+};
