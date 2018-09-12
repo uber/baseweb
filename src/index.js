c5a2b3b4db6c2f5b834d1f620ff0bac69d49b902
Nadiia <nadiia.dmytrenko@gmail.com>
2018-09-11 17:17:58 -0700

    Test probot

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 // @flow
+
 export {styled, ThemeProvider} from './styles';
 export {LightTheme} from './themes';
 export {withProps} from './helpers';
