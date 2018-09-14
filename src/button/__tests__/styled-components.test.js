242c78f63154e5113474145b4b44c789f05d105d
Anh Mai <abmai91@gmail.com>
2018-09-13 20:25:11 -0700

    feat(button): add loading style; refactor tests

--- src/button/__tests__/styled-components.test.js
+++ src/button/__tests__/styled-components.test.js
@@ -1,7 +1,20 @@
-/* eslint-disable flowtype/require-valid-file-annotation, react/prop-types */
-import React from 'react';
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+import * as React from 'react';
 import {shallow} from 'enzyme';
-import {BaseButton, StartEnhancer, EndEnhancer} from '../styled-components';
+import {LightTheme} from '../../themes';
+import {
+  BaseButton,
+  StartEnhancer,
+  EndEnhancer,
+  getStyleForKind,
+  getStyleForShape,
+} from '../styled-components';
 import {KIND, SIZE, SHAPE} from '../constants';
 
 function makeTest({
@@ -10,8 +23,16 @@ function makeTest({
   props = {},
   children = null,
   snapshotName = 'correct styles',
+}: {
+  title?: string,
+  component?: React.ComponentType<*>,
+  props?: {},
+  children?: *,
+  snapshotName?: string,
 }) {
+  // $FlowFixMe
   test(title, () => {
+    // $FlowFixMe
     const shallowed = shallow(<Component {...props}>{children}</Component>);
     expect(shallowed.instance().getStyles()).toMatchSnapshot(snapshotName);
   });
@@ -20,21 +41,37 @@ function makeTest({
 const allKinds = Object.values(KIND);
 const allSizes = Object.values(SIZE);
 const allShapes = Object.values(SHAPE);
+const allLoadingStates = [false, true];
 
 describe('Button Styled Components', () => {
-  allKinds.forEach(kind => {
-    allSizes.forEach(size => {
-      allShapes.forEach(shape => {
-        makeTest({
-          title: `BaseButton - ${kind} ${size} ${shape}`,
-          component: BaseButton,
-          props: {
-            $kind: kind,
-            $shape: shape,
-            $size: size,
-          },
-        });
+  allKinds.forEach($kind => {
+    allLoadingStates.forEach($isLoading => {
+      // $FlowFixMe
+      test(`getStyleForKind ${$kind} and loading ${$isLoading}`, () => {
+        expect(
+          // $FlowFixMe
+          getStyleForKind({$theme: LightTheme, $kind, $isLoading}),
+        ).toMatchSnapshot();
       });
+    });
+  });
+
+  allShapes.forEach($shape => {
+    allSizes.forEach($size => {
+      // $FlowFixMe
+      test(`getStyleForShape ${$shape} ${$size}`, () => {
+        expect(
+          // $FlowFixMe
+          getStyleForShape({$theme: LightTheme, $shape, $size}),
+        ).toMatchSnapshot();
+      });
+    });
+  });
+
+  allShapes.forEach(shape => {
+    makeTest({
+      title: `BaseButton - basic render`,
+      component: BaseButton,
     });
   });
 
--- src/button/types.js
+++ src/button/types.js
@@ -15,12 +15,13 @@ export type OverridesT = {
 };
 
 export type ButtonPropsT = {
+  overrides: OverridesT,
+  size: $Keys<typeof SIZE>,
+  kind: $Keys<typeof KIND>,
+  shape: $Keys<typeof SHAPE>,
+  isLoading: boolean,
+  disabled: boolean,
   startEnhancer?: () => React$Node | React$Node,
   endEnhancer?: () => React$Node | React$Node,
-  disabled?: boolean,
-  overrides?: OverridesT,
-  size?: $Keys<typeof SIZE>,
-  kind?: $Keys<typeof KIND>,
-  shape?: $Keys<typeof SHAPE>,
   children?: React$Node,
 };
