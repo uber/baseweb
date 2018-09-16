86c35ee55f88546faba6b3823c9a009e6625cff3
Anh Mai <abmai91@gmail.com>
2018-09-16 15:43:57 -0700

    chore: export types

--- src/pagination/index.js
+++ src/pagination/index.js
@@ -5,7 +5,6 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 // @flow
-
 export {default as Pagination} from './pagination';
 export {default as StatefulPagination} from './stateful-pagination';
 export {default as StatefulContainer} from './stateful-container';
@@ -18,3 +17,5 @@ export {
 } from './styled-components';
 // Constants
 export {STATE_CHANGE_TYPE} from './constants';
+// Flow
+export * from './types';
