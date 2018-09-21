b8b2dba34c5472339de539748483727e8e442e79
Nadiia <nadiia.dmytrenko@gmail.com>
2018-09-21 13:32:41 -0700

    chore: add a component template (#295)

---
+++ src/template-component/__tests__/component.test.js
@@ -0,0 +1,44 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+
+import React from 'react';
+import {mount} from 'enzyme';
+import {Component, StyledRoot} from '../index';
+
+describe('Component', () => {
+  test('basic click functionality', () => {
+    let renderedRoot;
+    const onClick = jest.fn();
+    const wrapper = mount(<Component onClick={onClick} />);
+
+    renderedRoot = wrapper.find(StyledRoot).first();
+    expect(renderedRoot).toExist();
+
+    // pass new prop value set to false
+    wrapper.setProps({prop: false});
+
+    renderedRoot = wrapper.find(StyledRoot).first();
+    expect(renderedRoot.props().$prop).toBe(false);
+  });
+
+  test('component overrides', () => {
+    const overrides = {
+      Root: jest
+        .fn()
+        .mockImplementation(({children}) => <span>{children}</span>),
+    };
+
+    const wrapper = mount(
+      // $FlowFixMe
+      <Component overrides={overrides} />,
+    );
+
+    const root = wrapper.find(overrides.Root);
+    expect(root).toHaveLength(1);
+  });
+});
---
+++ src/template-component/index.js
@@ -0,0 +1,16 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+export {default as StatefulComponent} from './stateful-component';
+export {default as StatefulContainer} from './stateful-container';
+export {default as Component} from './component';
+// Constants
+export {STATE_CHANGE_TYPE} from './constants';
+// Styled elements
+export {Root as StyledRoot} from './styled-components';
+// Flow
+export * from './types';
