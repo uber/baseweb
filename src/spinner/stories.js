4c9930a7fd506c21c2dd42f81d8f27bf8c2c38b0
Nadiia <nadiia.dmytrenko@gmail.com>
2018-09-25 10:48:33 -0700

    feat(spinner): spinner component implementation (#304)

---
+++ src/spinner/stories.js
@@ -0,0 +1,21 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+/* global module */
+import {storiesOf} from '@storybook/react';
+import {withReadme} from 'storybook-readme';
+import examples from './examples';
+
+//$FlowFixMe
+import SpinnerReadme from '../../rfcs/spinner-component.md';
+
+Object.entries(examples).forEach(([description, example]) =>
+  storiesOf('Spinner', module)
+    .addDecorator(withReadme(SpinnerReadme))
+    // $FlowFixMe
+    .add(description, example),
+);
