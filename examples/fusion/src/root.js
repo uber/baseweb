61e16ab601e16a65614ec5651384ed750ab623f6
Nadiia <nadiia.dmytrenko@gmail.com>
2018-09-28 15:34:22 -0700

    add ThemeProvider at root

--- examples/fusion/src/root.js
+++ examples/fusion/src/root.js
@@ -1,15 +1,18 @@
 // @flow
 import React from 'react';
 import {Route, Switch} from 'fusion-plugin-react-router';
+import {LightTheme, ThemeProvider} from 'baseui';
 
 import Home from './pages/home.js';
 import PageNotFound from './pages/pageNotFound.js';
 
 const root = (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route component={PageNotFound} />
-  </Switch>
+  <ThemeProvider theme={LightTheme}>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route component={PageNotFound} />
+    </Switch>
+  </ThemeProvider>
 );
 
 export default root;
