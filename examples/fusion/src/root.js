7f6b7171ebbb8d86323f77d5947239e38f54658e
Nadiia <nadiia.dmytrenko@gmail.com>
2018-09-28 15:38:10 -0700

    add a comment

--- examples/fusion/src/root.js
+++ examples/fusion/src/root.js
@@ -6,6 +6,7 @@ import {LightTheme, ThemeProvider} from 'baseui';
 import Home from './pages/home.js';
 import PageNotFound from './pages/pageNotFound.js';
 
+// if you want to use a custom theme pass it in to the ThemeProvider
 const root = (
   <ThemeProvider theme={LightTheme}>
     <Switch>
