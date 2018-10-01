a5d8aa9e92591a07a09a1791582cc0a19de573d2
Gergely Nemeth <gergelyke@users.noreply.github.com>
2018-10-01 16:04:14 -0700

    refactor(cards): follow the pattern other components follow for examples (#328)

---
+++ src/card/examples-list.js
@@ -0,0 +1,18 @@
+/* eslint-env node */
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+module.exports = {
+  TEXT_ONLY: 'Text only',
+  TEXT_IMAGE: 'Text w/Image',
+  TEXT_IMAGE_LINK: 'Text w/Image & Link',
+  TEXT_IMAGE_BUTTON: 'Text w/Image & Button',
+  TEXT_LINK: 'Text w/Link',
+  TEXT_BUTTON: 'Text w/Button',
+  HEADLINE_TEXT_THUMBNAIL: 'Headline & Text w/Thumbnail',
+  HEADLINE_IMAGE: 'Headline w/Image',
+  HEADLINE_TEXT: 'Headline & Text',
+  THUMBNAIL_LINK: 'w/Thumbnail & Link',
+  THUMBNAIL_BUTTON: 'w/Thumbnail & Button',
+  KITCHEN_SINK_PROPS: 'Kitchen Sink (props)',
+  KITCHEN_SINK_COMPOSED: 'Kitchen Sink (composed)',
+};
