a9ae90b7852b018b75ffa0092532b9b066f33a15
Gergely Nemeth <gnemeth@uber.com>
2018-10-02 14:45:19 -0700

    test(cards): add end to end tests

--- e2e/helpers/index.js
+++ e2e/helpers/index.js
@@ -7,19 +7,20 @@ function getUrl({launchUrl, suite, test}) {
   )}&test=${encodeURIComponent(test)}`;
 }
 
-function goToUrl({suite, test, client}) {
-  return client.url(getUrl({launchUrl: client.launchUrl, suite, test}));
+function goToUrl({suite, test, browser}) {
+  const url = getUrl({launchUrl: browser.launchUrl, suite, test});
+  return browser.url(url);
 }
 
 function formatFileName(testName) {
   return testName.toLowerCase().replace(/ /g, '-');
 }
 
-function assertVisuals({client, id}) {
+function assertVisuals({browser, id}) {
   const fileName = formatFileName(id);
 
-  if (client.options.desiredCapabilities.isVrt) {
-    client
+  if (browser.options.desiredCapabilities.isVrt) {
+    browser
       .resizeWindow(1024, 768)
       .assert.screenshotIdenticalToBaseline(fileName);
   }
