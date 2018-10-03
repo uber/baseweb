85453d3c2c0f233b4bd6b3fb546470488e4853e9
Gergely Nemeth <gnemeth@uber.com>
2018-10-03 15:51:12 -0700

    docs(move): use the same addon.js for storybook

--- .storybook-move/addons.js
+++ .storybook-move/addons.js
@@ -1,7 +1,1 @@
-import 'storybook-readme/register';
-import '@storybook/addon-viewport';
-import '@storybook/addon-a11y/register';
-import '@storybook/addon-options/register';
-import '@storybook/addon-actions/register';
-import '@storybook/addon-links/register';
-import '@storybook/addon-knobs/register';
+import '../.storybook-base/addons.js';
