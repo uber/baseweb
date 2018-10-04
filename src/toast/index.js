e2a9b9e9faf87c1a33a86aba0a287ef6d72ded20
Nadiia Dmytrenko <nadiia@uber.com>
2018-10-03 18:20:18 -0700

    feat(toast): initial implementation of the toast component

---
+++ src/toast/index.js
@@ -0,0 +1,16 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+export {default as StatefulToast} from './stateful-toast';
+export {default as StatefulContainer} from './stateful-container';
+export {default as Toast} from './toast';
+// Constants
+export {KIND, PLACEMENT} from './constants';
+// Styled elements
+export {Body as StyledBody, Svg as StyledSvg} from './styled-components';
+// Flow
+export * from './types';
