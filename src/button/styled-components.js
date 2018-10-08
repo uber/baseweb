7b94744c4cb277edc21d86dde89bc5b637140e60
Anh Mai <abmai91@gmail.com>
2018-10-07 23:56:25 -0700

    fix: implement using internal onclick

--- src/button/styled-components.js
+++ src/button/styled-components.js
@@ -21,7 +21,6 @@ export const BaseButton = styled(
     ...($size === SIZE.compact
       ? $theme.typography.font200
       : $theme.typography.font300),
-    pointerEvents: $isLoading ? 'none' : 'initial',
     display: 'inline-flex',
     alignItems: 'center',
     justifyContent: 'center',
