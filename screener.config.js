eaa97c95394154d776d551cc07bdeac47990294c
Gergely Nemeth <gnemeth@uber.com>
2018-10-11 11:19:55 -0700

    feat(vrt): add screener

---
+++ screener.config.js
@@ -0,0 +1,9 @@
+/* eslint-env node */
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+module.exports = {
+  projectRepo: 'uber-web/baseui',
+  storybookConfigDir: '.storybook-move',
+  apiKey: process.env.SCREENER_API_KEY,
+  resolution: '1024x768',
+};
