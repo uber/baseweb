0d0c37ceaef981b6ad2209ad8cdefacd13c662a9
Gergely Nemeth <gergelyke@users.noreply.github.com>
2018-10-16 11:29:18 -0700

    refactor(popover): prep for e2e tests (#392)

---
+++ src/popover/examples-list.js
@@ -0,0 +1,13 @@
+/* eslint-env node */
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+module.exports = {
+  SIMPLE_EXAMPLE: 'stateless popover',
+  WITH_CLICK: 'stateful popover (click)',
+  WITH_HOVER: 'stateful popover (hover)',
+  PLACEMENTS: 'popover placements',
+  ARROW: 'popover w/ arrow',
+  CLOSE_CALLBACK: 'Popover close callback',
+  CLIPPING: 'Popover clipping avoidance',
+  STYLE_OVERRIDES: 'Popover style overrides',
+};
