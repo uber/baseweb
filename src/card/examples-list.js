631b0dcfa8871926df6214d24dabc5b6df5543a3
Steve Anderson <steve@twindx.com>
2018-10-25 18:26:49 +0100

    Fixes typo (#417)

--- src/card/examples-list.js
+++ src/card/examples-list.js
@@ -4,7 +4,7 @@
 module.exports = {
   TEXT_ONLY: 'Text only',
   TEXT_IMAGE: 'Text with Image',
-  TEXT_IMAGE_LINK: 'Text wwith Image and Link',
+  TEXT_IMAGE_LINK: 'Text with Image and Link',
   TEXT_IMAGE_BUTTON: 'Text with Image and Button',
   TEXT_LINK: 'Text with Link',
   TEXT_BUTTON: 'Text with Button',
