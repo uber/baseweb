eb31b9fae9e4bb69514a645e7ff9f7d91e19c380
Gergely Nemeth <gnemeth@uber.com>
2018-10-30 10:20:23 -0700

    refactor(pagination): prep for e2e tests

---
+++ src/pagination/examples-list.js
@@ -0,0 +1,7 @@
+/* eslint-env node */
+/* eslint-disable flowtype/require-valid-file-annotation */
+module.exports = {
+  STATEFUL_PAGINATION: 'Stateful Pagination',
+  STATELESS_PAGINATION: 'Stateless Pagination',
+  PAGINATION_OVERRIDE: 'With Overrides',
+};
