4f68b6619f0c4a0aa15bae7fb6880f12f4f1d85c
Gergely Nemeth <gergelyke@users.noreply.github.com>
2018-10-31 13:37:10 -0700

    chore(radio): refactoring examples to prep for e2e test (#443)

---
+++ src/radio/examples-list.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+/* eslint-disable flowtype/require-valid-file-annotation */
+module.exports = {
+  SIMPLE_EXAMPLE: 'Radio Group example',
+  WITH_ERROR: 'RadioGroup with an error',
+  DISABLED: 'RadioGroup disabled',
+  HORIZONTAL_ALIGN: 'RadioGroup horizontal',
+  VERTICAL_ALIGN: 'RadioGroup vertical',
+  STYLES_OVERRIDES: 'RadioGroup with style overrides',
+  COMPONENTS_OVERRIDES: 'RadioGroup with override components',
+  EXTRA_PROPS: 'RadioGroup with extra props',
+};
