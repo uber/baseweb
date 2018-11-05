69c69e673a8f25d8b259f7a08e26c741e8f3a308
Nadiia <nadiia.dmytrenko@gmail.com>
2018-11-05 11:23:32 -0800

    fix(textarea): remove types related condition in the input stateful container that was breaking stateful textarea (#450)

---
+++ src/textarea/examples-list.js
@@ -0,0 +1,10 @@
+/* eslint-env node */
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+module.exports = {
+  SIMPLE_EXAMPLE: 'Controlled and uncontrolled textarea',
+  SIZE_EXAMPLE: 'Textarea size',
+  STATE_EXAMPLE: 'Textarea state',
+  OVERRIDES_EXAMPLE: 'Textarea with style overrides and extra props passed',
+  REF_EXAMPLE: 'Textarea with a ref',
+};
