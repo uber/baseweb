aef71cbc41ac62613805f0d3522ce9d898dd89bd
Austin Green <austin.green@uber.com>
2018-11-07 13:43:37 -0800

    chore(license): add license header linting rules

--- examples/fusion/src/main.js
+++ examples/fusion/src/main.js
@@ -1,3 +1,10 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
 // @flow
 import App from 'fusion-react';
 import Router from 'fusion-plugin-react-router';
--- src/icon/icon-exports.js
+++ src/icon/icon-exports.js
@@ -1,3 +1,10 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
 // @flow
 export {default as Alert} from './alert';
 export {default as ArrowDown} from './arrow-down';
