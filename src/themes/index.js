7452bd0767eeb5870342543be5c7da5556714900
Gergely Nemeth <gergelyke@users.noreply.github.com>
2018-11-08 13:50:52 -0800

    fix(fonts): ubermove scale updates (#460)

--- src/themes/index.js
+++ src/themes/index.js
@@ -6,13 +6,11 @@ LICENSE file in the root directory of this source tree.
 */
 // @flow
 import createTheme from './creator';
-import {primitives as lightThemePrimitives} from './light-theme';
+import {primitives as lightThemePrimitives} from './light-theme-primitives';
+import {LightThemeMove} from './light-theme-with-move';
 
 export const LightTheme = createTheme(lightThemePrimitives);
 
-export const LightThemeMove = createTheme({
-  ...lightThemePrimitives,
-  primaryFontFamily:
-    'UberMoveText, "Open Sans", "Helvetica Neue", Helvetica, sans-serif',
-});
+export {LightThemeMove};
+
 export * from './types';
---
+++ src/themes/light-theme-with-move.js
@@ -0,0 +1,104 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+import createTheme from './creator';
+import {primitives as lightThemePrimitives} from './light-theme-primitives';
+
+const primaryFontFamily =
+  'UberMoveText, "Open Sans", "Helvetica Neue", Helvetica, sans-serif';
+
+const secondaryFontFamily =
+  'UberMove, UberMoveText, "Open Sans", "Helvetica Neue", Helvetica, sans-serif';
+
+export const LightThemeMove = createTheme(
+  {
+    ...lightThemePrimitives,
+    primaryFontFamily,
+  },
+  {
+    typography: {
+      font200: {
+        fontFamily: primaryFontFamily,
+        fontSize: '12px',
+        fontWeight: 'normal',
+        lineHeight: '20px',
+      },
+      font250: {
+        fontFamily: primaryFontFamily,
+        fontSize: '12px',
+        fontWeight: 'bold',
+        lineHeight: '20px',
+      },
+      font300: {
+        fontFamily: primaryFontFamily,
+        fontSize: '14px',
+        fontWeight: 'normal',
+        lineHeight: '20px',
+      },
+      font350: {
+        fontFamily: primaryFontFamily,
+        fontSize: '14px',
+        fontWeight: 'bold',
+        lineHeight: '20px',
+      },
+      font400: {
+        fontFamily: primaryFontFamily,
+        fontSize: '16px',
+        fontWeight: 'normal',
+        lineHeight: '24px',
+      },
+      font450: {
+        fontFamily: primaryFontFamily,
+        fontSize: '16px',
+        fontWeight: 'bold',
+        lineHeight: '24px',
+      },
+      font500: {
+        fontFamily: primaryFontFamily,
+        fontSize: '20px',
+        fontWeight: 'bold',
+        lineHeight: '28px',
+      },
+      font600: {
+        fontFamily: primaryFontFamily,
+        fontSize: '24px',
+        fontWeight: 'bold',
+        lineHeight: '36px',
+      },
+      font700: {
+        fontFamily: primaryFontFamily,
+        fontSize: '32px',
+        fontWeight: 'bold',
+        lineHeight: '48px',
+      },
+      font800: {
+        fontFamily: primaryFontFamily,
+        fontSize: '40px',
+        fontWeight: 'bold',
+        lineHeight: '56px',
+      },
+      font900: {
+        fontFamily: primaryFontFamily,
+        fontSize: '52px',
+        fontWeight: 'bold',
+        lineHeight: '68px',
+      },
+      font1000: {
+        fontFamily: primaryFontFamily,
+        fontSize: '72px',
+        fontWeight: 'normal',
+        lineHeight: '96px',
+      },
+      font1100: {
+        fontFamily: secondaryFontFamily,
+        fontSize: '96px',
+        fontWeight: 'normal',
+        lineHeight: '116px',
+      },
+    },
+  },
+);
