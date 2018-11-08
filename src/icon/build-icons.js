77d873f037f5305f38bac4cab90284ae8b7fb182
Gergely Nemeth <gnemeth@uber.com>
2018-11-08 11:46:33 -0800

    fix(icons): update to new styles

--- src/icon/build-icons.js
+++ src/icon/build-icons.js
@@ -7,8 +7,9 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 
-// @flow
+/* eslint-disable flowtype/require-valid-file-annotation */
 /* eslint-env node*/
+
 const fs = require('fs');
 const path = require('path');
 const prettier = require('prettier');
