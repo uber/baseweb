d17a45cef93e1cbf49908a638f253d4ea01c074a
Austin Green <austingreenkansas@gmail.com>
2018-11-08 11:20:04 -0800

    chore(license): add license header linting rules (#467)

--- scripts/flow-copy-src.js
+++ scripts/flow-copy-src.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
 /* eslint-env node */
 // @flow
 
--- src/link/stories.js
+++ src/link/stories.js
@@ -1,5 +1,13 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
 /* @flow */
 /*global module */
+
 import {storiesOf} from '@storybook/react';
 
 import examples from './examples';
