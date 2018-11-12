cc7d193f0ecf3a46b3250f8885543637c0de38ef
Gergely Nemeth <gergelyke@users.noreply.github.com>
2018-11-12 12:09:38 -0800

    test(tags): add axe end to end tests (#490)

---
+++ src/tag/e2e.js
@@ -0,0 +1,41 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+/* eslint-env node */
+/* eslint-disable flowtype/require-valid-file-annotation */
+/* global after */
+
+const scenarios = require('./examples-list');
+const {goToUrl} = require('../../e2e/helpers');
+
+const suite = 'Tag Test Suite';
+
+const selectors = {
+  tags: 'span',
+};
+
+describe.only('The Tag component', () => {
+  after((browser, done) => {
+    browser.end(() => done());
+  });
+
+  it('passes basic accessiblity tests', browser => {
+    goToUrl({
+      suite,
+      test: scenarios.ALL_BASIC_COLORS,
+      browser,
+    })
+      .initAccessibility()
+      .waitForElementVisible(selectors.tags)
+      .assert.accessibility('html', {
+        rules: {
+          'color-contrast': {
+            enabled: false,
+          },
+        },
+      });
+  });
+});
