b1408eb2e837f2284e64de679ea7870fa963f3c1
Austin Green <austingreenkansas@gmail.com>
2018-11-12 08:56:10 -0800

    feat(block): introduce new Block layout and typography component (#481)

---
+++ src/block/examples-list.js
@@ -0,0 +1,16 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+/* eslint-env node */
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+module.exports = {
+  DEFAULT: 'Themable colors',
+  FONTS: 'Themable typography styles',
+  FLEXBOX: 'Flexbox layout',
+  OVERRIDES: 'Applying styles via overrides',
+};
---
+++ src/block/examples.js
@@ -0,0 +1,104 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+// @flow
+
+import React from 'react';
+import {Block} from './';
+import examples from './examples-list';
+
+export default {
+  [examples.DEFAULT]: function Story1() {
+    const colors = ['primary', 'negative', 'warning', 'positive'];
+    const elements = [];
+
+    for (const color of colors) {
+      for (let x = 100; x <= 700; x += 100) {
+        const colorString = `${color}${x}`;
+        elements.push(<Block color={colorString}>{colorString}</Block>);
+      }
+    }
+
+    return elements;
+  },
+  [examples.FONTS]: function Story2() {
+    const sizes = [
+      100,
+      200,
+      250,
+      300,
+      350,
+      400,
+      450,
+      500,
+      600,
+      700,
+      800,
+      900,
+      1000,
+    ];
+
+    return sizes.map(size => {
+      const fontString = `font${size}`;
+
+      return (
+        <Block key={size} font={fontString}>
+          {fontString}
+        </Block>
+      );
+    });
+  },
+  [examples.FLEXBOX]: function Story4() {
+    const elements = [];
+
+    for (let x = 0; x < 25; x++) {
+      elements.push(
+        <Block
+          key={x}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          width="200px"
+          height="200px"
+          margin="scale200"
+          font="font400"
+          color="primary"
+          $style={{border: 'grey solid 2px'}}
+        >
+          Positioned with flexbox
+        </Block>,
+      );
+    }
+
+    return (
+      <Block
+        display="flex"
+        direction="column"
+        padding="scale3200"
+        $style={{border: 'grey solid 4px'}}
+        flexWrap
+      >
+        {elements}
+      </Block>
+    );
+  },
+  [examples.OVERRIDES]: function Story5() {
+    return (
+      <Block
+        as="h2"
+        font="font500"
+        overrides={{
+          Block: {
+            style: {color: 'red'},
+          },
+        }}
+      >
+        These styles are provided by styletron
+      </Block>
+    );
+  },
+};
