4c7b934be8069b581e83e6d569262087fcd148d5
Austin Green <austingreenkansas@gmail.com>
2018-11-27 09:29:01 -0800

    feat: introduce StarRating and EmoticonRating components (#540)

---
+++ src/rating/examples-list.js
@@ -0,0 +1,16 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+/* eslint-env node */
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+module.exports = {
+  DEFAULT: 'Rating stars',
+  CUSTOM_STARS: 'Rating with 10 stars',
+  EMOTICON: 'Rating emoticons',
+  OVERRIDES: 'Rating overrides customization',
+};
