4c4de4bc85a8fcc50a972327ae1b852caf066f15
Oleg Gomozov <blackswanny@gmail.com>
2018-11-30 13:21:42 -0800

    feat(header-navigation): add implementation (#525)
    * feat: add header navigation implementation
    
    * feat: add unit tests to header navigation
    
    * feat: add HOC for props transformation to styled elements:
    
    * feat: add HOC for props transformation to styled elements:

---
+++ src/header-navigation/__tests__/header-navigation.test.js
@@ -0,0 +1,42 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+import React from 'react';
+import {mount} from 'enzyme';
+import {styled} from '../../styles';
+import {HeaderNavigation} from '../index';
+
+describe('Stateless header navigation', function() {
+  let wrapper, children;
+  let allProps: any = {};
+
+  beforeEach(function() {
+    children = 'Some tag';
+  });
+
+  afterEach(function() {
+    jest.restoreAllMocks();
+    wrapper && wrapper.unmount();
+  });
+
+  test('should render component', function() {
+    wrapper = mount(
+      <HeaderNavigation {...allProps}>{children}</HeaderNavigation>,
+    );
+    expect(wrapper).toMatchSnapshot('Component has correct render');
+  });
+
+  test('should replace overriden root', function() {
+    const newRoot = styled('div', {color: 'red'});
+    allProps.overrides = {Root: newRoot};
+    wrapper = mount(
+      <HeaderNavigation {...allProps}>{children}</HeaderNavigation>,
+    );
+    const renderedRoot = wrapper.find(allProps.overrides.Root);
+    expect(renderedRoot).toHaveLength(1);
+  });
+});
