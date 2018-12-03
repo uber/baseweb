298a8acf0f3508531372467ab2d2fb909f503acb
chasestarr <starr@uber.com>
2018-12-03 14:49:00 -0800

    docs: popover rfc review

--- src/popover/stories.js
+++ src/popover/stories.js
@@ -10,8 +10,7 @@ import {storiesOf} from '@storybook/react';
 import {withReadme} from 'storybook-readme';
 import examples from './examples';
 
-//$FlowFixMe
-import PopoverReadme from '../../rfcs/popover-component.md';
+import PopoverReadme from './README.md';
 
 Object.entries(examples).forEach(([description, example]) =>
   storiesOf('Popover', module)
