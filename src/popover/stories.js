763616eab88a0aed918548f8129290a34223a51f
Chase Starr <chasestarr@gmail.com>
2018-12-05 11:12:53 -0800

    docs(popover) review popover rfc (#605)
    * docs: review popover rfc
    
    * Update README.md

--- src/popover/stories.js
+++ src/popover/stories.js
@@ -11,7 +11,7 @@ import {withReadme} from 'storybook-readme';
 import examples from './examples';
 
 //$FlowFixMe
-import PopoverReadme from '../../rfcs/popover-component.md';
+import PopoverReadme from './README.md';
 
 Object.entries(examples).forEach(([description, example]) =>
   storiesOf('Popover', module)
