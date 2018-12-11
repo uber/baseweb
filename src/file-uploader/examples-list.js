17513fd6c7fcadd1752634e5c512fc76e57619e4
Chase Starr <chasestarr@gmail.com>
2018-12-11 13:37:39 -0800

    feat(file-uploader): implements file uploader (#628)
    * feat: adds attr-accept package for file uploader
    
    * feat: initial/rough file uploader implementation
    
    * feat: wip on the file-uploader
    
    * feat: adds react-dropzone as dep
    
    * chore: removes attr-accept package
    
    * feat: gets file uploader implementation settled
    
    * feat: enables dark mode on file uploader
    
    * feat: adds overrides
    
    * fix: allow for mixed to pass eslint
    
    * fix: lint
    
    * fix: adds display names
    
    * fix: lint
    
    * feat: styled-component snap tests
    
    * feat: component test for a11y
    
    * fix: lint
    
    * feat: adds to e2e tests
    
    * feat: actually use it in the e2e test
    
    * feat: adds file uploader to screener
    
    * fix: only add one example to screener
    
    * fix: removes old rfc
    
    * feat: prefix component with 'unstable'
    
    * fix: various style updates

---
+++ src/file-uploader/examples-list.js
@@ -0,0 +1,15 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+/* eslint-env node */
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+module.exports = {
+  FILE_UPLOADER_EXAMPLE: 'File Uploader',
+  FILE_UPLOADER_ACCEPT: 'Custom accept value',
+  FILE_UPLOADER_DISABLED: 'Disabled',
+  FILE_UPLOADER_OVERRIDES: 'Overrides',
+};
