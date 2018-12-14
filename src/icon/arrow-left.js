1d47d16637b9dbb3b93fcbb36b2fc23d170bedf1
Gergely Nemeth <gergelyke@users.noreply.github.com>
2018-12-14 14:29:08 -0800

    fix(icons): revert to original icons (#655)

--- src/icon/arrow-left.js
+++ src/icon/arrow-left.js
@@ -14,20 +14,11 @@ import type {IconPropsT} from './types.js';
 export default function ArrowLeft(props: IconPropsT) {
   return (
     <Icon title="Arrow Left" viewBox="0 0 24 24" {...props}>
-      <g
-        transform="matrix(
-        1 0
-        0 1
-        1.800048828125 3
-      )"
-      >
-        <path
-          fillRule="nonzero"
-          clipRule="nonzero"
-          d="M 20.199996948242188 10.5 L 4.5 10.5 L 10 18 L 6.5 18 L 0 9 L 6.5 0 L 10 0 L 4.5 7.5 L 20.199996948242188 7.5 L 20.199996948242188 10.5 Z"
-          fill="currentColor"
-        />
-      </g>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M6.29289 11.2929C5.90237 11.6834 5.90237 12.3166 6.29289 12.7071L10.2929 16.7071C10.6834 17.0976 11.3166 17.0976 11.7071 16.7071C12.0976 16.3166 12.0976 15.6834 11.7071 15.2929L9.41421 13H17C17.5523 13 18 12.5523 18 12C18 11.4477 17.5523 11 17 11H9.41421L11.7071 8.70711C12.0976 8.31658 12.0976 7.68342 11.7071 7.29289C11.3166 6.90237 10.6834 6.90237 10.2929 7.29289L6.29289 11.2929Z"
+      />
     </Icon>
   );
 }
