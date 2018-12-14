82fd9f25bc52798a722eb4d8cdfd140c0997d20a
Vojtech Miksu <vojtech@uber.com>
2018-12-13 17:42:34 -0800

    fix(header-navigation): add missing import for types

--- src/header-navigation/styled-components.js
+++ src/header-navigation/styled-components.js
@@ -5,6 +5,7 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 // @flow
+import * as React from 'react';
 import {styled, asPrimaryExport} from '../styles/index.js';
 import {ALIGN} from './index.js';
 
