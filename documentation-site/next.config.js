0a165dcd26fe28efa217dbf52fb69ff065e46177
Gergely Nemeth <gnemeth@uber.com>
2018-12-19 13:16:59 -0800

    feat(docs): skeleton for new docs site

--- documentation-site/next.config.js
+++ documentation-site/next.config.js
@@ -1,3 +1,13 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+/* eslint-disable flowtype/require-valid-file-annotation */
+/* eslint-env node */
+
 const {resolve} = require('path');
 
 module.exports = {
--- documentation-site/pages/index.js
+++ documentation-site/pages/index.js
@@ -1,3 +1,12 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+/* eslint-disable flowtype/require-valid-file-annotation */
+
 import {Card, StyledTitle, StyledBody} from 'baseui/card';
 
 export default function Hello() {
