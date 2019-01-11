afa2e00fae12952092fa20324b7de3dd3152cbf9
Chase Starr <chasestarr@gmail.com>
2019-01-11 15:32:27 -0800

    docs(checkbox): adds to new docs site (#746)

--- src/checkbox/types.js
+++ src/checkbox/types.js
@@ -38,27 +38,47 @@ export type DefaultPropsT = {
 };
 
 export type PropsT = {
+  /** Component or String value for label of checkbox. */
   children?: React$Node,
   overrides?: OverridesT,
+  /** Check or uncheck the control. */
   checked?: boolean,
+  /** Disable the checkbox from being changed. */
   disabled?: boolean,
+  /** Marks the checkbox as required. */
   required?: boolean,
+  /** Renders checkbox in errored state. */
   isError?: boolean,
+  /** Used to get a ref to the input element. Useful for programmatically focusing the input */
   inputRef: {current: ?HTMLInputElement},
+  /** Focus the checkbox on initial render. */
   autoFocus?: boolean,
+  /** Passed to the input element type attribute */
   type?: string,
+  /** Passed to the input element name attribute */
   name?: string,
+  /** Passed to the input element value attribute */
   value?: string,
+  /** Indicates a 'half' state for the checkmark. In this case, `checked` is ignored. */
   isIndeterminate?: boolean,
+  /** How to position the label relative to the checkbox itself. */
   labelPlacement?: LabelPlacementT,
+  /** Renders UI as checkmark or toggle switch. */
   checkmarkType: StyleTypeT,
   $theme?: *,
+  /** Handler for change events on trigger element. */
   onChange?: (e: SyntheticInputEvent<HTMLInputElement>) => void,
+  /** Handler for mouseenter events on trigger element. */
   onMouseEnter: (e: SyntheticInputEvent<HTMLInputElement>) => void,
+  /** Handler for mouseleave events on trigger element. */
   onMouseLeave: (e: SyntheticInputEvent<HTMLInputElement>) => void,
+  /** Handler for mousedown events on trigger element. */
   onMouseDown: (e: SyntheticInputEvent<HTMLInputElement>) => void,
+  /** Handler for mouseup events on trigger element. */
   onMouseUp: (e: SyntheticInputEvent<HTMLInputElement>) => void,
+  /** handler for focus events on trigger element. */
   onFocus: (e: SyntheticInputEvent<HTMLInputElement>) => void,
+  /** handler for blur events on trigger element. */
   onBlur: (e: SyntheticInputEvent<HTMLInputElement>) => void,
 };
 
@@ -93,25 +113,42 @@ export type DefaultStatefulPropsT = {
 
 export type StatefulContainerPropsT = {
   overrides?: OverridesT,
+  /** Component or String value for label of checkbox. */
   children?: (*) => React$Node,
+  /** Defines the components initial state value */
   initialState?: StateT,
+  /** A state change handler. Used to override default state transitions. */
   stateReducer: StateReducerT,
+  /** Handler for change events on trigger element. */
   onChange?: (e: SyntheticInputEvent<HTMLInputElement>) => void,
+  /** Handler for mouseenter events on trigger element. */
   onMouseEnter?: (e: SyntheticInputEvent<HTMLInputElement>) => void,
+  /** Handler for mouseleave events on trigger element. */
   onMouseLeave?: (e: SyntheticInputEvent<HTMLInputElement>) => void,
+  /** Handler for focus events on trigger element. */
   onFocus?: (e: SyntheticInputEvent<HTMLInputElement>) => void,
+  /** Handler for blur events on trigger element. */
   onBlur?: (e: SyntheticInputEvent<HTMLInputElement>) => void,
+  /** Focus the checkbox on initial render. */
   autoFocus?: boolean,
 };
 
 export type StatefulCheckboxPropsT = {
   overrides?: OverridesT,
+  /** Component or String value for label of checkbox. */
   children?: React$Node,
+  /** Defines the components initial state value */
   initialState?: StateT,
+  /** Focus the checkbox on initial render. */
   autoFocus?: boolean,
+  /** Handler for change events on trigger element. */
   onChange?: (e: SyntheticInputEvent<HTMLInputElement>) => void,
+  /** Handler for mouseenter events on trigger element. */
   onMouseEnter?: (e: SyntheticInputEvent<HTMLInputElement>) => void,
+  /** Handler for mouseleave events on trigger element. */
   onMouseLeave?: (e: SyntheticInputEvent<HTMLInputElement>) => void,
+  /** Handler for focus events on trigger element. */
   onFocus?: (e: SyntheticInputEvent<HTMLInputElement>) => void,
+  /** Handler for blur events on trigger element. */
   onBlur?: (e: SyntheticInputEvent<HTMLInputElement>) => void,
 };
