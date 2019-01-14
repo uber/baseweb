879c50bb7ae3ae2a41bd343937c42fa0809eeb8a
chasestarr <starr@uber.com>
2019-01-14 10:10:36 -0800

    fix: removes unnecessary control flow

--- documentation-site/pages/_app.js
+++ documentation-site/pages/_app.js
@@ -12,16 +12,12 @@ import {LightTheme, ThemeProvider} from 'baseui';
 import App, {Container} from 'next/app';
 import {Provider as StyletronProvider} from 'styletron-react';
 
-import {isServer, styletron} from '../helpers/styletron';
+import {styletron} from '../helpers/styletron';
 
 export default class MyApp extends App {
   render() {
     const {Component, pageProps} = this.props;
-    return isServer ? (
-      <Container>
-        <Component {...pageProps} />
-      </Container>
-    ) : (
+    return (
       <Container>
         <StyletronProvider value={styletron}>
           <ThemeProvider theme={LightTheme}>
