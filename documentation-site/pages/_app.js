52eb6ccdfe3873b87af11fe916bba22625f9e825
chasestarr <starr@uber.com>
2019-01-14 08:37:43 -0800

    fix: resolves flash of unstyled content

--- documentation-site/pages/_app.js
+++ documentation-site/pages/_app.js
@@ -5,25 +5,23 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 
-/* eslint-disable flowtype/require-valid-file-annotation */
-
 import React from 'react';
-import App, {Container} from 'next/app';
-
-import {Provider as StyletronProvider} from 'styletron-react';
 import {LightTheme, ThemeProvider} from 'baseui';
+import App, {Container} from 'next/app';
+import {Provider as StyletronProvider} from 'styletron-react';
 
-import getStyletron from '../helpers/styletron';
-import Head from '../components/meta';
+import {isServer, styletron} from '../helpers/styletron';
 
 export default class MyApp extends App {
   render() {
     const {Component, pageProps} = this.props;
-
-    return (
+    return isServer ? (
       <Container>
-        <Head />
-        <StyletronProvider value={getStyletron()}>
+        <Component {...pageProps} />
+      </Container>
+    ) : (
+      <Container>
+        <StyletronProvider value={styletron}>
           <ThemeProvider theme={LightTheme}>
             <Component {...pageProps} />
           </ThemeProvider>
---
+++ documentation-site/pages/_document.js
@@ -0,0 +1,45 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+import Document, {Head, Main, NextScript} from 'next/document';
+import {Provider as StyletronProvider} from 'styletron-react';
+
+import Meta from '../components/meta';
+import {styletron} from '../helpers/styletron';
+
+export default class MyDocument extends Document {
+  static getInitialProps(props) {
+    const page = props.renderPage(App => props => (
+      <StyletronProvider value={styletron}>
+        <App {...props} />
+      </StyletronProvider>
+    ));
+    const stylesheets = styletron.getStylesheets() || [];
+    return {...page, stylesheets};
+  }
+  render() {
+    return (
+      <html>
+        <Head>
+          {this.props.stylesheets.map((sheet, i) => (
+            <style
+              className="_styletron_hydrate_"
+              dangerouslySetInnerHTML={{__html: sheet.css}}
+              media={sheet.attrs.media || ''}
+              key={i}
+            />
+          ))}
+        </Head>
+        <Meta />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
