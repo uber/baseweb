396d1073a0941544e56a1c68b28091fd25be0af4
Nadiia <nadiia.dmytrenko@gmail.com>
2019-01-23 11:36:31 -0800

    docs(form-control): move form-control to docs site (#781)

--- src/form-control/types.js
+++ src/form-control/types.js
@@ -11,12 +11,18 @@ import type {OverrideT} from '../helpers/overrides.js';
 
 export type FormControlPropsT = {
   overrides: {
+    /** Customizes the label element. */
     Label?: OverrideT<*>,
+    /** Customizes the caption element. */
     Caption?: OverrideT<*>,
+    /** Customizes the container element. */
     ControlContainer?: OverrideT<*>,
   },
+  /** A label rendered above the input field. */
   label: ?(React.Node | ((props: {}) => React.Node)),
+  /** A caption rendered below the input field. */
   caption: ?(React.Node | ((props: {}) => React.Node)),
+  /** Error state of the input. If an error prop passed it will be rendered in place of caption as an error message. */
   error: boolean | React.Node | ((props: {}) => React.Node),
   children: React.Node,
 };
