65c73d7b9a3ca8e434f8fefa7af1248cf97ce4fc
Gergely Nemeth <gnemeth@uber.com>
2019-01-23 10:31:46 -0800

    feat(puppeteer): adding the test runner

---
+++ src/accordion/pup-e2e.js
@@ -0,0 +1,19 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+// @flow
+/*eslint-env node*/
+
+describe('Google', () => {
+  beforeAll(async () => {
+    await page.goto('https://google.com');
+  });
+
+  it('should display "google" text on page', async () => {
+    await expect(page).toMatch('google');
+  });
+});
