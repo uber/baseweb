4bdde5742341946b4e03f07707f01e0d4224d036
Gergely Nemeth <gnemeth@uber.com>
2019-01-25 13:16:02 -0800

    feat(e2e): migrate component tests part 1

--- src/input/e2e.js
+++ src/input/e2e.js
@@ -7,9 +7,9 @@ LICENSE file in the root directory of this source tree.
 
 /* eslint-env node */
 /* eslint-disable flowtype/require-valid-file-annotation */
-/* global after */
+
 const scenarios = require('./examples-list');
-const {goToUrl} = require('../../e2e/helpers');
+const {getPuppeteerUrl, analyzeAccessibility} = require('../../e2e/helpers');
 
 const suite = 'Input Test Suite';
 
@@ -17,49 +17,41 @@ const selectors = {
   input: 'input[data-test="e2e"]',
 };
 
-describe('The input component', () => {
-  after((browser, done) => {
-    browser.end(() => done());
+describe(suite, () => {
+  beforeAll(async () => {
+    await page.goto(
+      getPuppeteerUrl({
+        suite,
+        test: scenarios.STATE_EXAMPLE,
+      }),
+    );
   });
 
-  xit('passes basic a11y tests', browser => {
-    goToUrl({
-      suite,
-      test: scenarios.STATE_EXAMPLE,
-      browser,
-    })
-      .initAccessibility()
-      .waitForElementVisible(selectors.input)
-      .assert.accessibility('html', {
-        rules: {
-          label: {
-            enabled: false,
-          },
+  it('passes basic a11y tests', async () => {
+    await page.waitFor(selectors.input);
+    const accessibilityReport = await analyzeAccessibility(page, {
+      rules: [
+        {
+          id: 'label',
+          enabled: false,
         },
-      });
+      ],
+    });
+    expect(accessibilityReport).toHaveNoAccessibilityIssues();
   });
 
-  it('preset value is displayed', browser => {
-    goToUrl({
-      suite,
-      test: scenarios.STATE_EXAMPLE,
-      browser,
-    })
-      .waitForElementVisible(selectors.input)
-      .getValue(selectors.input, function(result) {
-        this.assert.equal(result.value, 'uber');
-      });
+  it('preset value is displayed', async () => {
+    await page.waitFor(selectors.input);
+    const value = await page.$eval(selectors.input, input => input.value);
+    expect(value).toBe('uber');
   });
-  it('entered value is displayed', browser => {
-    goToUrl({
-      suite,
-      test: scenarios.STATE_EXAMPLE,
-      browser,
-    })
-      .waitForElementVisible(selectors.input)
-      .setValue(selectors.input, '_good')
-      .getValue(selectors.input, function(result) {
-        this.assert.equal(result.value, 'uber_good');
-      });
+
+  it('entered value is displayed', async () => {
+    await page.waitFor(selectors.input);
+
+    await page.keyboard.type('_good');
+
+    const value = await page.$eval(selectors.input, input => input.value);
+    expect(value).toBe('uber_good');
   });
 });
