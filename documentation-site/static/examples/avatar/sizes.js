d6c0ba70fa42044a7b179b14a9a646faf4c22d3b
Chase Starr <chasestarr@gmail.com>
2019-01-28 12:21:50 -0800

    feat: avatar fallback initials (#798)

--- documentation-site/static/examples/avatar/sizes.js
+++ documentation-site/static/examples/avatar/sizes.js
@@ -6,7 +6,7 @@ export default () => (
     {['scale800', 'scale1000', 'scale1200', 'scale1400', '64px'].map(
       (size, index) => (
         <Avatar
-          name={`user name # ${index}`}
+          name={`user`}
           size={size}
           src={`https://api.adorable.io/avatars/285/${index}@adorable.io.png`}
           key={size}
