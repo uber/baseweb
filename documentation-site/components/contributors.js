ffb6b43c091dabd56da9e02c1d814c97b7a97970
chasestarr <starr@uber.com>
2019-01-28 16:08:03 -0800

    fix: adds auth token to contributors request

--- documentation-site/components/contributors.js
+++ documentation-site/components/contributors.js
@@ -5,15 +5,21 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 // @flow
-/* global fetch */
 
 import React from 'react';
 import {Avatar} from 'baseui/avatar';
 import {Block} from 'baseui/block';
 
+type Contributor = {
+  avatar_url: string,
+  html_url: string,
+  login: string,
+  type: 'Bot' | 'User',
+};
+
 const isUser = user => user.type === 'User';
 
-function Contributors(props) {
+function Contributors(props: {contributors: Contributor[]}) {
   if (!props.contributors.length) {
     return null;
   }
@@ -21,7 +27,7 @@ function Contributors(props) {
   return (
     <Block>
       <Block font="font450">Thank you to our contributors:</Block>
-      <Block display="flex" flexWrap="wrap" maxWidth="768px">
+      <Block display="flex" flexWrap maxWidth="768px">
         {props.contributors.filter(isUser).map(contributor => (
           <Block
             as="a"
--- documentation-site/pages/index.js
+++ documentation-site/pages/index.js
@@ -5,6 +5,7 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 // @flow
+/* global process */
 
 import React from 'react';
 import {Block} from 'baseui/block';
@@ -14,7 +15,14 @@ import Layout from '../components/layout';
 import Contributors from '../components/contributors';
 import Markdown from '../components/markdown-elements';
 
-const Index = props => (
+type Contributor = {
+  avatar_url: string,
+  html_url: string,
+  login: string,
+  type: 'Bot' | 'User',
+};
+
+const Index = (props: {contributors: Contributor[]}) => (
   <Layout>
     <Markdown.h1>Welcome</Markdown.h1>
     <Markdown.p>
@@ -27,13 +35,22 @@ const Index = props => (
       providing designers with a unique catalog of components.
     </Markdown.p>
     <a href="https://github.com/uber-web/baseui">
-      <img src="https://img.shields.io/badge/Source%20Code-On%20GitHub-blue.svg" />
+      <img
+        alt="base ui repository"
+        src="https://img.shields.io/badge/Source%20Code-On%20GitHub-blue.svg"
+      />
     </a>
     <a href="https://join.slack.com/t/baseui/shared_invite/enQtNDI0NTgwMjU0NDUyLTk3YzM1NWY2MjY3NTVjNjk3NzY1MTE5OTI4Y2Q2ZmVkMTUyNDc1MTcwYjZhYjlhOWQ2M2NjOWJkZmQyNjFlYTA">
-      <img src="https://img.shields.io/badge/Join%20us%20on-Slack-e01563.svg" />
+      <img
+        alt="base ui slack"
+        src="https://img.shields.io/badge/Join%20us%20on-Slack-e01563.svg"
+      />
     </a>
     <a href="https://buildkite.com/uberopensource/baseui">
-      <img src="https://badge.buildkite.com/92a7500cd98f619621c4801833d8b358c2fd79efc9b98f1b98.svg?branch=master" />
+      <img
+        alt="base ui buildkite"
+        src="https://badge.buildkite.com/92a7500cd98f619621c4801833d8b358c2fd79efc9b98f1b98.svg?branch=master"
+      />
     </a>
     <Markdown.p>
       To get started with Base UI, read through the{' '}
@@ -52,7 +69,8 @@ const Index = props => (
 
 Index.getInitialProps = async () => {
   const res = await fetch(
-    'https://api.github.com/repos/uber-web/baseui/contributors',
+    `https://api.github.com/repos/uber-web/baseui/contributors?access_token=${process
+      .env.GITHUB_AUTH_TOKEN || ''}`,
   );
   const contributors = await res.json();
 
