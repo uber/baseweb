0ee206c902a35a0ab759c85317a950a932604a47
Vojtech Miksu <vojtech@uber.com>
2019-01-29 17:55:05 -0800

    feat(docs): add overrides to menu

--- src/input/__tests__/styled-components.test.js
+++ src/input/__tests__/styled-components.test.js
@@ -9,7 +9,7 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import {
   StyledRoot,
-  StyledInputEnhancer,
+  StyledStartEnhancer as StyledInputEnhancer,
   StyledInputContainer,
   StyledInput,
   SIZE,
