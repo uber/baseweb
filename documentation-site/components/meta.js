d6cc50db40128b688fa55b342ab5cb29847a757c
Gergely Nemeth <gergelyke@users.noreply.github.com>
2019-01-31 08:49:53 -0800

    feat(docs): add favicon (#823)

--- documentation-site/components/meta.js
+++ documentation-site/components/meta.js
@@ -6,18 +6,49 @@ LICENSE file in the root directory of this source tree.
 */
 
 /* eslint-disable flowtype/require-valid-file-annotation */
+/* global process */
 
 import React from 'react';
 import Head from 'next/head';
 
+const title =
+  process.env.WEBSITE_ENV === 'production'
+    ? 'Base UI documentation'
+    : '[DEV] Base UI documentation';
+
 export default () => (
   <Head>
-    <title>Base UI documentation</title>
+    <title>{title}</title>
     <meta
       name="viewport"
       content="initial-scale=1.0, width=device-width"
       key="viewport"
     />
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="/static/apple-touch-icon.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="/static/favicon-32x32.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="/static/favicon-16x16.png"
+    />
+    <link rel="manifest" href="/static/site.webmanifest" />
+    <link
+      rel="mask-icon"
+      href="/static/safari-pinned-tab.svg"
+      color="#5bbad5"
+    />
+    <meta name="msapplication-TileColor" content="#da532c" />
+    <meta name="theme-color" content="#ffffff" />
     <style>{`
       body {
         margin: 0;
