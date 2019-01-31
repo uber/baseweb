118c1f39a948fb189a3567602c779d45bd14052e
chasestarr <starr@uber.com>
2019-01-30 16:49:04 -0800

    fix: help flow out

--- src/typography/index.js
+++ src/typography/index.js
@@ -12,65 +12,65 @@ import type {BlockPropsT} from '../block/types.js';
 
 // Captions - aka Caption, CaptionLabel
 export const Caption1 = (props: BlockPropsT) => (
-  <Block font="font200" {...props} />
+  <Block {...props} font={props.font || 'font200'} />
 );
 
 export const Caption2 = (props: BlockPropsT) => (
-  <Block font="font250" {...props} />
+  <Block {...props} font={props.font || 'font250'} />
 );
 
 // Display
 export const Display = (props: BlockPropsT) => (
-  <Block font="font1100" {...props} />
+  <Block {...props} font={props.font || 'font1100'} />
 );
 
 // Headings
 export function H1(props: BlockPropsT) {
   const as = props.as || 'h1';
-  return <Block as={as} font="font1000" {...props} />;
+  return <Block as={as} {...props} font={props.font || 'font1000'} />;
 }
 
 export function H2(props: BlockPropsT) {
   const as = props.as || 'h2';
-  return <Block as={as} font="font900" {...props} />;
+  return <Block as={as} {...props} font={props.font || 'font900'} />;
 }
 
 export function H3(props: BlockPropsT) {
   const as = props.as || 'h3';
-  return <Block as={as} font="font800" {...props} />;
+  return <Block as={as} {...props} font={props.font || 'font800'} />;
 }
 
 export function H4(props: BlockPropsT) {
   const as = props.as || 'h4';
-  return <Block as={as} font="font700" {...props} />;
+  return <Block as={as} {...props} font={props.font || 'font700'} />;
 }
 
 export function H5(props: BlockPropsT) {
   const as = props.as || 'h5';
-  return <Block as={as} font="font600" {...props} />;
+  return <Block as={as} {...props} font={props.font || 'font600'} />;
 }
 
 export function H6(props: BlockPropsT) {
   const as = props.as || 'h6';
-  return <Block as={as} font="font500" {...props} />;
+  return <Block as={as} {...props} font={props.font || 'font500'} />;
 }
 
 // Labels - aka Label1, Label2
 export const Label1 = (props: BlockPropsT) => (
-  <Block font="font350" {...props} />
+  <Block {...props} font={props.font || 'font350'} />
 );
 
 export const Label2 = (props: BlockPropsT) => (
-  <Block font="font450" {...props} />
+  <Block {...props} font={props.font || 'font450'} />
 );
 
 // Paragraphs - Paragraph1, Paragraph2
 export function Paragraph1(props: BlockPropsT) {
   const as = props.as || 'p';
-  return <Block as={as} font="font300" {...props} />;
+  return <Block as={as} {...props} font={props.font || 'font300'} />;
 }
 
 export function Paragraph2(props: BlockPropsT) {
   const as = props.as || 'p';
-  return <Block as={as} font="font400" {...props} />;
+  return <Block as={as} {...props} font={props.font || 'font400'} />;
 }
