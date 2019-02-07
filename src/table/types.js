8ac326eac2875dc6d28bbc4517390621becd00a3
chasestarr <starr@uber.com>
2019-02-07 12:53:59 -0800

    docs(table): adds comments to type fields

--- src/table/types.js
+++ src/table/types.js
@@ -11,17 +11,28 @@ import * as React from 'react';
 import type {ThemeT} from '../styles/types.js';
 
 export type TablePropsT = {|
+  /** Table columns. Data passed to each header cell. */
   columns: Array<string | React.Node>,
+  /** Table rows. Data passed to each row and cell */
   data: Array<Array<React.Node>>,
+  /** Table width fills this provided value. */
   horizontalScrollWidth?: string,
+  /** Renders the table in a loading state. Not implemented, yet. */
   isLoading?: boolean,
 |};
 
 export type FilterProps = {|
+  /** Displays the filter icon in an active state. */
   active?: boolean,
+  /** Arbitrary content to display in the filter popover. You will likely want to add checkboxes
+   * or similar inputs to control filtering.
+   */
   children: React.Node,
+  /** Disables the icon click action. Filter menu does not open when clicked. */
   disabled?: boolean,
+  /** Callback for when the 'reset' button is clicked. */
   onReset?: () => mixed,
+  /** Callback for when the 'select all' button is clicked. */
   onSelectAll?: () => mixed,
 |};
 
