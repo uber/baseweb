ddd2be31a2ccaa8896b7c0c3ab091b5e0b76c0ff
chasestarr <starr@uber.com>
2019-02-11 10:58:05 -0800

    chore: removes extra tag stories

--- src/tag/__tests__/tag.stories.js
+++ src/tag/__tests__/tag.stories.js
@@ -4,12 +4,14 @@ Copyright (c) 2018 Uber Technologies, Inc.
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
-
-/* eslint-env node */
+/* global module */
 /* eslint-disable flowtype/require-valid-file-annotation */
-module.exports = {
-  ALL_BASIC_COLORS: 'All basic colors',
-  WITH_CUSTOM_COLORS: 'With custom colors',
-  DISABLED: 'Disabled',
-  UNCLOSEABLE: 'Uncloseable',
-};
+
+import {storiesOf} from '@storybook/react';
+
+import scenarios from './*.scenario.js';
+
+scenarios.reduce(
+  (stories, scenario) => stories.add(scenario.name, scenario.component),
+  storiesOf('Tag', module),
+);
