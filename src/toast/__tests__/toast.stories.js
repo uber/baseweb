a9bfb7e04168ffeded247fe018b707fd39c64dc0
chasestarr <starr@uber.com>
2019-02-11 11:03:02 -0800

    chore: removes extra toast stories

--- src/toast/__tests__/toast.stories.js
+++ src/toast/__tests__/toast.stories.js
@@ -4,15 +4,14 @@ Copyright (c) 2018 Uber Technologies, Inc.
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
-
-/* eslint-env node */
+/* global module */
 /* eslint-disable flowtype/require-valid-file-annotation */
 
-module.exports = {
-  projectRepo: 'uber-web/baseui',
-  storybookConfigDir: '.storybook',
-  apiKey: process.env.SCREENER_API_KEY,
-  resolution: '1024x768',
-  baseBranch: 'master',
-  failureExitCode: 0,
-};
+import {storiesOf} from '@storybook/react';
+
+import scenarios from './*.scenario.js';
+
+scenarios.reduce(
+  (stories, scenario) => stories.add(scenario.name, scenario.component),
+  storiesOf('Toast', module),
+);
