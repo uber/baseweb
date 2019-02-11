0159ccf24395e17ccd558b169e563921376f6e68
chasestarr <starr@uber.com>
2019-02-11 10:11:08 -0800

    chore: link scenarios

--- src/link/__tests__/link.scenario.js
+++ src/link/__tests__/link.scenario.js
@@ -5,11 +5,11 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 // @flow
-import React from 'react';
-import {StyledLink} from './index.js';
 
-export default {
-  'Default Link': function Story1() {
-    return <StyledLink href="#">I am a Link!</StyledLink>;
-  },
-};
+import React from 'react';
+
+import {StyledLink} from '../index.js';
+
+export const name = 'link';
+
+export const component = () => <StyledLink href="#">I am a Link!</StyledLink>;
