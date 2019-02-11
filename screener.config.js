131defdb572b74f34360100d1701411364f5f168
chasestarr <starr@uber.com>
2019-02-11 11:00:23 -0800

    chore: removes extra textarea stories

--- screener.config.js
+++ screener.config.js
@@ -8,7 +8,6 @@ LICENSE file in the root directory of this source tree.
 /* eslint-env node */
 /* eslint-disable flowtype/require-valid-file-annotation */
 
-const Textarea = require('./src/textarea/examples-list');
 const Toast = require('./src/toast/examples-list');
 
 module.exports = {
@@ -17,9 +16,6 @@ module.exports = {
   apiKey: process.env.SCREENER_API_KEY,
   resolution: '1024x768',
   baseBranch: 'master',
-  includeRules: [
-    new RegExp(Textarea.STATE_EXAMPLE),
-    new RegExp(Toast.TOAST_EXAMPLE),
-  ],
+  includeRules: [new RegExp(Toast.TOAST_EXAMPLE)],
   failureExitCode: 0,
 };
