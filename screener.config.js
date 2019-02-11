d33944d7ee12a980d64e8091e3911ef65490a774
chasestarr <starr@uber.com>
2019-02-11 10:38:23 -0800

    chore: removes extra select stories

--- screener.config.js
+++ screener.config.js
@@ -8,7 +8,6 @@ LICENSE file in the root directory of this source tree.
 /* eslint-env node */
 /* eslint-disable flowtype/require-valid-file-annotation */
 
-const Select = require('./src/select/examples-list');
 const Slider = require('./src/slider/examples-list');
 const Textarea = require('./src/textarea/examples-list');
 const Toast = require('./src/toast/examples-list');
@@ -20,8 +19,6 @@ module.exports = {
   resolution: '1024x768',
   baseBranch: 'master',
   includeRules: [
-    new RegExp(Select.MULTI_SELECT),
-    new RegExp(Select.SINGLE_SELECT_SEARCH),
     new RegExp(Slider.AS_SIMPLE_RANGE_SLIDER),
     new RegExp(Textarea.STATE_EXAMPLE),
     new RegExp(Toast.TOAST_EXAMPLE),
