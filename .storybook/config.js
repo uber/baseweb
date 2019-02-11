7a88da2ceaf0c2b58e61ee11db02263976cdbba1
chasestarr <starr@uber.com>
2019-02-11 11:42:01 -0800

    chore: removes storybook addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {configure, addDecorator} from '@storybook/react';
-import {themes} from '@storybook/components';
-import {withOptions, setOptions} from '@storybook/addon-options';
-import {withInfo} from '@storybook/addon-info';
-import {checkA11y} from '@storybook/addon-a11y';
-import {withKnobs} from '@storybook/addon-knobs';
+
 import {Provider as StyletronProvider} from 'styletron-react';
 import {Client as Styletron} from 'styletron-engine-atomic';
+
+import {ThemeProvider} from '../src/styles';
+import {LightTheme} from '../src/themes';
 
 // automatically import all files ending in *.stories.js
 const req = require.context(
@@ -19,33 +18,8 @@ export default function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
-import {ThemeProvider} from '../src/styles';
-import {LightTheme} from '../src/themes';
-
-withOptions({
-  name: 'baseui',
-  url: 'https://github.com/uber-web/baseui',
-});
-
-setOptions({
-  theme: {
-    ...themes.normal,
-    highlightColor: LightTheme.colors.primary,
-    successColor: LightTheme.colors.positive,
-    warningColor: LightTheme.colors.warning,
-    failColor: LightTheme.colors.negative,
-  },
-});
-
-const engine = new Styletron();
-
-// this should be first decorator to avoid extra code to be parsed here
-addDecorator(withInfo);
-
-addDecorator(withKnobs);
-addDecorator(checkA11y);
-
 // Add providers for theme and styletron
+const engine = new Styletron();
 addDecorator(story => {
   return (
     <StyletronProvider value={engine}>
