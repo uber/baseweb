64e5b4ff352056a87442b576aad2850387ecd536
chasestarr <starr@uber.com>
2019-02-12 08:44:25 -0800

    chore(slider): moves to scenarios

---
+++ src/slider/__tests__/slider-step.scenario.js
@@ -0,0 +1,24 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+/* eslint-disable react/display-name*/
+
+import React from 'react';
+
+import {StatefulSlider} from '../index.js';
+
+export const name = 'slider-step';
+
+export const component = () => (
+  <div
+    style={{
+      maxWidth: '500px',
+    }}
+  >
+    <StatefulSlider initialState={{value: [0]}} step={5} min={-300} max={300} />
+  </div>
+);
