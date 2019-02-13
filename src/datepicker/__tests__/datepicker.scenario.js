980d72d13060d796c76e15bcbe536c9a58ce0d12
Nadiia <nadiia.dmytrenko@gmail.com>
2019-02-13 15:58:11 -0800

    fix(datepicker): add e2e tests (#880)

---
+++ src/datepicker/__tests__/datepicker.scenario.js
@@ -0,0 +1,20 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+
+import * as React from 'react';
+
+import {Unstable_StatefulDatepicker as StatefulDatepicker} from '../index.js';
+
+export const name = 'datepicker';
+
+export const component = () => (
+  <StatefulDatepicker
+    aria-label="Select a date"
+    highlightedDate={new Date('March 10, 2019')}
+  />
+);
