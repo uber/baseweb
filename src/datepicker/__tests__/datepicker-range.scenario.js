d72f9dbfb6f73e99fe703a2511af63ff09aa1942
Nadiia Dmytrenko <nadiia@uber.com>
2019-02-13 11:05:05 -0800

    fix(datepicker): add e2e tests

---
+++ src/datepicker/__tests__/datepicker-range.scenario.js
@@ -0,0 +1,22 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+
+import * as React from 'react';
+
+import {Unstable_StatefulDatepicker as StatefulDatepicker} from '../index.js';
+
+export const name = 'datepicker-range';
+
+export const component = () => (
+  <StatefulDatepicker
+    aria-label="Select a date"
+    initialState={{value: []}}
+    isRange
+    highlightedDate={new Date('March 10, 2019')}
+  />
+);
