90acaf84db7f4a0a28578e356559f6b91c7b5077
Chase Starr <chasestarr@gmail.com>
2019-02-13 15:51:09 -0800

    test(slider): e2e a11y test (#881)
    * test(slider): e2e a11y test
    
    * fix: lint

---
+++ src/slider/__tests__/slider.e2e.js
@@ -0,0 +1,20 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+/* eslint-env node */
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+const {mount, analyzeAccessibility} = require('../../../e2e/helpers');
+
+// most slider actions are covered in react-range e2e tests: https://github.com/tajo/react-range/tree/master/e2e
+describe('slider', () => {
+  it('passes basic a11y tests', async () => {
+    await mount(page, 'slider');
+    const accessibilityReport = await analyzeAccessibility(page);
+    expect(accessibilityReport).toHaveNoAccessibilityIssues();
+  });
+});
