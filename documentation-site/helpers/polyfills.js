3099c6caec0cd26c1562edef17a86e9d0f60b047
chasestarr <starr@uber.com>
2019-02-20 15:24:05 -0800

    fix: removes console statement

--- documentation-site/helpers/polyfills.js
+++ documentation-site/helpers/polyfills.js
@@ -4,10 +4,6 @@ import includes from 'core-js/library/fn/string/virtual/includes';
 import repeat from 'core-js/library/fn/string/virtual/repeat';
 import assign from 'core-js/library/fn/object/assign';
 
-// Add your polyfills
-// This files runs at the very beginning (even before React and Next.js core)
-console.log('Load your polyfills');
-
 String.prototype.includes = includes;
 String.prototype.repeat = repeat;
 Object.assign = assign;
