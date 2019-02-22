e2c4c62730bbb37c332184a9e7c5a157935dc670
Chase Starr <chasestarr@gmail.com>
2019-02-22 09:38:56 -0800

    feat(table): adds filter functionality (#840)
    * feat(table): adds filter functionality
    
    * fix: updates check labels to medium weight
    
    * fix: moves type out of file, improves readability
    
    * test: updates snaps, creates new snaps for filter components
    
    * test(table-filter): adds unit test
    
    * fix(table-filter): ensure click does not open when disabled
    
    * docs(table): adds comments to type fields
    
    * fix(table-filter): fix active filters bug in example
    
    * fix(table-filter): enables keyboard nav to filter for a11y
    
    * feat(table-filter): enables overrides
    
    * docs(table-filter): adds doc site example
    
    * fix: disables events for the filter popover
    
    * fix: ignore flow error in scenario
    
    * feat: trap tabbing in filter popover
    
    * fix: updates to latest styles in figma
    
    * fix: update snap
    
    * fix(table): filter bugs were all from my table helper

---
+++ src/table/__tests__/filter.test.js
@@ -0,0 +1,65 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+
+import React from 'react';
+import {mount} from 'enzyme';
+
+import {Button} from '../../button/index.js';
+import {Filter, StyledFilterButton, StyledFilterContent} from '../index.js';
+
+describe('Table-Filter', () => {
+  it('does not display filter content by default', () => {
+    const wrapper = mount(<Filter>hello</Filter>);
+    const content = wrapper.find(StyledFilterContent);
+    expect(content).not.toExist();
+  });
+
+  it('displays filter content on click', () => {
+    const children = 'hello';
+    const wrapper = mount(<Filter>{children}</Filter>);
+    const icon = wrapper.find(StyledFilterButton);
+    icon.simulate('click');
+
+    const content = wrapper.find(StyledFilterContent);
+    expect(content).toExist();
+    expect(content.text()).toBe(children);
+  });
+
+  it('does not display filter content if disabled', () => {
+    const wrapper = mount(<Filter disabled>hello</Filter>);
+    const icon = wrapper.find(StyledFilterButton);
+    icon.simulate('click');
+
+    const content = wrapper.find(StyledFilterContent);
+    expect(content).not.toExist();
+  });
+
+  it('calls provided onSelectAll handler', () => {
+    const spy = jest.fn();
+    const wrapper = mount(<Filter onSelectAll={spy}>hello</Filter>);
+    const icon = wrapper.find(StyledFilterButton);
+    icon.simulate('click');
+
+    const button = wrapper.find(Button).at(0);
+    button.simulate('click');
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('calls provided onReset handler', () => {
+    const spy = jest.fn();
+    const wrapper = mount(<Filter onReset={spy}>hello</Filter>);
+    const icon = wrapper.find(StyledFilterButton);
+    icon.simulate('click');
+
+    const button = wrapper.find(Button).at(1);
+    button.simulate('click');
+
+    expect(spy).toHaveBeenCalled();
+  });
+});
