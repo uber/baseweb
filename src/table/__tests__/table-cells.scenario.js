2a3017ff241d00c8364fb9947c561ae58c87b7ba
Chase Starr <chasestarr@gmail.com>
2019-02-25 14:00:10 -0800

    table(feat): adds large text cell (#948)
    * feat(table): adds heading cell example
    
    * fix(table): scroll bar should not affect layout
    
    * feat(table): adds delta cell example
    
    * fix(table): fixed issue where the table body was scrolling a bit
    
    * feat(table): adds large text cell example
    
    * fix(table): update snaps

--- src/table/__tests__/table-cells.scenario.js
+++ src/table/__tests__/table-cells.scenario.js
@@ -8,6 +8,10 @@ LICENSE file in the root directory of this source tree.
 
 import React from 'react';
 
+import {styled} from '../../styles/index.js';
+import {Block} from '../../block/index.js';
+import ArrowDown from '../../icon/arrow-down.js';
+import ArrowUp from '../../icon/arrow-up.js';
 import Search from '../../icon/search.js';
 import Plus from '../../icon/plus.js';
 import Delete from '../../icon/delete.js';
@@ -25,43 +29,83 @@ import {
 
 export const name = 'table-cells';
 
+const StyledHeadingCell = styled(StyledCell, {paddingTop: 0, paddingBottom: 0});
+
+const StyledDeltaCell = styled(StyledCell, props => ({
+  ...props.$theme.typography.font500,
+  alignItems: 'center',
+  backgroundColor: props.$isNegative
+    ? props.$theme.colors.negative50
+    : props.$theme.colors.positive50,
+  color: props.$isNegative
+    ? props.$theme.colors.negative
+    : props.$theme.colors.positive,
+}));
+
+const StyledLargeText = styled(StyledCell, {
+  alignItems: 'center',
+});
+
 const DATA = [
-  ['Marlyn', 10],
-  ['Luther', 15],
-  ['Kiera', 13],
-  ['Edna', 20],
-  ['Soraya', 18],
-  ['Dorris', 32],
-  ['Astrid', 26],
-  ['Wendie', 17],
-  ['Marna', 11],
-  ['Malka', 14],
-  ['Jospeh', 10],
-  ['Roselee', 12],
-  ['Justine', 35],
-  ['Marlon', 30],
-  ['Mellissa', 32],
-  ['Fausto', 21],
-  ['Alfredia', 22],
-  ['Abel', 18],
-  ['Winford', 19],
-  ['Neil', 27],
+  ['Marlyn', 'Engineering', 'San Francisco', -100, 1234.5],
+  ['Luther', 'Marketing', 'Seattle', 50, 2435.2],
+  ['Kiera', 'Operations', 'Los Angeles', 40, 8348.1],
+  ['Edna', 'Design', 'Atlanta', 700, 2893.4],
+  ['Soraya', 'Finance', 'Denver', 99, 8787.3],
+  ['Dorris', 'Legal', 'Dallas', -20, 6325.2],
+  ['Astrid', 'Product', 'Tempe', 0, 7392.7],
+  ['Wendie', 'Engineering', 'Pittsburgh', -15, 9283.1],
+  ['Marna', 'Marketing', 'Indianapolis', -2, 7720.9],
+  ['Malka', 'Operations', 'New Orleans', 30, 6273.3],
+  ['Jospeh', 'Design', 'New York City', -22, 8837.4],
+  ['Roselee', 'Finance', 'Oakland', 4, 9277.9],
+  ['Justine', 'Legal', 'Louisville', -9, 7737.2],
+  ['Marlon', 'Engineering', 'Baltimore', -2, 2330.3],
+  ['Mellissa', 'Marketing', 'Boulder', -30, 4458.8],
+  ['Fausto', 'Operations', 'Chicago', -10, 6363.9],
+  ['Alfredia', 'Design', 'Grand Rapids', 70, 2235.2],
+  ['Abel', 'Finance', 'Nashville', 30, 9882.3],
+  ['Winford', 'Legal', 'Sacramento', 10, 8774.7],
+  ['Neil', 'Product', 'Columbus', -5, 2673.2],
 ];
 
 export const component = () => (
-  <div style={{height: '500px', width: '400px'}}>
+  <div style={{height: '500px', width: '900px'}}>
     <StyledTable>
       <StyledHead>
         <StyledHeadCell>Name</StyledHeadCell>
-        <StyledHeadCell>Age</StyledHeadCell>
+        <StyledHeadCell>Role</StyledHeadCell>
+        <StyledHeadCell>Delta</StyledHeadCell>
+        <StyledHeadCell>Amount</StyledHeadCell>
         <StyledHeadCell>Actions</StyledHeadCell>
       </StyledHead>
       <StyledBody>
         {DATA.map((row, index) => (
           <StyledRow key={index}>
-            {row.map((cell, cellIndex) => (
-              <StyledCell key={cellIndex}>{cell}</StyledCell>
-            ))}
+            <StyledCell>{row[0]}</StyledCell>
+
+            <StyledHeadingCell>
+              <Block>
+                <Block font="font200" color="mono600">
+                  {row[2]}
+                </Block>
+                <Block font="font300">{row[1]}</Block>
+              </Block>
+            </StyledHeadingCell>
+
+            <StyledDeltaCell $isNegative={row[3] < 0}>
+              <>
+                {row[3] < 0 ? <ArrowDown size={24} /> : <ArrowUp size={24} />}
+                {row[3]}%
+              </>
+            </StyledDeltaCell>
+
+            <StyledLargeText>
+              <Block font="font500">{row[4]}</Block>
+              <Block color="mono700" font="font250" paddingLeft="scale200">
+                +1000%
+              </Block>
+            </StyledLargeText>
 
             <StyledCell>
               <StyledAction>
