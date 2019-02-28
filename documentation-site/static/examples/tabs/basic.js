56bc4f723ee63fccdc9608d3e622817c23587422
Vojtech Miksu <vojtech@miksu.cz>
2019-02-28 11:38:28 -0800

    docs: fix API doc headings and StatefulTab examples (#967)
    * docs: fix API docs headings
    
    * docs: fix StatefulTab examples

--- documentation-site/static/examples/tabs/basic.js
+++ documentation-site/static/examples/tabs/basic.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {StatefulTabs, TabPanel} from 'baseui/tabs';
 
 export default () => (
-  <StatefulTabs activeKey="1">
+  <StatefulTabs initialState={{activeKey: '0'}}>
     <TabPanel title="Tab Link 1">Tab 1 content</TabPanel>
     <TabPanel title="Tab Link 2">Tab 2 content</TabPanel>
     <TabPanel title="Tab Link 3">Tab 3 content</TabPanel>
