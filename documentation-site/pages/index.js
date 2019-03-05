9cad133083076fb507f12ad7d65b51b86f9030bd
Gergely Nemeth <gergelyke@users.noreply.github.com>
2019-03-05 10:41:44 -0800

    chore(project): rebrand to use base-web (#982)

--- documentation-site/pages/index.js
+++ documentation-site/pages/index.js
@@ -37,22 +37,22 @@ const cardOverrides = {
 
 const Index = (props: {contributors: Contributor[]}) => (
   <Layout>
-    <H4>Base UI React Components</H4>
+    <H4>Base Web React Components</H4>
     <Markdown.p>
-      Base UI is a foundation, a basis for initiating, evolving, and unifying
+      Base Web is a foundation, a basis for initiating, evolving, and unifying
       web products. The system is designed to be fully responsive and device
       agnostic providing designers and developers with a unique catalog of
       components.
     </Markdown.p>
 
-    <H6>Getting started with Base UI</H6>
+    <H6>Getting started with Base Web</H6>
 
     <Block display="flex" flexWrap>
-      <Card title="Installing Base UI" overrides={cardOverrides}>
+      <Card title="Installing Base Web" overrides={cardOverrides}>
         <StyledBody>
-          Base UI is distributed as an npm package. As Base UI is built on top
+          Base Web is distributed as an npm package. As Base Web is built on top
           of a CSS-in-JS engine, all you need is the dependencies from npm. Let
-          {"'"}s add Base UI to your project!
+          {"'"}s add Base to your project!
         </StyledBody>
         <StyledAction>
           <Block
@@ -60,17 +60,17 @@ const Index = (props: {contributors: Contributor[]}) => (
             href="/getting-started/installation"
             $style={{textDecoration: 'none'}}
           >
-            <Button style={{width: '100%'}}>Install Base UI</Button>
+            <Button style={{width: '100%'}}>Install Base Web</Button>
           </Block>
         </StyledAction>
       </Card>
 
-      <Card title="Learning Base UI" overrides={cardOverrides}>
+      <Card title="Learning Base Web" overrides={cardOverrides}>
         <StyledBody>
-          Probably the best way to learn Base UI is by start building an
+          Probably the best way to learn Base Web is by start building an
           application using it. On this page, you
           {"'"}
-          ll find a simple and a more complex app built using Base UI.
+          ll find a simple and a more complex app built using Base Web.
         </StyledBody>
         <StyledAction>
           <Block
