ba4e19309c31acef710723d8d10d8b7653cd3cbc
Vojtech Miksu <vojtech@uber.com>
2019-03-07 15:07:19 -0800

    docs: responsive code examples

--- documentation-site/components/json.js
+++ documentation-site/components/json.js
@@ -7,13 +7,25 @@ LICENSE file in the root directory of this source tree.
 // @flow
 import * as React from 'react';
 import Code from './code.js';
+import {Block} from 'baseui/block';
 
 type PropsT = {
   src: string,
 };
 
 const JSONViewer = (props: PropsT) => (
-  <Code language="javascript">{JSON.stringify(props.src, null, 2)}</Code>
+  <Block
+    paddingLeft="scale600"
+    overrides={{
+      Block: {
+        style: {
+          borderLeft: '5px solid #f6ba8b',
+        },
+      },
+    }}
+  >
+    <Code language="javascript">{JSON.stringify(props.src, null, 2)}</Code>
+  </Block>
 );
 
 export default JSONViewer;
