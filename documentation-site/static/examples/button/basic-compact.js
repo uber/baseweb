f225316f0dedd61e6604b4e46935cfd8370af866
Gergely Nemeth <gergelyke@users.noreply.github.com>
2019-03-07 15:34:42 -0800

    docs(button): simplify and add spacing (#998)

--- documentation-site/static/examples/button/basic-compact.js
+++ documentation-site/static/examples/button/basic-compact.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Button, SIZE} from 'baseui/button';
+import {Block} from 'baseui/block';
 
 export default () => (
   <React.Fragment>
     <Button size={SIZE.default}>Default size</Button>
+    <Block marginBottom="scale300" />
     <Button size={SIZE.compact}>Compact size</Button>
   </React.Fragment>
 );
