09a3748eec60d07fce211cf3e392d1c229b9462c
chasestarr <starr@uber.com>
2019-03-21 16:10:51 -0700

    fix(tabs): ensures proper rendering with no children

---
+++ src/tabs/__tests__/tabs-controlled.scenario.js
@@ -0,0 +1,44 @@
+/*
+Copyright (c) 2018 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+
+import * as React from 'react';
+
+import {Tabs, Tab} from '../index.js';
+
+export const name = 'tabs-controlled';
+
+type StateT = {
+  activeKey: string,
+};
+
+class Example extends React.Component<{}, StateT> {
+  state = {
+    activeKey: '0',
+  };
+
+  onChange = (params: {activeKey: React.Key}) => {
+    this.setState({activeKey: String(params.activeKey)});
+  };
+
+  render() {
+    const content = ['Tab Content 1', 'Tab Content 2', 'Tab Content 3'];
+    return (
+      <React.Fragment>
+        <Tabs activeKey={this.state.activeKey} onChange={this.onChange}>
+          <Tab title="Tab Link 1" />
+          <Tab title="Tab Link 2" />
+          <Tab title="Tab Link 3" />
+        </Tabs>
+        <div>Content:</div>
+        <div>{content[Number(this.state.activeKey)]}</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export const component = () => <Example />;
