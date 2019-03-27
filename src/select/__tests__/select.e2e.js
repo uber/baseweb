435ae8e035c3b4b21e71a9252af0a19b36683ed6
Vojtech Miksu <vojtech@uber.com>
2019-03-27 10:47:50 -0700

    fix(select): add e2e test for creatable multi

--- src/select/__tests__/select.e2e.js
+++ src/select/__tests__/select.e2e.js
@@ -123,4 +123,34 @@ describe('select', () => {
     );
     expect(inputValue).toBe('Paris');
   });
+
+  it('creates multiple options', async () => {
+    await mount(page, 'select-creatable-multi');
+    await page.waitFor(selectors.selectInput);
+    await page.click(selectors.selectInput);
+    await page.waitFor(selectors.selectDropDown);
+
+    // add "Paris"
+    await page.keyboard.type('Paris');
+    await page.click(optionAtPosition(1));
+    const inputValue = await page.$eval(
+      selectors.selectedList,
+      select => select.innerText,
+    );
+    expect(inputValue).toBe('Paris');
+
+    // add "London"
+    await page.keyboard.type('London');
+    await page.click(optionAtPosition(1));
+    const inputValue2 = await page.$eval(
+      selectors.selectedList,
+      select => select.innerText,
+    );
+    expect(inputValue2).toBe('Paris\nLondon');
+
+    // add "Paris" again, option to create should not be provided
+    await page.keyboard.type('Paris');
+    const canBeParisCreated = (await page.$(optionAtPosition(1))) !== null;
+    expect(canBeParisCreated).toBeFalsy();
+  });
 });
