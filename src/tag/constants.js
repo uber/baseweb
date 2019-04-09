621ef7b9d5fb43af79ca32afd5998f3c32ceb3df
Chase Starr <chasestarr@gmail.com>
2019-04-09 15:36:57 -0700

    feat(tag): dark theme (#1140)
    * feat(tag): installs polished package
    
    * feat(tag): enables tag dark theme, reworks color logic
    
    * fix(tag): lint

--- src/tag/constants.js
+++ src/tag/constants.js
@@ -8,6 +8,7 @@ LICENSE file in the root directory of this source tree.
 
 import type {TagKindT} from './types.js';
 
+// Remove this in the next major version
 export const COLOR_STYLE_KEYS = {
   neutral: 'tagNeutralBackground',
   primary: 'tagPrimaryBackground',
@@ -17,9 +18,9 @@ export const COLOR_STYLE_KEYS = {
 };
 
 export const VARIANT = Object.freeze({
+  solid: 'solid',
   light: 'light',
   outlined: 'outlined',
-  solid: 'solid',
 });
 
 export const KIND: {[TagKindT]: string} = Object.keys({
