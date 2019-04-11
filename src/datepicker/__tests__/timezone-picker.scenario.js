5e66239a24203c6b50b9b7c2dee461820e509dfe
chasestarr <starr@uber.com>
2019-04-11 14:49:05 -0700

    fix(timezone-picker): clean up scenario file

--- src/datepicker/__tests__/timezone-picker.scenario.js
+++ src/datepicker/__tests__/timezone-picker.scenario.js
@@ -32,7 +32,7 @@ class Controlled extends React.Component<any, any> {
 export const component = () => (
   <Block width="400px">
     <FormControl label="daylight savings time">
-      <TimezonePicker date={new Date(2019, 3, 1)} onChange={console.log} />
+      <TimezonePicker date={new Date(2019, 3, 1)} />
     </FormControl>
     <FormControl label="standard time">
       <TimezonePicker date={new Date(2019, 2, 1)} />
