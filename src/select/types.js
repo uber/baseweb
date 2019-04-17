edb7055dafdd745a63e17d75cf8d975c284ebdb2
igorwilbert <42987854+IgorWilbert@users.noreply.github.com>
2019-04-17 15:14:20 -0300

    feat(Menu/Select): add onHighlightedItemChanged handler  (#1138)
    * feat(highlighted item listener): first attempt (#994)
    
    * Overrides menu throught select
    
    * Overrides menu with default props
    
    * Adds example to documentation website
    
    * Scenario file test
    
    * Fixes console errors

--- src/select/types.js
+++ src/select/types.js
@@ -52,6 +52,7 @@ export type OverridesT = {
   DropdownOption?: OverrideT<*>,
   DropdownListItem?: OverrideT<*>,
   OptionContent?: OverrideT<*>,
+  StatefulMenu?: OverrideT<*>,
 };
 
 export type OverridesDropdownT = {
@@ -61,6 +62,7 @@ export type OverridesDropdownT = {
   DropdownOption?: OverrideT<*>,
   DropdownListItem?: OverrideT<*>,
   OptionContent?: OverrideT<*>,
+  StatefulMenu?: OverrideT<*>,
 };
 
 export type PropsT = {
