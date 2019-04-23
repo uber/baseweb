ed160d775eec8d15f14e025d53248bb1876d268f
Chase Starr <chasestarr@gmail.com>
2019-04-23 09:09:23 -0700

    feat(timezone-picker): Timezone Picker (#1156)
    * feat(timezone-picker): adds timezone-support pkg
    
    * feat(timezone-picker): adds component
    
    * feat(timezone-picker): adds type descriptions
    
    * feat(timezone-picker): clarify in-line comments
    
    * fix(timezone-picker): clean up scenario file
    
    * feat(timezone-picker): add example to docs site
    
    * feat(timezone-picker): adds tests
    
    * feat(timezone-picker): adds optional label transformation
    
    * Update src/datepicker/types.js
    
    * feat(timezone-picker): updates options if date changes
    
    * fix(timezone-picker): adds delay to e2e test
    
    * fix(timezone-picker): removes flakey e2e test
    
    * fix(timepicker): removes flakey test

--- src/datepicker/types.js
+++ src/datepicker/types.js
@@ -11,6 +11,8 @@ import type {ThemeT} from '../styles/types.js';
 import type {OverrideT} from '../helpers/overrides.js';
 import {STATE_CHANGE_TYPE} from './constants.js';
 
+import type {OnChangeParamsT, OptionT, ValueT} from '../select/index.js';
+
 // eslint-disable-next-line flowtype/no-weak-types
 type LocaleT = any; // see https://github.com/date-fns/date-fns/blob/master/src/locale/index.js.flow
 
@@ -252,3 +254,33 @@ export type StatefulDatepickerPropsT<T> = $Diff<
     children: T => React.Node,
   },
 >;
+
+export type TimezonePickerStateT = {
+  /** List of timezones from the IANA database. */
+  timezones: OptionT[],
+  /** Internal value provided to the select component. */
+  value: ?ValueT,
+};
+export type TimezonePickerPropsT = {
+  /**
+   * If not provided, defaults to new Date(). Important to note that the timezone picker only
+   * displays options related to the provided date. Take Pacific Time for example. On March 9th,
+   * Pacific Time equates to the more specific Pacific Standard Time. On March 10th, it operates on
+   * Pacific Daylight Time. The timezone picker will never display PST and PDT together. If you need
+   * exact specificity, provide a date. Otherwise it will default to the relevant timezone at render.
+   */
+  date?: Date,
+  /**
+   * Customize the option's label. Useful for translations and optionally mapping from
+   * 'America/Los_Angeles' to 'Pacific Time'.
+   */
+  mapLabels?: OptionT => React.Node,
+  /** Callback for when the timezone selection changes. Follows same pattern as Select component. */
+  onChange?: (params: OnChangeParamsT) => mixed,
+  overrides?: {Select?: OverrideT<*>},
+  /**
+   * Optional value that can be provided to fully control the component. If not provided,
+   * TimezonePicker will manage state internally.
+   */
+  value?: ValueT,
+};
