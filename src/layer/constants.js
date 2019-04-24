d828769a3cf482618e2541505de708470f4d13bc
Nadiia <nadiia.dmytrenko@gmail.com>
2019-04-24 14:00:16 -0700

    feat(layer): initial implementation of the TetherBehavior  (Part 2) (#1148)

--- src/layer/constants.js
+++ src/layer/constants.js
@@ -5,7 +5,7 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 // @flow
-export const PLACEMENT = {
+export const TETHER_PLACEMENT = {
   auto: 'auto',
   topLeft: 'topLeft',
   top: 'top',
--- src/layer/index.js
+++ src/layer/index.js
@@ -7,3 +7,6 @@ LICENSE file in the root directory of this source tree.
 // @flow
 export {default as LayersManager} from './layers-manager.js';
 export {default as Layer} from './layer.js';
+export {default as TetherBehavior} from './tether.js';
+export {TETHER_PLACEMENT} from './constants.js';
+export * from './types.js';
