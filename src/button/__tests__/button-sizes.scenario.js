9f0ab5863d47fd7f1cb23b3716f4fe681234e80a
Graham Murdoch <grahammurdoch8@gmail.com>
2019-04-29 15:55:41 -0700

    fix: lint error

--- src/button/__tests__/button-sizes.scenario.js
+++ src/button/__tests__/button-sizes.scenario.js
@@ -9,7 +9,6 @@ LICENSE file in the root directory of this source tree.
 import React from 'react';
 
 import {Button} from '../index.js';
-import ArrowRight from '../../icon/arrow-right.js';
 import {SIZE} from '../constants.js';
 
 export const name = 'button-sizes';
