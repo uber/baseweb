50ad8916911c653ce4cce3fd277a33fc4c54e64c
Nadiia Dmytrenko <nadiia@uber.com>
2019-05-09 10:50:02 -0700

    fix(tether): print out a warning message if an anchor ref is not set correctly

--- src/layer/tether.js
+++ src/layer/tether.js
@@ -43,7 +43,14 @@ class Tether extends React.Component<TetherPropsT, TetherStateT> {
         this.popper && this.popper.scheduleUpdate();
       }
       if (this.state.isMounted !== prevState.isMounted) {
-        this.initializePopper();
+        if (!this.props.anchorRef) {
+          // eslint-disable-next-line no-console
+          console.warn(
+            `[baseui][TetherBehavior] ref has not been passed to the Popper's anchor element`,
+          );
+        } else {
+          this.initializePopper();
+        }
       }
     }
   }
