53ccd4d5b2bd4080f5d8b41e3296b65d474e3eff
David Su <david.su@uber.com>
2019-05-17 17:20:33 -0700

    fix: fix header

--- src/tabs/__tests__/tabs-one-child.scenario.js
+++ src/tabs/__tests__/tabs-one-child.scenario.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2018 Uber Technologies, Inc.
+Copyright (c) 2018-2019 Uber Technologies, Inc.
 
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
@@ -10,7 +10,7 @@ import React from 'react';
 
 import {StatefulTabs, Tab} from '../index.js';
 
-export const name = 'tabs';
+export const name = 'tabs-one-child';
 
 export const component = () => (
   <StatefulTabs>
