15cf566e1b29870e230dc2975b4b48b8ceff9fb4
chasestarr <starr@uber.com>
2019-05-21 13:39:46 -0700

    fix(tests): lint

--- src/header-navigation/styled-components.js
+++ src/header-navigation/styled-components.js
@@ -5,7 +5,6 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 // @flow
-import * as React from 'react';
 import {styled} from '../styles/index.js';
 import {ALIGN} from './index.js';
 
