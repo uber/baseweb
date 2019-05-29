fd3f4fe83aa0359e3de13dacf2ed30d0090a6683
Derek Guenther <derek.guenther@uber.com>
2019-05-29 11:41:26 -0700

    feat(timepicker): add prop to allow manual entry of times (#1322)

--- src/datepicker/__tests__/time-picker.e2e.js
+++ src/datepicker/__tests__/time-picker.e2e.js
@@ -13,8 +13,11 @@ const {mount, analyzeAccessibility} = require('../../../e2e/helpers');
 const selectors = {
   twelveHour: '[data-e2e="12-hour"]',
   twentyFourHour: '[data-e2e="24-hour"]',
+  twelveHourCreatable: '[data-e2e="12-hour-creatable"]',
+  twentyFourHourCreatable: '[data-e2e="24-hour-creatable"]',
   input: 'input[role="combobox"]',
   dropdown: '[role="listbox"]',
+  option: '[role="option"]',
   value: '[data-id="selected"]',
 };
 
@@ -62,5 +65,122 @@ describe('TimePicker', () => {
     );
 
     expect(value).toBe('02:00');
+  });
+
+  it('renders a date that is not one of the steps', async () => {
+    await mount(page, 'time-picker');
+    await page.waitFor(selectors.twelveHourCreatable);
+    await page.waitFor(selectors.twentyFourHourCreatable);
+
+    const twelveHourValue = await page.$eval(
+      `${selectors.twelveHourCreatable} ${selectors.value}`,
+      select => select.textContent,
+    );
+
+    const twentyFourHourValue = await page.$eval(
+      `${selectors.twentyFourHourCreatable} ${selectors.value}`,
+      select => select.textContent,
+    );
+
+    expect(twelveHourValue).toBe('1:11 AM');
+    expect(twentyFourHourValue).toBe('01:11');
+  });
+
+  describe('creatable', () => {
+    it('shows both AM and PM options when a 12-hour time without meridiem is entered', async () => {
+      await mount(page, 'time-picker');
+      await page.waitFor(selectors.twelveHourCreatable);
+      await page.click(`${selectors.twelveHourCreatable} ${selectors.input}`);
+      await page.waitFor(selectors.dropdown);
+      await page.keyboard.type('3:33');
+
+      const options = await page.$$(`${selectors.option}`);
+
+      expect(options.length).toBe(2);
+      const option1 = await page.evaluate(e => e.textContent, options[0]);
+      const option2 = await page.evaluate(e => e.textContent, options[1]);
+      expect(option1).toBe('3:33 AM');
+      expect(option2).toBe('3:33 PM');
+    });
+
+    it('shows AM option when a 12-hour time with partial meridiem is entered', async () => {
+      await mount(page, 'time-picker');
+      await page.waitFor(selectors.twelveHourCreatable);
+      await page.click(`${selectors.twelveHourCreatable} ${selectors.input}`);
+      await page.waitFor(selectors.dropdown);
+      await page.keyboard.type('3:33a');
+
+      const options = await page.$$(`${selectors.option}`);
+
+      expect(options.length).toBe(1);
+      const option1 = await page.evaluate(e => e.textContent, options[0]);
+      expect(option1).toBe('3:33 AM');
+    });
+
+    it('generates the correct seconds for 12PM', async () => {
+      await mount(page, 'time-picker');
+      await page.waitFor(selectors.twelveHourCreatable);
+      await page.click(`${selectors.twelveHourCreatable} ${selectors.input}`);
+      await page.waitFor(selectors.dropdown);
+      await page.keyboard.type('12:11pm');
+      await page.keyboard.press('ArrowDown');
+      await page.keyboard.press('Enter');
+
+      const value = await page.$eval(
+        `${selectors.twelveHourCreatable} ${selectors.value}`,
+        select => select.textContent,
+      );
+
+      expect(value).toBe('12:11 PM');
+    });
+
+    it('generates the correct seconds for 12AM', async () => {
+      await mount(page, 'time-picker');
+      await page.waitFor(selectors.twelveHourCreatable);
+      await page.click(`${selectors.twelveHourCreatable} ${selectors.input}`);
+      await page.waitFor(selectors.dropdown);
+      await page.keyboard.type('12:22am');
+      await page.keyboard.press('ArrowDown');
+      await page.keyboard.press('Enter');
+
+      const value = await page.$eval(
+        `${selectors.twelveHourCreatable} ${selectors.value}`,
+        select => select.textContent,
+      );
+
+      expect(value).toBe('12:22 AM');
+    });
+
+    it('shows an option when a 24 hour time without leading zero is entered', async () => {
+      await mount(page, 'time-picker');
+      await page.waitFor(selectors.twentyFourHourCreatable);
+      await page.click(
+        `${selectors.twentyFourHourCreatable} ${selectors.input}`,
+      );
+      await page.waitFor(selectors.dropdown);
+      await page.keyboard.type('3:33');
+
+      const options = await page.$$(`${selectors.option}`);
+
+      expect(options.length).toBe(1);
+      const option1 = await page.evaluate(e => e.textContent, options[0]);
+      expect(option1).toBe('03:33');
+    });
+
+    it('shows only one option when a time is entered that matches an existing option', async () => {
+      await mount(page, 'time-picker');
+      await page.waitFor(selectors.twentyFourHourCreatable);
+      await page.click(
+        `${selectors.twentyFourHourCreatable} ${selectors.input}`,
+      );
+      await page.waitFor(selectors.dropdown);
+      await page.keyboard.type('00:15');
+
+      const options = await page.$$(`${selectors.option}`);
+
+      expect(options.length).toBe(1);
+      const option1 = await page.evaluate(e => e.textContent, options[0]);
+      expect(option1).toBe('00:15');
+    });
   });
 });
