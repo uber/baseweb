d1e8af6448190b619b1e89eb4a9154159749cb0e
Chase Starr <starr@uber.com>
2019-06-04 12:36:58 -0700

    feat(form-control): positive option

---
+++ documentation-site/static/examples/form-control/kinds.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import {FormControl} from 'baseui/form-control';
+import {StatefulInput, SIZE} from 'baseui/input';
+import {StatefulTextarea} from 'baseui/textarea';
+import {StatefulCheckbox} from 'baseui/checkbox';
+import {StatefulRadioGroup, Radio} from 'baseui/radio';
+
+export default () => (
+  <React.Fragment>
+    <FormControl label="Input label" caption="Input caption">
+      <StatefulInput />
+    </FormControl>
+
+    <FormControl
+      positive="Positive caption"
+      label="Input label"
+      caption="Input caption"
+    >
+      <StatefulInput positive />
+    </FormControl>
+
+    <FormControl
+      error="Error caption"
+      label="Input label"
+      caption="Input caption"
+    >
+      <StatefulInput error />
+    </FormControl>
+  </React.Fragment>
+);
