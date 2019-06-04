901afd2aa51055fe288d58b2f33863814a4b7e80
Vojtech Miksu <vojtech@miksu.cz>
2019-06-04 15:27:22 -0700

    docs: add debug mode (#1267)

--- documentation-site/helpers/styletron.js
+++ documentation-site/helpers/styletron.js
@@ -9,6 +9,7 @@ LICENSE file in the root directory of this source tree.
 /* global document */
 
 import {Client, Server} from 'styletron-engine-atomic';
+import {DebugEngine} from 'styletron-react';
 
 export const isServer = typeof window === 'undefined';
 
@@ -17,3 +18,7 @@ const getHydrate = () => document.getElementsByClassName('_styletron_hydrate_');
 export const styletron = isServer
   ? new Server()
   : new Client({hydrate: getHydrate()});
+
+export const debug =
+  /*eslint-disable*/
+  process.env.NODE_ENV === 'production' ? void 0 : new DebugEngine();
