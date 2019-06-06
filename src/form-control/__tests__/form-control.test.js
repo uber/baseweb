901945fc9f7801eff078698628e6e9f8253cd8ad
Chase Starr <chasestarr@gmail.com>
2019-06-06 09:15:43 -0700

    feat(form-control): positive option (#1364)
    * feat(form-control): positive option
    
    * Update src/form-control/__tests__/form-control.test.js

--- src/form-control/__tests__/form-control.test.js
+++ src/form-control/__tests__/form-control.test.js
@@ -25,36 +25,41 @@ describe('FormControl - Label and Caption for controls', () => {
     const label = rendered.find(Label).first();
     expect(label).toExist();
     expect(label).toHaveText('Label test');
-    expect(label.props()).toMatchObject({
-      $disabled: false,
-      $error: false,
-      $required: false,
-      $size: 'default',
-      children: 'Label test',
-    });
+    expect(label.props()).toMatchInlineSnapshot(`
+Object {
+  "$disabled": false,
+  "$error": false,
+  "$positive": false,
+  "children": "Label test",
+  "data-baseweb": "form-control-label",
+  "htmlFor": undefined,
+}
+`);
     let caption = rendered.find(Caption).first();
     expect(caption).toExist();
     expect(caption).toHaveText('Caption test');
-    expect(caption.props()).toMatchObject({
-      $disabled: false,
-      $error: false,
-      $required: false,
-      $size: 'default',
-      children: 'Caption test',
-    });
+    expect(caption.props()).toMatchInlineSnapshot(`
+Object {
+  "$disabled": false,
+  "$error": false,
+  "$positive": false,
+  "children": "Caption test",
+}
+`);
 
     rendered.setProps({
       error: 'Error test',
     });
     caption = rendered.find(Caption).first();
     expect(caption).toHaveText('Error test');
-    expect(caption.props()).toMatchObject({
-      $disabled: false,
-      $error: 'Error test',
-      $required: false,
-      $size: 'default',
-      children: 'Error test',
-    });
+    expect(caption.props()).toMatchInlineSnapshot(`
+Object {
+  "$disabled": false,
+  "$error": true,
+  "$positive": false,
+  "children": "Error test",
+}
+`);
   });
 
   test('Accepts a node for label, caption, and error', () => {
@@ -77,37 +82,85 @@ describe('FormControl - Label and Caption for controls', () => {
     expect(caption).toHaveText('Error test');
   });
 
-  test('Accepts a function for label, caption, and error', () => {
-    const label = jest.fn().mockReturnValue(<span>Label test</span>);
-    const caption = jest.fn().mockReturnValue(<span>Caption test</span>);
+  test('Renders error if error and positive and caption are provided', () => {
     const error = jest.fn().mockReturnValue(<span>Error test</span>);
+    const positive = jest.fn().mockReturnValue(<span>Positive test</span>);
+    const caption = jest.fn().mockReturnValue(<span>Caption test</span>);
     const rendered = mount(
-      <FormControl label={label} caption={caption}>
+      <FormControl caption={caption} error={error} positive={positive}>
         <Input />
       </FormControl>,
     );
+    const errorRendered = rendered.find(Caption).first();
+    expect(errorRendered).toHaveText('Error test');
+  });
+
+  test('Accepts a function for label', () => {
+    const label = jest.fn().mockReturnValue(<span>Label test</span>);
     const sharedProps = {
       $disabled: false,
       $error: false,
-      $required: false,
-      $size: 'default',
+      $positive: false,
     };
+    const rendered = mount(
+      <FormControl label={label}>
+        <Input />
+      </FormControl>,
+    );
     const labelRendered = rendered.find(Label).first();
     expect(label).toHaveBeenCalledWith(sharedProps);
     expect(labelRendered).toHaveText('Label test');
+  });
+
+  test('Accepts a function for caption', () => {
+    const caption = jest.fn().mockReturnValue(<span>Caption test</span>);
+    const sharedProps = {
+      $disabled: false,
+      $error: false,
+      $positive: false,
+    };
+    const rendered = mount(
+      <FormControl caption={caption}>
+        <Input />
+      </FormControl>,
+    );
     const captionRendered = rendered.find(Caption).first();
     expect(caption).toHaveBeenCalledWith(sharedProps);
     expect(captionRendered).toHaveText('Caption test');
-    expect(error).not.toHaveBeenCalled();
+  });
 
-    rendered.setProps({
-      error: error,
-    });
-    expect(error).toHaveBeenCalledWith({
-      ...sharedProps,
-      $error: error,
-    });
-    expect(captionRendered).toHaveText('Error test');
+  test('Accepts a function for error', () => {
+    const error = jest.fn().mockReturnValue(<span>Error test</span>);
+    const sharedProps = {
+      $disabled: false,
+      $error: true,
+      $positive: false,
+    };
+    const rendered = mount(
+      <FormControl error={error}>
+        <Input />
+      </FormControl>,
+    );
+    const errorRendered = rendered.find(Caption).first();
+    expect(error).toHaveBeenCalledWith(sharedProps);
+    expect(errorRendered).toHaveText('Error test');
+  });
+
+  test('Accepts a function for positive', () => {
+    const positive = jest.fn().mockReturnValue(<span>Positive test</span>);
+    const sharedProps = {
+      $disabled: false,
+      $error: false,
+      $positive: true,
+    };
+    const rendered = mount(
+      <FormControl positive={positive}>
+        <Input />
+      </FormControl>,
+    );
+    const positiveRendered = rendered.find(Caption).first();
+    expect(positive).toHaveBeenCalledWith(sharedProps);
+    expect(positiveRendered).toHaveText('Positive test');
   });
 
   test('Renders caption if error is set on the control', () => {
@@ -118,13 +171,14 @@ describe('FormControl - Label and Caption for controls', () => {
     );
     const caption = rendered.find(Caption).first();
     expect(caption).toHaveText('Caption test');
-    expect(caption.props()).toMatchObject({
-      $disabled: false,
-      $error: true,
-      $required: false,
-      $size: 'default',
-      children: 'Caption test',
-    });
+    expect(caption.props()).toMatchInlineSnapshot(`
+Object {
+  "$disabled": false,
+  "$error": false,
+  "$positive": false,
+  "children": "Caption test",
+}
+`);
   });
 
   test('Passes correct props from the control to label and caption', () => {
@@ -134,38 +188,46 @@ describe('FormControl - Label and Caption for controls', () => {
       </FormControl>,
     );
     const label = rendered.find(Label).first();
-    expect(label.props()).toMatchObject({
-      $disabled: true,
-      $error: true,
-      $required: true,
-      $size: 'default',
-      children: 'Label test',
-    });
+    expect(label.props()).toMatchInlineSnapshot(`
+Object {
+  "$disabled": false,
+  "$error": false,
+  "$positive": false,
+  "children": "Label test",
+  "data-baseweb": "form-control-label",
+  "htmlFor": undefined,
+}
+`);
     const caption = rendered.find(Caption).first();
-    expect(caption.props()).toMatchObject({
-      $disabled: true,
-      $error: true,
-      $required: true,
-      $size: 'default',
-      children: 'Caption test',
-    });
+    expect(caption.props()).toMatchInlineSnapshot(`
+Object {
+  "$disabled": false,
+  "$error": false,
+  "$positive": false,
+  "children": "Caption test",
+}
+`);
     rendered.setProps({
       error: 'Error test',
     });
-    expect(label.props()).toMatchObject({
-      $disabled: true,
-      $error: true,
-      $required: true,
-      $size: 'default',
-      children: 'Label test',
-    });
-    expect(caption.props()).toMatchObject({
-      $disabled: true,
-      $error: true,
-      $required: true,
-      $size: 'default',
-      children: 'Caption test',
-    });
+    expect(label.props()).toMatchInlineSnapshot(`
+Object {
+  "$disabled": false,
+  "$error": false,
+  "$positive": false,
+  "children": "Label test",
+  "data-baseweb": "form-control-label",
+  "htmlFor": undefined,
+}
+`);
+    expect(caption.props()).toMatchInlineSnapshot(`
+Object {
+  "$disabled": false,
+  "$error": false,
+  "$positive": false,
+  "children": "Caption test",
+}
+`);
     expect(caption).toHaveText('Error test');
   });
 
@@ -177,22 +239,26 @@ describe('FormControl - Label and Caption for controls', () => {
     );
     const label = rendered.find(Label).first();
     expect(label).toExist();
-    expect(label.props()).toMatchObject({
-      $disabled: false,
-      $error: false,
-      $required: true,
-      $size: 'default',
-      children: 'Label test',
-    });
+    expect(label.props()).toMatchInlineSnapshot(`
+Object {
+  "$disabled": false,
+  "$error": false,
+  "$positive": false,
+  "children": "Label test",
+  "data-baseweb": "form-control-label",
+  "htmlFor": undefined,
+}
+`);
     const caption = rendered.find(Caption).first();
     expect(caption).toExist();
-    expect(caption.props()).toMatchObject({
-      $disabled: false,
-      $error: false,
-      $required: true,
-      $size: 'default',
-      children: 'Caption test',
-    });
+    expect(caption.props()).toMatchInlineSnapshot(`
+Object {
+  "$disabled": false,
+  "$error": false,
+  "$positive": false,
+  "children": "Caption test",
+}
+`);
   });
 });
 
@@ -204,18 +270,26 @@ test('Renders label and caption for the Checkbox component', () => {
   );
   const label = rendered.find(Label).first();
   expect(label).toExist();
-  expect(label.props()).toMatchObject({
-    $disabled: false,
-    $required: true,
-    children: 'Label test',
-  });
+  expect(label.props()).toMatchInlineSnapshot(`
+Object {
+  "$disabled": false,
+  "$error": false,
+  "$positive": false,
+  "children": "Label test",
+  "data-baseweb": "form-control-label",
+  "htmlFor": undefined,
+}
+`);
   const caption = rendered.find(Caption).first();
   expect(caption).toExist();
-  expect(caption.props()).toMatchObject({
-    $disabled: false,
-    $required: true,
-    children: 'Caption test',
-  });
+  expect(caption.props()).toMatchInlineSnapshot(`
+Object {
+  "$disabled": false,
+  "$error": false,
+  "$positive": false,
+  "children": "Caption test",
+}
+`);
 });
 
 test('Renders label and caption for the RadioGroup component', () => {
@@ -230,18 +304,26 @@ test('Renders label and caption for the RadioGroup component', () => {
   );
   const label = rendered.find(Label).first();
   expect(label).toExist();
-  expect(label.props()).toMatchObject({
-    $disabled: false,
-    $required: true,
-    children: 'Label test',
-  });
+  expect(label.props()).toMatchInlineSnapshot(`
+Object {
+  "$disabled": false,
+  "$error": false,
+  "$positive": false,
+  "children": "Label test",
+  "data-baseweb": "form-control-label",
+  "htmlFor": undefined,
+}
+`);
   const caption = rendered.find(Caption).first();
   expect(caption).toExist();
-  expect(caption.props()).toMatchObject({
-    $disabled: false,
-    $required: true,
-    children: 'Caption test',
-  });
+  expect(caption.props()).toMatchInlineSnapshot(`
+Object {
+  "$disabled": false,
+  "$error": false,
+  "$positive": false,
+  "children": "Caption test",
+}
+`);
 });
 
 describe('FormControl - overrides', () => {
