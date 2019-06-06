e4e8387e28ad5cc0549557ce4f9ab5b5b5e0989e
Graham Murdoch <grahammurdoch8@gmail.com>
2019-06-06 11:44:21 -0700

    fix: use base64 pngs to fix safari issue

--- src/phone-input/flag.js
+++ src/phone-input/flag.js
@@ -8,15 +8,14 @@ LICENSE file in the root directory of this source tree.
 
 import React from 'react';
 
-import * as flagComponents from './flags/index.js';
+import * as flags from './flags/index.js';
 import type {CountryIsoT} from './types.js';
 
 export default function Flag(props: {iso: CountryIsoT, width?: string}) {
   const {iso, width = '16px', ...restProps} = props;
-  const FlagComponent = flagComponents['Flag' + iso.toUpperCase()];
+  const FlagComponent = flags[`Flag${props.iso.toUpperCase()}`];
   return (
     <FlagComponent
-      iso={iso}
       width={width}
       data-e2e="country-flag"
       data-iso={iso}
