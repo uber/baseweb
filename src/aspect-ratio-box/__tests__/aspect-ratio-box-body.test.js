ccc9cbc7038788c3b48c5d2452108793c00fd952
Leyan Lo <leyan@uber.com>
2019-06-19 06:07:52 -1000

    feat(aspect-ratio-box): add aspect-ratio-box utility component (#1414)

---
+++ src/aspect-ratio-box/__tests__/aspect-ratio-box-body.test.js
@@ -0,0 +1,26 @@
+/*
+Copyright (c) 2018-2019 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+
+import * as React from 'react';
+import {mount} from 'enzyme';
+
+import AspectRatioBoxBody from '../aspect-ratio-box-body.js';
+
+describe('AspectRatioBoxBody', () => {
+  it('renders', () => {
+    const wrapper = mount(<AspectRatioBoxBody />);
+    expect(wrapper).toMatchSnapshot('with default styles');
+
+    wrapper.setProps({
+      overrides: {
+        Block: {style: {marginBottom: '10px'}},
+      },
+    });
+    expect(wrapper).toMatchSnapshot('with overridden styles');
+  });
+});
