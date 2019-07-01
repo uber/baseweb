4ceff3e31a5e17e5df895faeb5237e513de3a5f6
Chase Starr <chasestarr@gmail.com>
2019-07-01 16:03:48 -0700

    feat(table-grid): creates table components using css-grid (#1489)
    * wip
    
    * feat(table-grid): implements basic version
    
    * fix(button): remove position relative
    
    * feat(theme): adds table striped row to theme
    
    * feat(table): adds $striped option to table row
    
    * feat(table-grid): updates styles
    
    * feat(table-grid): add examples
    
    * feat(table-grid): add docs site page
    
    * feat(table-grid): adds route
    
    * fix(table-grid): lint
    
    * Update documentation-site/pages/components/table-grid.mdx
    
    * feat(table-grid): renames with unstable prefix
    
    * feat(docs-site): adds unstable warning component
    
    * fix(docs-site): lint
    
    * feat(table-grid): option to make header not sticky
    
    * fix(docs-site): table-grid example width

---
+++ src/table-grid/__tests__/table-grid.scenario.js
@@ -0,0 +1,59 @@
+/*
+Copyright (c) 2018-2019 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+
+import React from 'react';
+
+import {useStyletron} from '../../styles/index.js';
+
+import {
+  Unstable_StyledTable as StyledTable,
+  Unstable_StyledHeadCell as StyledHeadCell,
+  Unstable_StyledBodyCell as StyledBodyCell,
+} from '../index.js';
+
+export const name = 'table-grid';
+
+const data = Array(100)
+  .fill(2)
+  .map(() => [
+    `As we find ourselves caught between unraveling old realities and emerging weird ones, Worlding
+     creating art, games, institutions, religions, or life itself: live to world and world to live.`,
+    'Cell two',
+    'Cell three',
+    'Cell four',
+    'Cell five',
+    'Cell six',
+  ]);
+
+export function component() {
+  const [css] = useStyletron();
+  return (
+    <div className={css({height: '750px', width: '900px'})}>
+      <StyledTable $gridTemplateColumns="minmax(400px, max-content) 200px 200px 200px 200px 200px">
+        <StyledHeadCell>Column 1</StyledHeadCell>
+        <StyledHeadCell>Column 2</StyledHeadCell>
+        <StyledHeadCell>Column 3</StyledHeadCell>
+        <StyledHeadCell>Column 4</StyledHeadCell>
+        <StyledHeadCell>Column 5</StyledHeadCell>
+        <StyledHeadCell>Column 6</StyledHeadCell>
+        {data.map(row => {
+          return (
+            <>
+              <StyledBodyCell>{row[0]}</StyledBodyCell>
+              <StyledBodyCell>{row[1]}</StyledBodyCell>
+              <StyledBodyCell>{row[2]}</StyledBodyCell>
+              <StyledBodyCell>{row[3]}</StyledBodyCell>
+              <StyledBodyCell>{row[4]}</StyledBodyCell>
+              <StyledBodyCell>{row[5]}</StyledBodyCell>
+            </>
+          );
+        })}
+      </StyledTable>
+    </div>
+  );
+}
