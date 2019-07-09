086a83f2ec16634e4a86c84a403fa68395b2aaf6
Gergely Nemeth <gergelyke@users.noreply.github.com>
2019-07-09 16:05:38 -0700

    fix(now): use cname from BUILDKITE_TAG (#1528)

--- now.sh
+++ now.sh
@@ -15,16 +15,18 @@ echo latest tagged commit: $latest_tagged_commit
 if [ "$this_commit" = "$latest_tagged_commit" ]; then
   echo current commit matches latest tagged commit
   echo deploying to now
+  BUILDKITE_TAG=$BUILDKITE_TAG
+  echo version $BUILDKITE_TAG
 
   now --scope=uber-ui-platform --token=$ZEIT_NOW_TOKEN --public --no-clipboard deploy ./public > deployment.txt
   deployment=`cat deployment.txt`
-  cname="${deployment//./-}"
+  cname="${BUILDKITE_TAG//./-}"
   curl -X POST "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/dns_records" \
      -H "X-Auth-Email: $CF_AUTH_EMAIL" \
      -H "X-Auth-Key: $CF_API_KEY" \
      -H "Content-Type: application/json" \
-     --data "{\"type\":\"CNAME\",\"name\":\"$latest_tagged_commit.baseweb.design\",\"content\":\"alias.zeit.co\",\"ttl\":1,\"priority\":10,\"proxied\":false}"
-  now --scope=uber-ui-platform --token=$ZEIT_NOW_TOKEN alias $deployment "$latest_tagged_commit.baseweb.design"
+     --data "{\"type\":\"CNAME\",\"name\":\"$cname.baseweb.design\",\"content\":\"alias.zeit.co\",\"ttl\":1,\"priority\":10,\"proxied\":false}"
+  now --scope=uber-ui-platform --token=$ZEIT_NOW_TOKEN alias $deployment "$cname.baseweb.design"
 else
   echo current commit does not match latest tagged commit
   echo exited without deploying to now
