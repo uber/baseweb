c879136439fcab4a42b2fb92bb0ec555726cbde5
Chase Starr <chasestarr@gmail.com>
2019-07-10 13:16:36 -0700

    feat(codemods): refactors codemods to dubstep and separate package (#1519)
    * feat(codemods): refactors codemods to dubstep and separate package
    
    * fix(linter): removes todo from flow-typed def
    
    * fix(codemod-npm): add ds_store to npm ignore
    
    * fix(codemod-npm): move from bin to main
    
    * feat(codemods): bump patch version
    
    * fix(codemod-pkg): make it runnable
    
    * feat(codemods): bump patch version
    
    * fix(codemod-binary): make it executable
    
    * feat(codemod-pkg): patch
    
    * fix(codemod): refine import path
    
    * feat(codemods): bump patch
    
    * feat(codemods): adds index file to import from
    
    * fix(codemods): removes x from cli file
    
    * fix(codemods): adds x to cli file
    
    * feat(codemods): bump patch
    
    * feat(codemod): improves v7 codemod to apply type args
    
    * feat(codemod): bump patch version
    
    * fix(codemod): ignore flow-error

---
+++ packages/baseui-codemods/jest.config.js
@@ -0,0 +1,15 @@
+/*
+Copyright (c) 2018-2019 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+// @flow
+/* global module */
+
+module.exports = {
+  rootDir: 'src',
+  testPathIgnorePatterns: ['/node_modules/'],
+  testRegex: '(.|-)test\\.js$',
+};
