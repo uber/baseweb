362aec9689cbf1fd37ea9abc5f626ad749abc80f
Vojtech Miksu <vojtech@uber.com>
2019-07-11 12:49:10 -0700

    feat(ts): copy ts def files into dist

---
+++ scripts/ts-copy.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+/*
+Copyright (c) 2018-2019 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+/* eslint-env node */
+// @flow
+
+const fs = require('fs-extra');
+const globby = require('globby');
+const path = require('path');
+
+(async () => {
+  const files = await globby(['src/**/*.d.ts']);
+  files.forEach(async file => {
+    const from = path.join(__dirname, '../', file);
+    const to = path.join(__dirname, '../', file.replace('src', 'dist'));
+    await fs.copy(from, to);
+  });
+})();
