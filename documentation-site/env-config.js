1b38a99a768586f94470407c242cc9fff0354087
Gergely Nemeth <gergelyke@users.noreply.github.com>
2019-07-15 09:54:45 -0700

    fix(build): use NODE_ENV for everything (#1565)

--- documentation-site/env-config.js
+++ documentation-site/env-config.js
@@ -7,7 +7,7 @@ LICENSE file in the root directory of this source tree.
 // @flow
 /* global module process */
 
-const prod = process.env.BUILD_ENV === 'production';
+const prod = process.env.NODE_ENV === 'production';
 
 // set production env for exact-react-types-loader
 if (prod) {
@@ -17,7 +17,7 @@ if (prod) {
 module.exports = {
   'process.env.STATIC_ROOT': '/static/',
   'process.env.GITHUB_AUTH_TOKEN': process.env.GITHUB_AUTH_TOKEN || '',
-  ...(process.env.BUILD_ENV === 'production'
+  ...(prod
     ? {
         'process.env.WEBSITE_ENV': 'production',
       }
