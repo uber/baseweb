b6bd38fe1ba6cf0d1a1e8f246c605b923095af57
jhey tompkins <jh3y@users.noreply.github.com>
2019-07-17 23:39:22 +0100

    fix(select): dont allow InputContainer to break flex flow (#1564)

--- src/select/multi-value.js
+++ src/select/multi-value.js
@@ -20,9 +20,9 @@ export default function MultiValue(props: any) {
         Root: {
           style: ({$theme: {sizing}}) => ({
             marginRight: sizing.scale0,
-            marginBottom: 0,
+            marginBottom: sizing.scale0,
             marginLeft: sizing.scale0,
-            marginTop: 0,
+            marginTop: sizing.scale0,
           }),
         },
       }}
