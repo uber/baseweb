4cc50fe9412dd6e019ec92907bc1762a254cdaea
Graham Murdoch <grahammurdoch8@gmail.com>
2019-07-29 15:26:34 -0700

    docs(form-control): seperate out input types so notes can be added

---
+++ documentation-site/examples/form-control/checkbox.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import {FormControl} from 'baseui/form-control';
+import {StatefulCheckbox} from 'baseui/checkbox';
+
+export default () => (
+  <FormControl label="Checkbox label" caption="Checkbox caption">
+    <StatefulCheckbox>Checkbox control</StatefulCheckbox>
+  </FormControl>
+);
