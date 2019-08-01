d968c68b94ed53126e7f5a20bf874b20bb204170
Graham Murdoch <grahammurdoch8@gmail.com>
2019-08-01 16:32:31 -0700

    test(input): add regression tests for password masking (#1684)
    * test(input): add regression tests for password masking
    
    * fix: comment
    
    * fix: screener actually submitting form
    
    * fix: screener typo

--- src/input/__tests__/input-password.scenario.js
+++ src/input/__tests__/input-password.scenario.js
@@ -5,6 +5,7 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 // @flow
+/* global window */
 
 import React from 'react';
 // eslint-disable-next-line import/extensions
@@ -14,25 +15,38 @@ import {StatefulInput} from '../index.js';
 export const name = 'input-password';
 
 export const component = () => {
-  const toggleSelector = `[data-screener="toggle"]`;
+  const toggleSelector = `[data-e2e="mask-toggle"]`;
   const steps = new Steps()
     .wait(toggleSelector)
     .click(toggleSelector)
     .snapshot('Input with password masking toggle')
     .end();
   return (
-    <Screener steps={steps}>
-      <StatefulInput
-        type="password"
-        initialState={{value: '1234'}}
-        overrides={{
-          MaskToggleButton: {
-            props: {
-              'data-screener': 'toggle',
+    <form
+      onSubmit={event => {
+        event.preventDefault();
+        window.__e2e__formSubmitted__ = true;
+        return false;
+      }}
+    >
+      <Screener steps={steps}>
+        <StatefulInput
+          type="password"
+          initialState={{value: '1234'}}
+          overrides={{
+            Input: {
+              props: {
+                'data-e2e': 'input',
+              },
             },
-          },
-        }}
-      />
-    </Screener>
+            MaskToggleButton: {
+              props: {
+                'data-e2e': 'mask-toggle',
+              },
+            },
+          }}
+        />
+      </Screener>
+    </form>
   );
 };
--- src/input/__tests__/input.e2e.js
+++ src/input/__tests__/input.e2e.js
@@ -4,9 +4,9 @@ Copyright (c) 2018-2019 Uber Technologies, Inc.
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
-
 /* eslint-env node */
 /* eslint-disable flowtype/require-valid-file-annotation */
+/* global window */
 
 const {mount, analyzeAccessibility} = require('../../../e2e/helpers');
 
@@ -14,6 +14,7 @@ const selectors = {
   input: '[data-e2e="input"]',
   clearIcon: '[data-e2e="clear-icon"]',
   lastInput: '[data-e2e="last-input"]',
+  maskToggle: '[data-e2e="mask-toggle"]',
 };
 
 describe('input', () => {
@@ -110,6 +111,54 @@ describe('input', () => {
       expect(await page.$eval(selectors.lastInput, input => input.value)).toBe(
         'Or other',
       );
+    });
+
+    // regression tests for https://github.com/uber-web/baseui/issues/1662
+    describe('while in a form', () => {
+      beforeEach(async () => {
+        await mount(page, 'input-password');
+        await page.waitFor(selectors.input);
+        // set global variable '__e2e__formSubmitted__' to false
+        await page.evaluate(() => (window.__e2e__formSubmitted__ = false));
+      });
+
+      it('while focusing input, "enter" does not toggle password masking', async () => {
+        // focus input
+        await page.focus(selectors.input);
+        // verify type is password, aka the text is masked
+        expect(await page.$eval(selectors.input, input => input.type)).toBe(
+          'password',
+        );
+        // hit enter
+        await page.keyboard.press('Enter');
+        // verify type is still password, aka the text is still masked
+        expect(await page.$eval(selectors.input, input => input.type)).toBe(
+          'password',
+        );
+        // verify global variable '__e2e__formSubmitted__' is true
+        expect(await page.evaluate(() => window.__e2e__formSubmitted__)).toBe(
+          true,
+        );
+      });
+
+      it('while focusing mask toggle, "enter" does not submit the form', async () => {
+        // focus password mask toggle
+        await page.focus(selectors.maskToggle);
+        // verify type is password, aka the text is masked
+        expect(await page.$eval(selectors.input, input => input.type)).toBe(
+          'password',
+        );
+        // hit enter
+        await page.keyboard.press('Enter');
+        // verify type is now text, aka the text is not masked
+        expect(await page.$eval(selectors.input, input => input.type)).toBe(
+          'text',
+        );
+        // verify global variable '__e2e__formSubmitted__' is still false
+        expect(await page.evaluate(() => window.__e2e__formSubmitted__)).toBe(
+          false,
+        );
+      });
     });
   });
 });
