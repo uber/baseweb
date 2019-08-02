ab452774b6d078688b80f8e0d7cec3dd5a77cd39
Graham Murdoch <grahammurdoch8@gmail.com>
2019-08-02 13:42:04 -0700

    docs(textarea): prefer stateless and split out examples

--- documentation-site/examples/textarea/ref.js
+++ documentation-site/examples/textarea/ref.js
@@ -1,15 +1,18 @@
 // @flow
 import * as React from 'react';
-import {StatefulTextarea as Textarea} from 'baseui/textarea';
+import {Textarea} from 'baseui/textarea';
 import {Button} from 'baseui/button';
 
-export default () => {
-  const inputRef = React.createRef();
+export default function() {
+  const [value, setValue] = React.useState('');
+  const inputRef = React.useRef(null);
   return (
     <React.Fragment>
       <Textarea
         inputRef={inputRef}
-        placeholder="With textarea input ref"
+        value={value}
+        onChange={e => setValue(e.currentTarget.value)}
+        placeholder="Focus me by clicking the button below..."
       />
       <br />
       <Button
@@ -17,8 +20,8 @@ export default () => {
           inputRef.current && inputRef.current.focus();
         }}
       >
-        Click here to focus input
+        Focus Textarea
       </Button>
     </React.Fragment>
   );
-};
+}
