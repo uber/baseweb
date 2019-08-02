b300c21bc5bfa6f34d4f80941a954edca508a89f
Gergely Nemeth <gergelyke@users.noreply.github.com>
2019-08-02 14:16:09 -0700

    fix(checkbox): rtl support (#1689)

---
+++ src/checkbox/__tests__/checkbox-rtl.scenario.js
@@ -0,0 +1,22 @@
+/*
+Copyright (c) 2018-2019 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+
+import * as React from 'react';
+
+import {component as Checkbox} from './checkbox.scenario.js';
+import {ThemeProvider, LightTheme} from '../../index.js';
+
+export const name = 'checkbox-rtl';
+
+export const component = () => (
+  <ThemeProvider theme={{...LightTheme, direction: 'rtl'}}>
+    <div dir="rtl">
+      <Checkbox />
+    </div>
+  </ThemeProvider>
+);
