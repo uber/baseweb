876f0b8f79b3e6d0010b7a781de173144fddd3cc
jhey tompkins <jh3y@users.noreply.github.com>
2019-08-02 19:52:37 +0100

    feat: rtl support progress steps (#1686)

---
+++ src/progress-steps/__tests__/progress-steps-rtl.scenario.js
@@ -0,0 +1,20 @@
+/*
+Copyright (c) 2018-2019 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+
+import * as React from 'react';
+
+import {component as ProgressSteps} from './progress-steps.scenario.js';
+import {ThemeProvider, LightTheme} from '../../index.js';
+
+export const name = 'progress-steps-rtl';
+
+export const component = () => (
+  <ThemeProvider theme={{...LightTheme, direction: 'rtl'}}>
+    <ProgressSteps />
+  </ThemeProvider>
+);
