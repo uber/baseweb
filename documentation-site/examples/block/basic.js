f406101ab1f957072ae1262c381fe68064e1d1c1
Chase Starr <starr@uber.com>
2019-08-05 10:42:29 -0700

    feat(docs): updates the block documentation

--- documentation-site/examples/block/basic.js
+++ documentation-site/examples/block/basic.js
@@ -1,21 +1,14 @@
 // @flow
-import * as React from 'react';
+import React from 'react';
 import {Block} from 'baseui/block';
 
-export default () => {
-  const colors = ['primary', 'negative', 'warning', 'positive'];
-  const elements = [];
-
-  for (const color of colors) {
-    for (let x = 100; x <= 700; x += 100) {
-      const colorString = `${color}${x}`;
-      elements.push(
-        <Block color={colorString} key={colorString}>
-          {colorString}
-        </Block>,
-      );
-    }
-  }
-
-  return elements;
-};
+export default function() {
+  return (
+    <React.Fragment>
+      <Block>first element</Block>
+      <Block paddingTop="100px">
+        padding top applied to this element
+      </Block>
+    </React.Fragment>
+  );
+}
