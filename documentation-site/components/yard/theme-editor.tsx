226acb7457c228619db2c874ec0bc4936ca5454d
Vojtech Miksu <vojtech@miksu.cz>
2019-08-06 19:32:56 +0200

    feat(yard): add theme editor (#1710)
    * feat(yard): add theme editor UI
    
    * feat(yard): finish theme editor feature
    
    * feat(yard): detect number of changed props

--- documentation-site/components/yard/theme-editor.tsx
+++ documentation-site/components/yard/theme-editor.tsx
@@ -1,20 +1,122 @@
 import * as React from 'react';
-import {Notification, KIND} from 'baseui/notification';
+import {Input, SIZE} from 'baseui/input';
 import {useStyletron} from 'baseui';
+import Link from 'next/link';
+import {StyledLink} from 'baseui/link';
+import {Caption1} from 'baseui/typography';
 
-const ThemeEditor = () => {
-  const [css] = useStyletron();
+type ThemeEditorProps = {
+  componentName: string;
+  theme: {[key: string]: string};
+  themeInit: {[key: string]: string};
+  set: (value: {[key: string]: string}) => void;
+};
+
+type ColumnProps = {
+  themeKeys: string[];
+  theme: {[key: string]: string};
+  themeInit: {[key: string]: string};
+  set: (value: {[key: string]: string}) => void;
+};
+
+const Column: React.FC<ColumnProps> = ({themeKeys, themeInit, theme, set}) => {
+  const [css, $theme] = useStyletron();
   return (
     <div
       className={css({
-        display: 'flex',
-        justifyContent: 'center',
-        marginTop: '3em',
-        marginBottom: '2em',
+        flexBasis: '50%',
       })}
     >
-      <Notification kind={KIND.warning}>Not implemented yet.</Notification>
+      {themeKeys.map(key => {
+        return (
+          <label
+            key={key}
+            className={css({
+              display: 'flex',
+              alignItems: 'center',
+            })}
+          >
+            <div
+              className={css({
+                width: '4px',
+                height: '36px',
+                backgroundColor: theme[key],
+              })}
+            ></div>
+            <Input
+              positive={theme[key] !== themeInit[key]}
+              size={SIZE.compact}
+              placeholder={themeInit[key]}
+              value={theme[key]}
+              onChange={e =>
+                set({...theme, [key]: (e.target as HTMLInputElement).value})
+              }
+              overrides={{Root: {style: {width: '100px'}}}}
+            />
+            <div
+              className={css({
+                ...($theme.typography.font200 as any),
+                color: $theme.colors.foreground,
+                marginLeft: $theme.sizing.scale300,
+              })}
+            >
+              {key}
+            </div>
+          </label>
+        );
+      })}
     </div>
+  );
+};
+
+const ThemeEditor: React.FC<ThemeEditorProps> = ({
+  theme,
+  themeInit,
+  set,
+  componentName,
+}) => {
+  const [css] = useStyletron();
+  const themeKeys = Object.keys(theme);
+
+  const firstThemeKeys = themeKeys.slice(0, themeKeys.length / 2);
+  const secondThemeKeys = themeKeys.slice(themeKeys.length / 2);
+
+  return (
+    <React.Fragment>
+      <Caption1
+        marginLeft="scale200"
+        marginRight="scale200"
+        marginBottom="scale400"
+      >
+        Do you want to change {componentName} colors globally? You can customize
+        the theme through ThemeProvider and set your own colors.{' '}
+        <Link href="/theming/custom-themes/#creating-a-custom-theme">
+          <StyledLink href="/theming/custom-themes/#creating-a-custom-theme">
+            Learn more
+          </StyledLink>
+        </Link>
+        . Try different values:
+      </Caption1>
+      <div
+        className={css({
+          display: 'flex',
+          flexDirection: 'row',
+        })}
+      >
+        <Column
+          themeKeys={firstThemeKeys}
+          theme={theme}
+          themeInit={themeInit}
+          set={set}
+        />
+        <Column
+          themeKeys={secondThemeKeys}
+          theme={theme}
+          themeInit={themeInit}
+          set={set}
+        />
+      </div>
+    </React.Fragment>
   );
 };
 
