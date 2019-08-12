1169d783096d2dadc55474ec9be817da819602f8
Chase Starr <starr@uber.com>
2019-08-12 09:54:22 -0700

    feat(docs): datepicker example standardization

--- documentation-site/examples/datepicker/calendar-multiple-months.js
+++ documentation-site/examples/datepicker/calendar-multiple-months.js
@@ -4,6 +4,7 @@ import {ORIENTATION, StatefulCalendar} from 'baseui/datepicker';
 
 export default () => (
   <StatefulCalendar
+    onChange={({date}) => console.log(date)}
     orientation={ORIENTATION.horizontal}
     monthsShown={2}
     range
