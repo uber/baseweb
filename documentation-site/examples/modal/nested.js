c63e7dbba09c3d9e6de81bd527760167ae0031b8
Nadiia Dmytrenko <nadiia@uber.com>
2019-08-15 10:45:39 -0700

    fix(modasl): use hooks

--- documentation-site/examples/modal/nested.js
+++ documentation-site/examples/modal/nested.js
@@ -11,98 +11,78 @@ import {
 
 class ModalStateContainer extends React.Component<
   {
-    isInitiallyOpen: boolean,
     children: ({
-      open: () => mixed,
-      close: () => mixed,
-      isOpen: boolean,
       isConfirmationOpen: boolean,
       toggleConfirm: (open?: boolean, cb?: () => mixed) => mixed,
     }) => React.Node,
   },
   {
-    isOpen: boolean,
     isConfirmationOpen: boolean,
   },
 > {
-  static defaultProps = {
-    isInitiallyOpen: false,
-  };
   state = {
-    isOpen: this.props.isInitiallyOpen,
     isConfirmationOpen: false,
-  };
-  toggle = (open: boolean = !this.state.isOpen) => {
-    this.setState({
-      isOpen: open,
-    });
   };
   toggleConfirm = (
     open: boolean = !this.state.isConfirmationOpen,
     cb: () => mixed = () => {},
   ) => {
-    this.setState(
-      {
-        isConfirmationOpen: open,
-      },
-      cb,
-    );
-  };
-  open = () => {
-    this.toggle(true);
-  };
-
-  close = () => {
-    this.toggle(false);
+    this.setState({isConfirmationOpen: open}, cb);
   };
   render() {
     return this.props.children({
-      open: this.open,
-      close: this.close,
-      isOpen: this.state.isOpen,
       isConfirmationOpen: this.state.isConfirmationOpen,
       toggleConfirm: this.toggleConfirm,
     });
   }
 }
 
-export default () => (
-  <ModalStateContainer>
-    {({open, close, isOpen, isConfirmationOpen, toggleConfirm}) => (
-      <React.Fragment>
-        <Button onClick={open}>Open Modal</Button>
-        <Modal onClose={close} isOpen={isConfirmationOpen}>
-          <ModalHeader>Confirm</ModalHeader>
-          <ModalBody>Confirm closing all.</ModalBody>
-          <ModalFooter>
-            <ModalButton onClick={() => toggleConfirm(false)}>
-              No
-            </ModalButton>
-            <ModalButton
-              onClick={() => {
-                toggleConfirm(false);
-                close();
-              }}
-            >
-              Yes
-            </ModalButton>
-          </ModalFooter>
-        </Modal>
-        <Modal onClose={close} isOpen={isOpen}>
-          <ModalHeader>Hello world</ModalHeader>
-          <ModalBody>
-            Proin ut dui sed metus pharetra hend rerit vel non mi.
-            Nulla ornare faucibus ex, non facilisis nisl. Maecenas
-            aliquet mauris ut tempus.
-          </ModalBody>
-          <ModalFooter>
-            <ModalButton onClick={close}>Cancel</ModalButton>
-            <ModalButton onClick={() => toggleConfirm(true)}>
-              Okay
-            </ModalButton>
-          </ModalFooter>
-        </Modal>
-      </React.Fragment>
-    )}
-  </ModalStateContainer>
-);
+export default () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <ModalStateContainer>
+      {({isConfirmationOpen, toggleConfirm}) => (
+        <React.Fragment>
+          <Button onClick={() => setIsOpen(true)}>
+            Open Modal
+          </Button>
+          <Modal onClose={() => setIsOpen(false)} isOpen={isOpen}>
+            <ModalHeader>Hello world</ModalHeader>
+            <ModalBody>
+              Proin ut dui sed metus pharetra hend rerit vel non mi.
+              Nulla ornare faucibus ex, non facilisis nisl. Maecenas
+              aliquet mauris ut tempus.
+            </ModalBody>
+            <ModalFooter>
+              <ModalButton onClick={() => setIsOpen(false)}>
+                Cancel
+              </ModalButton>
+              <ModalButton onClick={() => toggleConfirm(true)}>
+                Okay
+              </ModalButton>
+            </ModalFooter>
+          </Modal>
+          <Modal
+            onClose={() => toggleConfirm(false)}
+            isOpen={isConfirmationOpen}
+          >
+            <ModalHeader>Confirm</ModalHeader>
+            <ModalBody>Confirm closing all.</ModalBody>
+            <ModalFooter>
+              <ModalButton onClick={() => toggleConfirm(false)}>
+                No
+              </ModalButton>
+              <ModalButton
+                onClick={() => {
+                  toggleConfirm(false, () => setIsOpen(false));
+                }}
+              >
+                Yes
+              </ModalButton>
+            </ModalFooter>
+          </Modal>
+        </React.Fragment>
+      )}
+    </ModalStateContainer>
+  );
+};
--- documentation-site/examples/modal/nested.tsx
+++ documentation-site/examples/modal/nested.tsx
@@ -9,98 +9,80 @@ import {
 } from 'baseui/modal';
 
 interface Props {
-  isInitiallyOpen: boolean;
   children: (args: {
-    open: () => any;
-    close: () => any;
-    isOpen: boolean;
     isConfirmationOpen: boolean;
     toggleConfirm: (open?: boolean, cb?: () => any) => any;
   }) => React.ReactNode;
 }
 
 interface State {
-  isOpen: boolean;
   isConfirmationOpen: boolean;
 }
 
 class ModalStateContainer extends React.Component<Props, State> {
-  static defaultProps = {
-    isInitiallyOpen: false,
-  };
   state = {
-    isOpen: this.props.isInitiallyOpen,
     isConfirmationOpen: false,
-  };
-  toggle = (open = !this.state.isOpen) => {
-    this.setState({
-      isOpen: open,
-    });
   };
   toggleConfirm = (
     open = !this.state.isConfirmationOpen,
     cb: () => any = () => {},
   ) => {
-    this.setState(
-      {
-        isConfirmationOpen: open,
-      },
-      cb,
-    );
-  };
-  open = () => {
-    this.toggle(true);
-  };
-  close = () => {
-    this.toggle(false);
+    this.setState({isConfirmationOpen: open}, cb);
   };
   render() {
     return this.props.children({
-      open: this.open,
-      close: this.close,
-      isOpen: this.state.isOpen,
       isConfirmationOpen: this.state.isConfirmationOpen,
       toggleConfirm: this.toggleConfirm,
     });
   }
 }
 
-export default () => (
-  <ModalStateContainer>
-    {({open, close, isOpen, isConfirmationOpen, toggleConfirm}) => (
-      <React.Fragment>
-        <Button onClick={open}>Open Modal</Button>
-        <Modal onClose={close} isOpen={isOpen}>
-          <ModalHeader>Hello world</ModalHeader>
-          <ModalBody>
-            Proin ut dui sed metus pharetra hend rerit vel non mi.
-            Nulla ornare faucibus ex, non facilisis nisl. Maecenas
-            aliquet mauris ut tempus.
-          </ModalBody>
-          <ModalFooter>
-            <ModalButton onClick={close}>Cancel</ModalButton>
-            <ModalButton onClick={() => toggleConfirm(true)}>
-              Okay
-            </ModalButton>
-          </ModalFooter>
-        </Modal>
-        <Modal onClose={close} isOpen={isConfirmationOpen}>
-          <ModalHeader>Confirm</ModalHeader>
-          <ModalBody>Confirm closing all.</ModalBody>
-          <ModalFooter>
-            <ModalButton onClick={() => toggleConfirm(false)}>
-              No
-            </ModalButton>
-            <ModalButton
-              onClick={() => {
-                toggleConfirm(false, close);
-              }}
-            >
-              Yes
-            </ModalButton>
-          </ModalFooter>
-        </Modal>
-      </React.Fragment>
-    )}
-  </ModalStateContainer>
-);
+export default () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <ModalStateContainer>
+      {({isConfirmationOpen, toggleConfirm}) => (
+        <React.Fragment>
+          <Button onClick={() => setIsOpen(true)}>
+            Open Modal
+          </Button>
+          <Modal onClose={() => setIsOpen(false)} isOpen={isOpen}>
+            <ModalHeader>Hello world</ModalHeader>
+            <ModalBody>
+              Proin ut dui sed metus pharetra hend rerit vel non mi.
+              Nulla ornare faucibus ex, non facilisis nisl. Maecenas
+              aliquet mauris ut tempus.
+            </ModalBody>
+            <ModalFooter>
+              <ModalButton onClick={() => setIsOpen(false)}>
+                Cancel
+              </ModalButton>
+              <ModalButton onClick={() => toggleConfirm(true)}>
+                Okay
+              </ModalButton>
+            </ModalFooter>
+          </Modal>
+          <Modal
+            onClose={() => toggleConfirm(false)}
+            isOpen={isConfirmationOpen}
+          >
+            <ModalHeader>Confirm</ModalHeader>
+            <ModalBody>Confirm closing all.</ModalBody>
+            <ModalFooter>
+              <ModalButton onClick={() => toggleConfirm(false)}>
+                No
+              </ModalButton>
+              <ModalButton
+                onClick={() => {
+                  toggleConfirm(false, () => setIsOpen(false));
+                }}
+              >
+                Yes
+              </ModalButton>
+            </ModalFooter>
+          </Modal>
+        </React.Fragment>
+      )}
+    </ModalStateContainer>
+  );
+};
