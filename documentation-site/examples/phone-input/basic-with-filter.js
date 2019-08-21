d12360d75c8e6bdd18f2a53d45476d4802119099
Ihor Kulyk <ihor.kulyk34@gmail.com>
2019-08-21 19:13:28 +0300

    docs(phone-input):  update searchable examples

--- documentation-site/examples/phone-input/basic-with-filter.js
+++ documentation-site/examples/phone-input/basic-with-filter.js
@@ -1,5 +1,21 @@
 // @flow
-import React from 'react';
-import {StatefulPhoneInput} from 'baseui/phone-input';
+import React, {useState} from 'react';
+import {PhoneInput, COUNTRIES} from 'baseui/phone-input';
 
-export default () => <StatefulPhoneInput searchable />;
+export default () => {
+  const [text, setText] = useState('');
+  const [country, setCountry] = useState(COUNTRIES.US);
+  return (
+    <PhoneInput
+      searchable
+      text={text}
+      country={country}
+      onTextChange={event => {
+        setText(event.currentTarget.value);
+      }}
+      onCountryChange={(event: any) => {
+        setCountry(event.option);
+      }}
+    />
+  );
+};
--- documentation-site/examples/phone-input/basic-with-filter.tsx
+++ documentation-site/examples/phone-input/basic-with-filter.tsx
@@ -1,3 +1,22 @@
-import React from 'react';
-import {StatefulPhoneInput} from 'baseui/phone-input';
-export default () => <StatefulPhoneInput searchable />;
+import React, {useState} from 'react';
+import {PhoneInput, COUNTRIES} from 'baseui/phone-input';
+
+export default () => {
+  const [text, setText] = useState('');
+  const [country, setCountry] = useState(COUNTRIES.US);
+  return (
+    <PhoneInput
+      searchable
+      text={text}
+      country={country}
+      onTextChange={(
+        event: React.SyntheticEvent<HTMLInputElement, Event>,
+      ) => {
+        setText(event.currentTarget.value);
+      }}
+      onCountryChange={(event: any) => {
+        setCountry(event.option);
+      }}
+    />
+  );
+};
