701ff876b8cd83595b7f37eea0ed3a6868699e4d
Vojtech Miksu <vojtech@uber.com>
2019-09-05 17:21:05 -0700

    feat(yard): fix tsc

--- documentation-site/components/yard/index.tsx
+++ documentation-site/components/yard/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Card, StyledBody} from 'baseui/card';
+import {Card} from 'baseui/card';
 import {Button} from 'baseui/button';
 import {Paragraph2} from 'baseui/typography';
 import getConfig from 'next/config';
