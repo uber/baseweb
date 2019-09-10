13ed985aeb2f79d4d2d7f802ad122f06c8b2b5dd
Graham Murdoch <grahammurdoch8@gmail.com>
2019-09-10 10:51:15 -0700

    fix(buttons): update loading spinner colors
    Added new theme variables for styling the spinner for each button kind and for disabled buttons. This fixes some lingering blue styles and some visual issues in dark mode. I also updated the button storybook scenario to show more loading states.

--- src/button/__tests__/button.scenario.js
+++ src/button/__tests__/button.scenario.js
@@ -8,51 +8,44 @@ LICENSE file in the root directory of this source tree.
 
 import * as React from 'react';
 
-import {Block} from '../../block/index.js';
 import {Button, KIND} from '../index.js';
 
 export const name = 'button';
 
-export const component = () => (
-  <React.Fragment>
-    <Button kind={KIND.primary}>Button</Button>
-    <Block as="span" marginLeft="scale300" />
-    <Button kind={KIND.primary} isSelected>
-      Selected
-    </Button>
-    <Block as="span" marginLeft="scale300" />
-    <Button kind={KIND.primary} disabled>
-      Disabled
-    </Button>
-    <Block marginBottom="scale300" />
-    <Button kind={KIND.secondary}>Button</Button>
-    <Block as="span" marginLeft="scale300" />
-    <Button kind={KIND.secondary} isSelected>
-      Selected
-    </Button>
-    <Block as="span" marginLeft="scale300" />
-    <Button kind={KIND.secondary} disabled>
-      Disabled
-    </Button>
-    <Block marginBottom="scale300" />
-    <Button kind={KIND.tertiary}>Button</Button>
-    <Block as="span" marginLeft="scale300" />
-    <Button kind={KIND.tertiary} isSelected>
-      Selected
-    </Button>
-    <Block as="span" marginLeft="scale300" />
-    <Button kind={KIND.tertiary} disabled>
-      Disabled
-    </Button>
-    <Block marginBottom="scale300" />
-    <Button kind={KIND.minimal}>Button</Button>
-    <Block as="span" marginLeft="scale300" />
-    <Button kind={KIND.minimal} isSelected>
-      Selected
-    </Button>
-    <Block as="span" marginLeft="scale300" />
-    <Button kind={KIND.minimal} disabled>
-      Disabled
-    </Button>
-  </React.Fragment>
-);
+export const component = () => {
+  return (
+    <table>
+      <tbody>
+        {Object.keys(KIND).map(kind => {
+          return (
+            <tr key={kind}>
+              <td>
+                <Button kind={kind}>Move</Button>
+              </td>
+              <td>
+                <Button kind={kind} isSelected>
+                  Move
+                </Button>
+              </td>
+              <td>
+                <Button kind={kind} isLoading>
+                  Move
+                </Button>
+              </td>
+              <td>
+                <Button kind={kind} disabled>
+                  Move
+                </Button>
+              </td>
+              <td>
+                <Button kind={kind} disabled isLoading>
+                  Move
+                </Button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
