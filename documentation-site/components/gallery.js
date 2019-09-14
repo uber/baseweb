856a7fbf396c0b8223981afedb03b6ec4599af81
Graham Murdoch <grahammurdoch8@gmail.com>
2019-09-13 20:24:06 -0700

    refactor(gallery): change svg components import for brevity

--- documentation-site/components/gallery.js
+++ documentation-site/components/gallery.js
@@ -11,315 +11,256 @@ import React from 'react';
 import {H4} from 'baseui/typography';
 import {useStyletron} from 'baseui/styles';
 
-// Inputs
-import SvgButton from '../components/thumbs/components/Button.js';
-import SvgButtonGroup from '../components/thumbs/components/ButtonGroup.js';
-import SvgCheckbox from '../components/thumbs/components/Checkbox.js';
-import SvgFormControl from '../components/thumbs/components/FormControl.js';
-import SvgInput from '../components/thumbs/components/Input.js';
-import SvgPaymentCard from '../components/thumbs/components/PaymentCard.js';
-import SvgPhoneInput from '../components/thumbs/components/PhoneInput.js';
-import SvgPinCode from '../components/thumbs/components/PinCode.js';
-import SvgRadio from '../components/thumbs/components/Radio.js';
-import SvgSlider from '../components/thumbs/components/Slider.js';
-import SvgTextarea from '../components/thumbs/components/Textarea.js';
-
-// Pickers
-import SvgDatepicker from '../components/thumbs/components/Datepicker.js';
-import SvgFileUploader from '../components/thumbs/components/FileUploader.js';
-import SvgMenu from '../components/thumbs/components/Menu.js';
-import SvgRating from '../components/thumbs/components/Rating.js';
-import SvgSelect from '../components/thumbs/components/Select.js';
-
-// Navigation
-import SvgBreadcrumbs from '../components/thumbs/components/Breadcrumbs.js';
-import SvgHeaderNavigation from '../components/thumbs/components/HeaderNavigation.js';
-import SvgLink from '../components/thumbs/components/Link.js';
-import SvgPagination from '../components/thumbs/components/Pagination.js';
-import SvgSideNavigation from '../components/thumbs/components/SideNavigation.js';
-import SvgTabs from '../components/thumbs/components/Tabs.js';
-
-// Content
-import SvgAccordian from '../components/thumbs/components/Accordian.js';
-import SvgAvatar from '../components/thumbs/components/Avatar.js';
-import SvgDndList from '../components/thumbs/components/DndList.js';
-import SvgHeading from '../components/thumbs/components/Heading.js';
-import SvgIcon from '../components/thumbs/components/Icon.js';
-import SvgTable from '../components/thumbs/components/Table.js';
-import SvgTableGrid from '../components/thumbs/components/TableGrid.js';
-import SvgDataTable from '../components/thumbs/components/DataTable.js';
-import SvgTag from '../components/thumbs/components/Tag.js';
-import SvgTypography from '../components/thumbs/components/Typography.js';
-
-// Feedback
-import SvgNotification from '../components/thumbs/components/Notification.js';
-import SvgProgressBar from '../components/thumbs/components/ProgressBar.js';
-import SvgSpinner from '../components/thumbs/components/Spinner.js';
-import SvgToast from '../components/thumbs/components/Toast.js';
-
-// Surfaces
-import SvgCard from '../components/thumbs/components/Card.js';
-import SvgDrawer from '../components/thumbs/components/Drawer.js';
-import SvgModal from '../components/thumbs/components/Modal.js';
-import SvgPopover from '../components/thumbs/components/Popover.js';
-import SvgTooltip from '../components/thumbs/components/Tooltip.js';
-
-// Utility
-import SvgAspectRatioBox from '../components/thumbs/components/AspectRatioBox.js';
-import SvgFlexGrid from '../components/thumbs/components/FlexGrid.js';
-import SvgLayer from '../components/thumbs/components/Layer.js';
-import SvgUnstableA11y from '../components/thumbs/components/UnstableA11Y.js';
-import SvgTokens from '../components/thumbs/components/Tokens.js';
-import SvgBlock from '../components/thumbs/components/Block.js';
+import * as thumbnails from '../components/thumbs';
 
 const COMPONENTS = {
   Inputs: [
     {
       label: 'Button',
       href: '/components/button',
-      Component: SvgButton,
+      Component: thumbnails.SvgButton,
     },
     {
       label: 'Button Group',
       href: '/components/button-group',
-      Component: SvgButtonGroup,
+      Component: thumbnails.SvgButtonGroup,
     },
     {
       label: 'Input',
       href: '/components/input',
-      Component: SvgInput,
+      Component: thumbnails.SvgInput,
     },
     {
       label: 'Checkbox',
       href: '/components/checkbox',
-      Component: SvgCheckbox,
+      Component: thumbnails.SvgCheckbox,
     },
     {
       label: 'Radio',
       href: '/components/radio',
-      Component: SvgRadio,
+      Component: thumbnails.SvgRadio,
     },
     {
       label: 'Textarea',
       href: '/components/textarea',
-      Component: SvgTextarea,
+      Component: thumbnails.SvgTextarea,
     },
     {
       label: 'Form Control',
       href: '/components/form-control',
-      Component: SvgFormControl,
+      Component: thumbnails.SvgFormControl,
     },
     {
       label: 'Payment Card',
       href: '/components/payment-card',
-      Component: SvgPaymentCard,
+      Component: thumbnails.SvgPaymentCard,
     },
     {
       label: 'Pin Code',
       href: '/components/pin-code',
-      Component: SvgPinCode,
+      Component: thumbnails.SvgPinCode,
     },
     {
       label: 'Phone Input',
       href: '/components/phone-input',
-      Component: SvgPhoneInput,
+      Component: thumbnails.SvgPhoneInput,
     },
     {
       label: 'Slider',
       href: '/components/slider',
-      Component: SvgSlider,
+      Component: thumbnails.SvgSlider,
     },
   ],
   Pickers: [
     {
       label: 'Datepicker',
       href: '/components/datepicker',
-      Component: SvgDatepicker,
+      Component: thumbnails.SvgDatepicker,
     },
     {
       label: 'File Uploader',
       href: '/components/file-uploader',
-      Component: SvgFileUploader,
+      Component: thumbnails.SvgFileUploader,
     },
     {
       label: 'Menu',
       href: '/components/menu',
-      Component: SvgMenu,
+      Component: thumbnails.SvgMenu,
     },
     {
       label: 'Rating',
       href: '/components/rating',
-      Component: SvgRating,
+      Component: thumbnails.SvgRating,
     },
     {
       label: 'Select',
       href: '/components/select',
-      Component: SvgSelect,
+      Component: thumbnails.SvgSelect,
     },
   ],
   Navigation: [
     {
       label: 'Breadcrumbs',
       href: '/components/breadcrumbs',
-      Component: SvgBreadcrumbs,
+      Component: thumbnails.SvgBreadcrumbs,
     },
     {
       label: 'Header Navigation',
       href: '/components/header-navigation',
-      Component: SvgHeaderNavigation,
+      Component: thumbnails.SvgHeaderNavigation,
     },
     {
       label: 'Link',
       href: '/components/link',
-      Component: SvgLink,
+      Component: thumbnails.SvgLink,
     },
     {
       label: 'Pagination',
       href: '/components/pagination',
-      Component: SvgPagination,
+      Component: thumbnails.SvgPagination,
     },
     {
       label: 'Side Navigation',
       href: '/components/side-navigation',
-      Component: SvgSideNavigation,
+      Component: thumbnails.SvgSideNavigation,
     },
     {
       label: 'Tabs',
       href: '/components/tabs',
-      Component: SvgTabs,
+      Component: thumbnails.SvgTabs,
     },
   ],
   Content: [
     {
       label: 'Accordian',
       href: '/components/accordian',
-      Component: SvgAccordian,
+      Component: thumbnails.SvgAccordian,
     },
     {
       label: 'Avatar',
       href: '/components/avatar',
-      Component: SvgAvatar,
+      Component: thumbnails.SvgAvatar,
     },
     {
       label: 'Drag and Drop List',
       href: '/components/dnd-list',
-      Component: SvgDndList,
+      Component: thumbnails.SvgDndList,
     },
     {
       label: 'Heading',
       href: '/components/heading',
-      Component: SvgHeading,
+      Component: thumbnails.SvgHeading,
     },
     {
       label: 'Icon',
       href: '/components/icon',
-      Component: SvgIcon,
+      Component: thumbnails.SvgIcon,
     },
     {
       label: 'Table',
       href: '/components/table',
-      Component: SvgTable,
+      Component: thumbnails.SvgTable,
     },
     {
       label: 'Table Grid',
       href: '/components/table-grid',
-      Component: SvgTableGrid,
+      Component: thumbnails.SvgTableGrid,
     },
     {
       label: 'Data Table',
       href: '/components/data-table',
-      Component: SvgDataTable,
+      Component: thumbnails.SvgDataTable,
     },
     {
       label: 'Tag',
       href: '/components/tag',
-      Component: SvgTag,
+      Component: thumbnails.SvgTag,
     },
     {
       label: 'Typography',
       href: '/components/typography',
-      Component: SvgTypography,
+      Component: thumbnails.SvgTypography,
     },
   ],
   Feedback: [
     {
       label: 'Notification',
       href: '/components/notification',
-      Component: SvgNotification,
+      Component: thumbnails.SvgNotification,
     },
     {
       label: 'Progress Bar',
       href: '/components/progress-bar',
-      Component: SvgProgressBar,
+      Component: thumbnails.SvgProgressBar,
     },
     {
       label: 'Spinner',
       href: '/components/spinner',
-      Component: SvgSpinner,
+      Component: thumbnails.SvgSpinner,
     },
     {
       label: 'Toast',
       href: '/components/toast',
-      Component: SvgToast,
+      Component: thumbnails.SvgToast,
     },
   ],
   Surfaces: [
     {
       label: 'card',
       href: '/components/card',
-      Component: SvgCard,
+      Component: thumbnails.SvgCard,
     },
     {
       label: 'Drawer',
       href: '/components/drawer',
-      Component: SvgDrawer,
+      Component: thumbnails.SvgDrawer,
     },
     {
       label: 'Modal',
       href: '/components/modal',
-      Component: SvgModal,
+      Component: thumbnails.SvgModal,
     },
     {
       label: 'Pop-Over',
       href: '/components/popover',
-      Component: SvgPopover,
+      Component: thumbnails.SvgPopover,
     },
     {
       label: 'Tool-Tip',
       href: '/components/tooltip',
-      Component: SvgTooltip,
+      Component: thumbnails.SvgTooltip,
     },
   ],
   Utility: [
     {
       label: 'Aspect Ratio Box',
       href: '/components/aspect-ratio-box',
-      Component: SvgAspectRatioBox,
+      Component: thumbnails.SvgAspectRatioBox,
     },
     {
       label: 'Flex Grid',
       href: '/components/flex-grid',
-      Component: SvgFlexGrid,
+      Component: thumbnails.SvgFlexGrid,
     },
     {
       label: 'Layer',
       href: '/components/layer',
-      Component: SvgLayer,
+      Component: thumbnails.SvgLayer,
     },
     {
       label: 'A11y Validator',
       href: '/components/unstable-a11y',
-      Component: SvgUnstableA11y,
+      Component: thumbnails.SvgUnstableA11y,
     },
     {
       label: 'Tokens',
       href: '/components/tokens',
-      Component: SvgTokens,
+      Component: thumbnails.SvgTokens,
     },
     {
       label: 'Block',
       href: '/components/block',
-      Component: SvgBlock,
+      Component: thumbnails.SvgBlock,
     },
   ],
 };
