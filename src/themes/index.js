c219a15998868c14423b85b1af6cd69dff94ab90
Nadiia <nadiia.dmytrenko@gmail.com>
2019-09-26 11:42:55 -0700

    fix(dark-theme): export dark theme overrides for the dark theme creation (#1963)

--- src/themes/index.js
+++ src/themes/index.js
@@ -8,6 +8,7 @@ LICENSE file in the root directory of this source tree.
 import createTheme from './creator.js';
 import {primitives as lightThemePrimitives} from './light-theme-primitives.js';
 import {primitives as darkThemePrimitives} from './dark-theme-primitives.js';
+import darkThemeOverrides from './dark-theme-colors.js';
 import {LightThemeMove} from './light-theme-with-move.js';
 import {DarkThemeMove} from './dark-theme-with-move.js';
 import {DarkTheme} from './dark-theme.js';
@@ -21,6 +22,7 @@ export {
   LightThemeMove,
   lightThemePrimitives,
   darkThemePrimitives,
+  darkThemeOverrides,
   DarkTheme,
   DarkThemeMove,
 };
