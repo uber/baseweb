616ec6c30359bc04e2b410ccb5384495d37d102c
Graham Murdoch <grahammurdoch8@gmail.com>
2019-09-27 15:55:40 -0700

    feat: add accordion

---
+++ src/accordion/__tests__/accordion.vrt.js
@@ -0,0 +1,15 @@
+/*
+Copyright (c) 2018-2019 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+/* eslint-env node */
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+const vrt = require('../../../vrt');
+
+vrt.test({
+  name: 'accordion',
+});
