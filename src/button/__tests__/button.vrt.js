1ab547931e2d8137e705bdb6d9d31af7431d4709
Graham Murdoch <grahammurdoch8@gmail.com>
2019-09-30 16:01:48 -0700

    feat: reintro differencify

--- src/button/__tests__/button.vrt.js
+++ src/button/__tests__/button.vrt.js
@@ -8,11 +8,35 @@ LICENSE file in the root directory of this source tree.
 /* eslint-env node */
 /* eslint-disable flowtype/require-valid-file-annotation */
 
+const Differencify = require('differencify');
 const {getPuppeteerUrl} = require('../../../e2e/helpers');
 
 it('testing normal stuff', async () => {
   await page.goto(getPuppeteerUrl('button'));
   await page.waitFor('#root');
+});
+
+it('testing differencify stuff', async () => {
+  // spin up differencify
+  const differencify = new Differencify({debug: true});
+  await differencify.launchBrowser({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+  const target = differencify.init({chain: false});
+  const page = await target.newPage();
+
+  await page.goto(getPuppeteerUrl('button'));
+  await page.setViewport({width: 1600, height: 1200});
+
+  const root = await page.$('#root');
+  const image = await root.screenshot();
+  const result = await target.toMatchSnapshot(image);
+
+  expect(result).toEqual(true);
+
+  // clean up differencify
+  await page.close();
+  await differencify.cleanup();
 });
 
 /*
