7bf93fb5d069fb6a55a8e27dac03cd3b75ba6226
Graham Murdoch <grahammurdoch8@gmail.com>
2019-09-30 16:17:29 -0700

    feat: try chaining commands

--- src/button/__tests__/button.vrt.js
+++ src/button/__tests__/button.vrt.js
@@ -8,35 +8,39 @@ LICENSE file in the root directory of this source tree.
 /* eslint-env node */
 /* eslint-disable flowtype/require-valid-file-annotation */
 
-const Differencify = require('differencify');
 const {getPuppeteerUrl} = require('../../../e2e/helpers');
+
+const Differencify = require('differencify');
 
 it('testing normal stuff', async () => {
   await page.goto(getPuppeteerUrl('button'));
   await page.waitFor('#root');
 });
 
-it('testing differencify stuff', async () => {
-  // spin up differencify
+describe('differencify', () => {
   const differencify = new Differencify({debug: true});
-  await differencify.launchBrowser({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+
+  beforeAll(async () => {
+    await differencify.launchBrowser({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
   });
-  const target = differencify.init({chain: false});
-  const page = await target.newPage();
 
-  await page.goto(getPuppeteerUrl('button'));
-  await page.setViewport({width: 1600, height: 1200});
+  afterAll(async () => {
+    await differencify.cleanup();
+  });
 
-  const root = await page.$('#root');
-  const image = await root.screenshot();
-  const result = await target.toMatchSnapshot(image);
-
-  expect(result).toEqual(true);
-
-  // clean up differencify
-  await page.close();
-  await differencify.cleanup();
+  it('button', async () => {
+    await differencify
+      .init()
+      .launch()
+      .newPage()
+      .goto(getPuppeteerUrl('button'))
+      .screenshot()
+      .toMatchSnapshot()
+      .close()
+      .end();
+  });
 });
 
 /*
