bf82cae85522de6c179192d9b22120e8aa64160d
Nadiia Dmytrenko <nadiia@uber.com>
2019-10-01 11:22:06 -0700

    fix: remove dist before docs build

--- scripts/build-package-json.js
+++ scripts/build-package-json.js
@@ -13,33 +13,9 @@ LICENSE file in the root directory of this source tree.
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
-const recursive = require('recursive-readdir');
 
-const asyncRecursive = util.promisify(recursive);
 const asyncReaddir = util.promisify(fs.readdir);
 const asyncStat = util.promisify(fs.stat);
-
-const packageJson = require('../package.json');
-
-async function run() {
-  const files = await asyncRecursive(path.resolve(__dirname, '../dist/esm'));
-  const projectRelativePaths = files.map(file => file.split('dist/esm')[1]);
-
-  const modules = projectRelativePaths.reduce((acc, current) => {
-    if (current.endsWith('index.js')) {
-      acc[current] = `/esm${current}`;
-    }
-    return acc;
-  }, {});
-
-  packageJson.module = modules;
-  fs.writeFileSync(
-    path.join(__dirname, '../dist/package.json'),
-    JSON.stringify(packageJson, null, 2),
-  );
-}
-
-run();
 
 async function buildPackageJsonFiles() {
   const distPath = path.resolve(__dirname, '../dist');
