ff6e441e9d1a44c1c2b7b1c1df73737a75a7a33f
Vojtech Miksu <vojtech@uber.com>
2019-10-01 16:53:31 -0700

    feat(yard): fix tsc

--- documentation-site/@types/just-omit.d.ts
+++ documentation-site/@types/just-omit.d.ts
@@ -1,6 +1,6 @@
 declare module 'just-omit' {
-  declare function omit<T, U extends keyof T>(obj: T, select: U[]): Omit<T, U>;
-  declare function omit<T, U extends keyof T>(
+  function omit<T, U extends keyof T>(obj: T, select: U[]): Omit<T, U>;
+  function omit<T, U extends keyof T>(
     obj: T,
     select1: U,
     ...selectn: U[]
