da99ebfa87684cb8d745a87b2890713abcf1726d
Graham Murdoch <grahammurdoch8@gmail.com>
2019-10-02 15:33:01 -0700

    feat: try uploading artifacts in ci

--- vrt/index.js
+++ vrt/index.js
@@ -8,9 +8,14 @@ LICENSE file in the root directory of this source tree.
 /* eslint-disable flowtype/require-valid-file-annotation */
 /* eslint-env node */
 
+const fs = require('fs');
 const globby = require('globby');
-const {toMatchImageSnapshot} = require('jest-image-snapshot');
+const {configureToMatchImageSnapshot} = require('jest-image-snapshot');
 const {mount} = require('../e2e/helpers');
+
+const toMatchImageSnapshot = configureToMatchImageSnapshot({
+  customDiffDir: '__vrt_diffs__',
+});
 
 expect.extend({toMatchImageSnapshot});
 
@@ -24,23 +29,21 @@ function vrt(componentName, interactionsByScenario = {}) {
     getScenariosForComponent(componentName).forEach(scenarioName => {
       it(scenarioName, async () => {
         const root = await prepare(page, scenarioName);
-
         const image = await root.screenshot();
         expect(image).toMatchImageSnapshot({
-          customSnapshotIdentifier: `${scenarioName}`,
+          customSnapshotIdentifier: scenarioName,
         });
       });
 
       if (interactionsByScenario[scenarioName]) {
         interactionsByScenario[scenarioName].forEach(interaction => {
-          it(`${scenarioName}__${interaction.name}`, async () => {
+          const testName = `${scenarioName}__${interaction.name}`;
+          it(testName, async () => {
             const root = await prepare(page, scenarioName);
-            // interaction `behavior` should get the page into the correct state for a snapshot
-            // currently only one snapshot can be taken per interaction
             await interaction.behavior(page);
             const image = await root.screenshot();
             expect(image).toMatchImageSnapshot({
-              customSnapshotIdentifier: `${scenarioName}__${interaction.name}`,
+              customSnapshotIdentifier: testName,
             });
           });
         });
@@ -51,6 +54,27 @@ function vrt(componentName, interactionsByScenario = {}) {
 
 /**
  *
+ * A function for capturing an image of a failed test. This will be more useful
+ * for E2E tests in the future but saving just in case.
+ * @param {ImageBuffer} image - An image buffer
+ * @param {string} name - The name of the test and, if neccessary, the file to write.
+ */
+async function snapshot(image, name) {
+  try {
+    expect(image).toMatchImageSnapshot({
+      customSnapshotIdentifier: name,
+    });
+  } catch (er) {
+    const path = name + '.png';
+    await fs.writeFile(path, image);
+    printToBuildKiteConsole(path);
+    throw er;
+  }
+}
+
+/**
+ *
+ * A function wrapper around the e2e helper, `mount`, that freezes animations and waits until the `#root` element is ready.
  * @param {Page} page - An instance of a Puppeteer Page. https://pptr.dev/#?product=Puppeteer&version=v1.20.0&show=api-class-page
  * @param {string} scenarioName - The name of a scenario to mount for the test. This correlates to the `name` parameter in a Storybook URL.
  */
@@ -75,12 +99,24 @@ animation: none !important;
 
 /**
  *
+ * A function for grabbing all scenarios for a component. Just looks for `*.scenario.js`
+ * files in the specfied directory.
  * @param {string} componentName - The component directory to look for scenarios in. Ex: `button`, `datepicker`...
  */
 function getScenariosForComponent(componentName) {
   return globby
     .sync('./*.scenario.js', {cwd: `src/${componentName}/__tests__`})
     .map(file => file.split('.scenario.js')[0]);
+}
+
+/**
+ * A function for logging a file to the BuildKite console.
+ * @param {string} path - The path to the file to log.
+ */
+function printToBuildKiteConsole(path) {
+  if (process.env.CI) {
+    console.log(`\u001B]1338;url="artifact://${path}";alt="Screenshot"\u0007`);
+  }
 }
 
 module.exports = {
