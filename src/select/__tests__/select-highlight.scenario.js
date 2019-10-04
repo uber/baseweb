21bf4068fea55ffd69da1a460d1c336c3f0af12f
Ashish Kale <ashish.kale@uber.com>
2019-10-04 11:19:08 +0530

    fix: add screener test

--- src/select/__tests__/select-highlight.scenario.js
+++ src/select/__tests__/select-highlight.scenario.js
@@ -7,27 +7,36 @@ LICENSE file in the root directory of this source tree.
 // @flow
 
 import * as React from 'react';
-
 import {Select} from '../index.js';
+// eslint-disable-next-line import/extensions
+import Screener, {Steps} from 'screener-storybook/src/screener';
 
 export const name = 'select-highlight';
-
 export const component = () => {
   const [value, setValue] = React.useState([]);
+  const input = 'div[data-baseweb=select] > div:nth-child(1)';
   return (
-    <Select
-      options={[
-        {id: 'AliceBlue', color: '#F0F8FF'},
-        {id: 'AntiqueWhite', color: '#FAEBD7'},
-        {id: 'Aqua', color: '#00FFFF'},
-        {id: 'Aquamarine', color: '#7FFFD4'},
-        {id: 'Azure', color: '#F0FFFF'},
-        {id: 'Beige', color: '#F5F5DC'},
-      ]}
-      labelKey="id"
-      valueKey="color"
-      onChange={({value}) => setValue(value)}
-      value={[{id: 'Beige', color: '#F5F5DC'}]}
-    />
+    <Screener
+      steps={new Steps()
+        .wait(input)
+        .click(input)
+        .snapshot('Select - highlight selected option')
+        .end()}
+    >
+      <Select
+        options={[
+          {id: 'AliceBlue', color: '#F0F8FF'},
+          {id: 'AntiqueWhite', color: '#FAEBD7'},
+          {id: 'Aqua', color: '#00FFFF'},
+          {id: 'Aquamarine', color: '#7FFFD4'},
+          {id: 'Azure', color: '#F0FFFF'},
+          {id: 'Beige', color: '#F5F5DC'},
+        ]}
+        labelKey="id"
+        valueKey="color"
+        onChange={({value}) => setValue(value)}
+        value={[{id: 'Beige', color: '#F5F5DC'}]}
+      />
+    </Screener>
   );
 };
