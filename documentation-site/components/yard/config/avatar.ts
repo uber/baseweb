2b6ee6a346010d6b019184b6374da09677f6a41b
Chase Starr <chasestarr@gmail.com>
2019-10-04 09:15:36 -0700

    docs(yard): adds yard to avatar page (#2016)

---
+++ documentation-site/components/yard/config/avatar.ts
@@ -0,0 +1,46 @@
+import {Avatar} from 'baseui/avatar';
+import {PropTypes} from '../const';
+import {TConfig} from '../types';
+
+const AvatarConfig: TConfig = {
+  imports: {
+    'baseui/avatar': {
+      named: ['Avatar'],
+    },
+  },
+  scope: {Avatar},
+  theme: [],
+  props: {
+    name: {
+      value: 'Jane Doe',
+      type: PropTypes.String,
+      description: 'Alternative text description of the image.',
+    },
+    size: {
+      value: 'scale1200',
+      type: PropTypes.String,
+      description:
+        'Defines the width/height of the image. Accepts labels from theme.sizing, or passes value to height/width.',
+    },
+    src: {
+      value: 'https://api.adorable.io/avatars/285/10@adorable.io.png',
+      type: PropTypes.String,
+      description: 'Image to display.',
+    },
+    overrides: {
+      value: undefined,
+      type: PropTypes.Overrides,
+      description: 'Lets you customize all aspects of the component.',
+      names: ['Root', 'Avatar', 'Initials'],
+      sharedProps: {
+        $size: 'size',
+        $disImageFailToLoad: {
+          type: PropTypes.Boolean,
+          description: 'True when the src does not resolve to a valid image.',
+        },
+      },
+    },
+  },
+};
+
+export default AvatarConfig;
