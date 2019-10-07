854f8b89dd49e36123db25804c4e24a52c9526f4
Gergely Nemeth <gnemeth@uber.com>
2019-10-07 15:41:24 -0700

    docs(yard): add card

---
+++ documentation-site/components/yard/config/card.ts
@@ -0,0 +1,58 @@
+import {Card, StyledBody, StyledAction} from 'baseui/card';
+import {PropTypes} from '../const';
+import {TConfig} from '../types';
+
+const TabsConfig: TConfig = {
+  imports: {
+    'baseui/card': {named: ['Card']},
+  },
+  scope: {
+    Card,
+    StyledBody,
+    StyledAction,
+  },
+  theme: [],
+  props: {
+    children: {
+      value: `<StyledBody>
+  Proin ut dui sed metus pharetra hend rerit vel non mi. Nulla
+  ornare faucibus ex, non facilisis nisl.
+</StyledBody>`,
+      type: PropTypes.ReactNode,
+      description: `An array of Tab components.`,
+      imports: {
+        'baseui/card': {named: ['StyledBody']},
+      },
+    },
+    title: {
+      value: undefined,
+      placeholder: 'I am a card',
+      type: PropTypes.String,
+      description: 'Title to be displayed in the card.',
+    },
+    headerimage: {
+      value: undefined,
+      placeholder: 'https://source.unsplash.com/user/erondu/700x400',
+      type: PropTypes.String,
+      description: 'Image to be displayed in the card.',
+    },
+    overrides: {
+      value: undefined,
+      type: PropTypes.Overrides,
+      description: 'Lets you customize all aspects of the component.',
+      names: [
+        'Action',
+        'Body',
+        'Contents',
+        'HeaderImage',
+        'Root',
+        'Thumbnail',
+        'Title',
+        'Wrapper',
+      ],
+      sharedProps: {},
+    },
+  },
+};
+
+export default TabsConfig;
