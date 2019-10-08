4880a0221b8606fbc5d497ad2abc6f7620c2c43e
Gergely Nemeth <gergelyke@users.noreply.github.com>
2019-10-08 10:15:09 -0700

    docs(yard): add card (#2035)

---
+++ documentation-site/components/yard/config/card.ts
@@ -0,0 +1,68 @@
+import {Card, StyledBody, StyledAction} from 'baseui/card';
+import {Button} from 'baseui/button';
+import {PropTypes} from '../const';
+import {TConfig} from '../types';
+
+const TabsConfig: TConfig = {
+  imports: {
+    'baseui/card': {named: ['Card']},
+    'baseui/button': {named: ['Button']},
+  },
+  scope: {
+    Card,
+    StyledBody,
+    StyledAction,
+    Button,
+  },
+  theme: [],
+  props: {
+    children: {
+      value: `<StyledBody>
+  Proin ut dui sed metus pharetra hend rerit vel non mi. Nulla
+  ornare faucibus ex, non facilisis nisl. Proin ut dui sed metus
+  pharetra hend rerit vel non mi. Nulla
+  ornare faucibus ex, non facilisis nisl.
+</StyledBody>
+<StyledAction>
+<Button overrides={{BaseButton: {style: {width: '100%'}}}}>
+  Button Label
+</Button>
+</StyledAction>`,
+      type: PropTypes.ReactNode,
+      description: `An array of Tab components.`,
+      imports: {
+        'baseui/card': {named: ['StyledBody', 'StyledAction']},
+      },
+    },
+    title: {
+      value: undefined,
+      placeholder: 'I am a card',
+      type: PropTypes.String,
+      description: 'Title to be displayed in the card.',
+    },
+    headerImage: {
+      value: undefined,
+      placeholder: 'https://source.unsplash.com/user/erondu/700x400',
+      type: PropTypes.String,
+      description: 'Image to be displayed in the card.',
+    },
+    overrides: {
+      value: undefined,
+      type: PropTypes.Overrides,
+      description: 'Lets you customize all aspects of the component.',
+      names: [
+        'Action',
+        'Body',
+        'Contents',
+        'HeaderImage',
+        'Root',
+        'Thumbnail',
+        'Title',
+        'Wrapper',
+      ],
+      sharedProps: {},
+    },
+  },
+};
+
+export default TabsConfig;
