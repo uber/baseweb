fa3df82a0704b8b4e90a6ea842c382e9d711905c
Gergely Nemeth <gnemeth@uber.com>
2019-10-09 10:15:25 -0700

    docs(yard): add tooltip

---
+++ documentation-site/components/yard/config/tooltip.ts
@@ -0,0 +1,58 @@
+import {
+  StatefulTooltip,
+  ACCESSIBILITY_TYPE,
+  PLACEMENT,
+  TRIGGER_TYPE,
+} from 'baseui/tooltip';
+import {PropTypes} from '../const';
+import {TConfig} from '../types';
+
+import PopoverConfig from './popover';
+
+const TooltipConfig: TConfig = {
+  ...PopoverConfig,
+  imports: {
+    'baseui/tooltip': {named: ['StatefulTooltip']},
+  },
+  scope: {
+    StatefulTooltip,
+    ACCESSIBILITY_TYPE,
+    PLACEMENT,
+    TRIGGER_TYPE,
+  },
+  theme: [],
+  props: {
+    ...PopoverConfig.props,
+    placement: {
+      ...PopoverConfig.props.placement,
+      imports: {
+        'baseui/tooltip': {
+          named: ['PLACEMENT'],
+        },
+      },
+    },
+    triggerType: {
+      ...PopoverConfig.props.triggerType,
+      imports: {
+        'baseui/tooltip': {
+          named: ['TRIGGER_TYPE'],
+        },
+      },
+    },
+    accessibilityType: {
+      ...PopoverConfig.props.accessibilityType,
+      imports: {
+        'baseui/tooltip': {
+          named: ['ACCESSIBILITY_TYPE'],
+        },
+      },
+    },
+    children: {
+      value: `Hover me`,
+      type: PropTypes.ReactNode,
+      description: `The content that will trigger the popover.`,
+    },
+  },
+};
+
+export default TooltipConfig;
