7ce02d46948b2c53a934f192cca21e1f185c0989
Gergely Nemeth <gergelyke@users.noreply.github.com>
2019-10-09 14:05:04 -0700

    docs(zeit): add support for forks (#2052)

--- scripts/link-checker.sh
+++ scripts/link-checker.sh
@@ -4,7 +4,15 @@ set -e
 
 branchUrl=$(echo $BUILDKITE_BRANCH | tr /: -)
 branchUrl=$(echo $branchUrl | tr -d ._)
-url="https://baseui-git-$branchUrl.uber-ui-platform.now.sh/"
+
+if [[ $BUILDKITE_PULL_REQUEST_REPO == *"uber-web/baseui"* ]]; then
+  url="https://baseui-git-$branchUrl.uber-ui-platform.now.sh/"
+else
+  author=$(echo $BUILDKITE_PULL_REQUEST_REPO | cut -d'/' -f 4)
+  url="https://baseui-git-fork-$author-$branchUrl.uber-ui-platform.now.sh/"
+fi
+
+echo "Checking url $url"
 
 # based on recent zeit builds, it can take up to 7 minutes to do the build
 attempt_counter=0
@@ -18,7 +26,7 @@ until $(curl --output /dev/null --silent --head --fail $url); do
 
     printf '.'
     attempt_counter=$(($attempt_counter+1))
-    sleep 15
+    sleep 30
 done
 
 yarn blc $url -ro --exclude components/avatar --exclude github.com
