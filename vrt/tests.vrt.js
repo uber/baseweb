faf9166c9014d0395ca33f6448eb64ed5bc35fb0
Graham Murdoch <grahammurdoch8@gmail.com>
2019-10-09 13:58:08 -0700

    refactor: prefer screenshot of element to reduce size by third

--- vrt/tests.vrt.js
+++ vrt/tests.vrt.js
@@ -8,10 +8,10 @@ LICENSE file in the root directory of this source tree.
 /* eslint-disable flowtype/require-valid-file-annotation */
 /* eslint-env node */
 
-const globby = require('globby');
 const {configureToMatchImageSnapshot} = require('jest-image-snapshot');
 const {mount} = require('../e2e/helpers');
-const interactions = require('./interactions.js');
+const config = require('./config.js');
+const {getAllScenarioNames} = require('./utils.js');
 
 const toMatchImageSnapshot = configureToMatchImageSnapshot({
   customDiffDir: '__vrt_diffs__',
@@ -22,22 +22,58 @@ expect.extend({toMatchImageSnapshot});
 
 describe('visual regression tests', () => {
   getAllScenarioNames().forEach(scenarioName => {
+    const {fullPage = false, interactions = [], selector = null, skip = false} =
+      config[scenarioName] || {};
+
+    if (skip) return;
+
     it(scenarioName, async () => {
-      await prepare(page, scenarioName);
-      const image = await page.screenshot({fullPage: true});
+      const root = await prepare(page, scenarioName);
+
+      let image;
+      if (selector) {
+        const elementHandle = page.$(selector);
+        image = await elementHandle.screenshot();
+      } else if (fullPage) {
+        image = await page.screenshot({fullPage: true});
+      } else {
+        image = await root.screenshot();
+      }
+
       expect(image).toMatchImageSnapshot({
         customSnapshotIdentifier: scenarioName,
       });
     });
 
-    if (interactions[scenarioName]) {
-      interactions[scenarioName].forEach(interaction => {
+    if (interactions.length > 0) {
+      interactions.forEach(interaction => {
         const testName = `${scenarioName}__${interaction.name}`;
+        const _fullPage = interaction.hasOwnProperty('fullPage')
+          ? interaction.fullPage
+          : fullPage;
+        const _selector = interaction.hasOwnProperty('selector')
+          ? interaction.selector
+          : selector;
+
         it(testName, async () => {
-          await prepare(page, scenarioName);
+          const root = await prepare(page, scenarioName);
+
+          // run interaction script
           await interaction.behavior(page);
+
+          // let things settle down
           await page.waitFor(1000);
-          const image = await page.screenshot({fullPage: true});
+
+          let image;
+          if (_selector) {
+            const elementHandle = page.$(_selector);
+            image = await elementHandle.screenshot();
+          } else if (_fullPage) {
+            image = await page.screenshot({fullPage: true});
+          } else {
+            image = await root.screenshot();
+          }
+
           expect(image).toMatchImageSnapshot({
             customSnapshotIdentifier: testName,
           });
@@ -48,7 +84,7 @@ describe('visual regression tests', () => {
 });
 
 async function prepare(page, scenarioName) {
-  // load page and wait to settle
+  // load page and let things settle down
   await mount(page, scenarioName);
 
   // freeze animations
@@ -61,11 +97,6 @@ animation: none !important;
 }`,
   });
 
-  await page.$('#root');
-}
-
-function getAllScenarioNames() {
-  return globby
-    .sync('src/**/*.scenario.js')
-    .map(filePath => filePath.match(/__tests__\/(.*).scenario/)[1]);
+  // return root element for screenshot
+  return await page.$('#root');
 }
