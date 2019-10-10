8b3b6ea653e8e55516c2f09be400aeb8d9eb2bcb
Graham Murdoch <grahammurdoch8@gmail.com>
2019-10-10 15:58:45 -0700

    fix: flow

--- jest.e2e.setup.js
+++ jest.e2e.setup.js
@@ -5,7 +5,7 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 
-// @flow
+/* eslint-disable flowtype/require-valid-file-annotation */
 /*eslint-env node*/
 
 const mkdirp = require('mkdirp');
