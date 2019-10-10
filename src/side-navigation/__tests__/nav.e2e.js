ba91b42c0a7cc77186aecb4a2037da71f4c9bfe8
Graham Murdoch <grahammurdoch8@gmail.com>
2019-10-09 17:57:38 -0700

    feat: add additional test for timeouts

--- src/side-navigation/__tests__/nav.e2e.js
+++ src/side-navigation/__tests__/nav.e2e.js
@@ -15,19 +15,17 @@ const selectors = {
 };
 
 describe('side navigation', () => {
-  e2e('passes basic a11y tests', async () => {
+  e2e('a11y test error', async () => {
     await mount(page, 'side-navigation');
-    // await page.waitFor('#foo');
     await page.waitFor(selectors.root);
+    const accessibilityReport = await analyzeAccessibility(page); // expect link failure
+    expect(accessibilityReport).toHaveNoAccessibilityIssues();
+  });
+
+  e2e('a11y timeout error', async () => {
+    await mount(page, 'side-navigation');
+    await page.waitFor('#foo'); // does not exist
     const accessibilityReport = await analyzeAccessibility(page);
-    // const accessibilityReport = await analyzeAccessibility(page, {
-    //   rules: [
-    //     {
-    //       id: 'skip-link',
-    //       enabled: false,
-    //     },
-    //   ],
-    // });
     expect(accessibilityReport).toHaveNoAccessibilityIssues();
   });
 });
