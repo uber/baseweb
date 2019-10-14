0716d7a624a83c767acc00d739a06d194fbda56f
Gergely Nemeth <gergelyke@users.noreply.github.com>
2019-10-14 08:46:32 -0700

    docs(yard): add progress-steps (#2058)
    * docs(yard): add progress-steps
    
    * docs(yard): add progress-steps
    
    * docs(yard): add ability to define custom transform in propHook

---
+++ documentation-site/components/yard/config/progress-steps-numbered.ts
@@ -0,0 +1,38 @@
+import {ProgressSteps, NumberedStep} from 'baseui/progress-steps';
+import {Button} from 'baseui/button';
+import {PropTypes} from '../const';
+import {TConfig} from '../types';
+
+import ProgressStepsConfig from './progress-steps';
+
+const ProgressStepsNumberedConfig: TConfig = {
+  ...ProgressStepsConfig,
+  scope: {
+    ProgressSteps,
+    NumberedStep,
+    Button,
+  },
+  props: {
+    ...ProgressStepsConfig.props,
+    children: {
+      value: `<NumberedStep title="Verify Address">
+  <p>Address on file: 1455 Market Street</p>
+  <Button size="compact" onClick={() => setCurrent(1)}>
+    Next
+  </Button>
+</NumberedStep>
+<NumberedStep title="Verify Payment">
+  Payment verified
+</NumberedStep>`,
+      type: PropTypes.ReactNode,
+      description: `An array of Tab components.`,
+      imports: {
+        'baseui/progress-steps': {named: ['NumberedStep']},
+        'baseui/button': {named: ['Button']},
+      },
+      propHook: ProgressStepsConfig.props.children.propHook,
+    },
+  },
+};
+
+export default ProgressStepsNumberedConfig;
