252caa9ad5dec65e0a7a1db96548c1c7ccddfaf3
Chase Starr <chasestarr@gmail.com>
2019-10-21 08:52:19 -0700

    feat(table-grid): stabilize the component (#2119)
    * feat(table-grid): stabilize the component
    
    * fix(table-grid): removes unstable warning from doc page

--- src/table-grid/index.js
+++ src/table-grid/index.js
@@ -6,12 +6,8 @@ LICENSE file in the root directory of this source tree.
 */
 // @flow
 
-import {
+export {
   StyledTable,
   StyledHeadCell,
   StyledBodyCell,
 } from './styled-components.js';
-
-export const Unstable_StyledTable = StyledTable;
-export const Unstable_StyledHeadCell = StyledHeadCell;
-export const Unstable_StyledBodyCell = StyledBodyCell;
