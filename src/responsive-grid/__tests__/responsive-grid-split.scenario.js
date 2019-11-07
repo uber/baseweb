cf94cc3f18909c21b476257fd2462a9b0028be96
Graham Murdoch <grahammurdoch8@gmail.com>
2019-11-06 17:16:00 -0800

    feat(responsive-grid): add split scenario

---
+++ src/responsive-grid/__tests__/responsive-grid-split.scenario.js
@@ -0,0 +1,37 @@
+/*
+Copyright (c) 2018-2019 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+
+import React from 'react';
+import {Grid, StyledCell as Cell} from '../index.js';
+
+export const name = 'responsive-grid-split';
+
+function Inner(props) {
+  return (
+    <div
+      style={{
+        border: 'solid 1px black',
+        padding: '8px',
+      }}
+      {...props}
+    />
+  );
+}
+
+export function component() {
+  return (
+    <Grid>
+      <Cell $span={[4, 4, 6]}>
+        <Inner>1</Inner>
+      </Cell>
+      <Cell $span={[4, 4, 6]}>
+        <Inner>2</Inner>
+      </Cell>
+    </Grid>
+  );
+}
