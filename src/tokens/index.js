62afdec722d135393d937f2210f45ac7e03acc03
Nadiia Dmytrenko <nadiia@uber.com>
2019-11-14 14:00:39 -0800

    feat(theme): refactor theming and add semantic tokens

--- src/tokens/index.js
+++ src/tokens/index.js
@@ -5,7 +5,12 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 // @flow
-import colors from './colors.js';
+export {default as colors} from './colors.js';
+export {
+  default as lightThemeSemanticColors,
+} from './light-theme-color-semantic.js';
+export {
+  default as darkThemeSemanticColors,
+} from './dark-theme-color-semantic.js';
 
-export {colors};
 export * from './types.js';
