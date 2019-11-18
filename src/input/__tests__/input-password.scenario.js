cd8687c22b740e753544d1f43af391c9c98583fd
Gergely Nemeth <gergelyke@users.noreply.github.com>
2019-11-18 11:15:25 -0700

    fix(input): height for password field (#2334)

--- src/input/__tests__/input-password.scenario.js
+++ src/input/__tests__/input-password.scenario.js
@@ -8,33 +8,47 @@ LICENSE file in the root directory of this source tree.
 /* global window */
 
 import React from 'react';
-import {StatefulInput} from '../index.js';
+import {StatefulInput, SIZE} from '../index.js';
 
 export const name = 'input-password';
 
 export const component = () => (
-  <form
-    onSubmit={event => {
-      event.preventDefault();
-      window.__e2e__formSubmitted__ = true;
-      return false;
-    }}
-  >
+  <React.Fragment>
     <StatefulInput
+      size={SIZE.compact}
       type="password"
       initialState={{value: '1234'}}
-      overrides={{
-        Input: {
-          props: {
-            'data-e2e': 'input',
-          },
-        },
-        MaskToggleButton: {
-          props: {
-            'data-e2e': 'mask-toggle',
-          },
-        },
-      }}
     />
-  </form>
+    <br />
+    <form
+      onSubmit={event => {
+        event.preventDefault();
+        window.__e2e__formSubmitted__ = true;
+        return false;
+      }}
+    >
+      <StatefulInput
+        type="password"
+        initialState={{value: '1234'}}
+        overrides={{
+          Input: {
+            props: {
+              'data-e2e': 'input',
+            },
+          },
+          MaskToggleButton: {
+            props: {
+              'data-e2e': 'mask-toggle',
+            },
+          },
+        }}
+      />
+    </form>
+    <br />
+    <StatefulInput
+      size={SIZE.large}
+      type="password"
+      initialState={{value: '1234'}}
+    />
+  </React.Fragment>
 );
