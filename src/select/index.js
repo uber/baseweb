6a7148e4dd412be53c9aa266bca09027a175a37e
Leyan Lo <leyan@uber.com>
2019-12-04 03:28:48 -0800

    address feedback and export StyledSearchIcon

--- src/select/index.js
+++ src/select/index.js
@@ -29,6 +29,8 @@ export {
   StyledIconsContainer,
   StyledSelectArrow,
   StyledClearIcon,
+  // TODO(v10): remove StyledSearchIconContainer as StyledSearchIcon
+  StyledSearchIconContainer as StyledSearchIcon,
   StyledSearchIconContainer,
   StyledDropdownContainer,
   StyledDropdown,
