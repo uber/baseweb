9680106105583d30e2c3e2111f8d1042b4eccda6
Chase Starr <starr@uber.com>
2019-12-09 14:52:55 -0800

    fix(datepicker): label should not have a height

--- src/datepicker/datepicker.js
+++ src/datepicker/datepicker.js
@@ -279,6 +279,7 @@ export default class Datepicker extends React.Component<
             <p
               id={this.props['aria-describedby']}
               style={{
+                position: 'fixed',
                 width: '1px',
                 height: '1px',
                 margin: '-1px',
