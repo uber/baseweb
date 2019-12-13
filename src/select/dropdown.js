715066454598c8f816544ff4a0bea53dd51c138a
Chase Starr <starr@uber.com>
2019-12-13 15:48:31 -0800

    fix(select): refrain from grouping optimizations

--- src/select/dropdown.js
+++ src/select/dropdown.js
@@ -13,169 +13,192 @@ import {
   StyledOptionContent,
 } from './styled-components.js';
 import {StatefulMenu} from '../menu/index.js';
-import type {DropdownPropsT, OptionT} from './types.js';
+import type {DropdownPropsT, OptionT, ValueT} from './types.js';
 import {getOverrides, mergeOverrides} from '../helpers/overrides.js';
 
-export default function SelectDropdown(props: DropdownPropsT) {
-  const {overrides = {}} = props;
-  const [OptionContent, optionContentProps] = getOverrides(
-    overrides.OptionContent,
-    StyledOptionContent,
+function groupOptions(options: ValueT) {
+  return options.reduce(
+    (groups, option) => {
+      if (option.__optgroup) {
+        if (!groups[option.__optgroup]) {
+          groups[option.__optgroup] = [];
+        }
+        groups[option.__optgroup].push(option);
+      } else {
+        groups.__ungrouped.push(option);
+      }
+      return groups;
+    },
+    {__ungrouped: []},
   );
-  const [DropdownContainer, dropdownContainerProps] = getOverrides(
-    overrides.DropdownContainer,
-    StyledDropdownContainer,
-  );
-  const [ListItem, listItemProps] = getOverrides(
-    overrides.DropdownListItem,
-    StyledDropdownListItem,
-  );
-  const [
-    OverriddenStatefulMenu,
-    // $FlowFixMe
-    {overrides: statefulMenuOverrides = {}, ...restStatefulMenuProps},
-  ] = getOverrides(overrides.StatefulMenu, StatefulMenu);
+}
 
-  const sharedProps = {
-    $error: props.error,
-    $isLoading: props.isLoading,
-    $multi: props.multi,
-    $required: props.required,
-    $searchable: props.searchable,
-    $size: props.size,
-    $type: props.type,
-    $width: props.width,
+export default class SelectDropdown extends React.Component<DropdownPropsT> {
+  getSharedProps() {
+    const {
+      error,
+      isLoading,
+      multi,
+      required,
+      size,
+      searchable,
+      type,
+      width,
+    } = this.props;
+    return {
+      $error: error,
+      $isLoading: isLoading,
+      $multi: multi,
+      $required: required,
+      $searchable: searchable,
+      $size: size,
+      $type: type,
+      $width: width,
+    };
+  }
+  // eslint-disable-next-line flowtype/no-weak-types
+  getItemLabel = (option: {[string]: any}) => {
+    const {getOptionLabel, overrides = {}, value, valueKey} = this.props;
+
+    const [OptionContent, optionContentProps] = getOverrides(
+      overrides.OptionContent,
+      StyledOptionContent,
+    );
+
+    let $selected;
+    if (Array.isArray(value)) {
+      $selected = !!value.find(
+        selected => selected && selected[valueKey] === option[valueKey],
+      );
+    } else {
+      $selected = value[valueKey] === option[valueKey];
+    }
+
+    const optionSharedProps = {
+      $selected,
+      $disabled: option.disabled,
+      $isHighlighted: option.isHighlighted,
+    };
+    return (
+      <OptionContent
+        aria-readonly={option.disabled}
+        aria-selected={$selected}
+        key={option[valueKey]}
+        {...this.getSharedProps()}
+        {...optionSharedProps}
+        {...optionContentProps}
+      >
+        {getOptionLabel({option, optionState: optionSharedProps})}
+      </OptionContent>
+    );
   };
 
-  const getItemLabel = React.useCallback(
-    (option: OptionT) => {
-      let $selected;
-      if (Array.isArray(props.value)) {
-        $selected = !!props.value.find(
-          selected =>
-            selected && selected[props.valueKey] === option[props.valueKey],
-        );
-      } else {
-        $selected = props.value[props.valueKey] === option[props.valueKey];
-      }
-
-      const optionSharedProps = {
-        $selected,
-        $disabled: Boolean(option.disabled),
-        $isHighlighted: option.isHighlighted,
-      };
-      return (
-        <OptionContent
-          aria-readonly={option.disabled}
-          aria-selected={$selected}
-          key={option[props.valueKey]}
-          {...sharedProps}
-          {...optionSharedProps}
-          {...optionContentProps}
-        >
-          {props.getOptionLabel({option, optionState: optionSharedProps})}
-        </OptionContent>
-      );
-    },
-    [overrides, props.getOptionLabel, props.value, props.valueKey],
-  );
-
-  const onMouseDown = React.useCallback((e: Event) => {
+  onMouseDown = (e: Event) => {
     e.preventDefault();
-  }, []);
+  };
 
-  const highlightedIndex = React.useMemo(() => {
+  getHighlightedIndex = () => {
+    const {value, options, valueKey} = this.props;
     // Highlight only first value as menu supports only a single highlight index
     let firstValue: OptionT = {};
 
-    if (Array.isArray(props.value) && props.value.length > 0) {
-      firstValue = props.value[0];
-    } else if (!(props.value instanceof Array)) {
-      firstValue = props.value;
+    if (Array.isArray(value) && value.length > 0) {
+      firstValue = value[0];
+    } else if (!(value instanceof Array)) {
+      firstValue = value;
     }
 
     if (Object.keys(firstValue).length > 0) {
-      const a = props.options.findIndex(
-        option =>
-          option && option[props.valueKey] === firstValue[props.valueKey],
+      const a = options.findIndex(
+        option => option && option[valueKey] === firstValue[valueKey],
       );
       return a || 0;
     }
     return 0;
-  }, [props.value, props.options, props.valueKey]);
+  };
 
-  const groupedOptions = React.useMemo(() => {
-    return props.options.reduce(
-      (groups, option) => {
-        if (option.__optgroup) {
-          if (!groups[option.__optgroup]) {
-            groups[option.__optgroup] = [];
-          }
-          groups[option.__optgroup].push(option);
-        } else {
-          groups.__ungrouped.push(option);
-        }
-        return groups;
-      },
-      {__ungrouped: []},
+  render() {
+    // TODO(#185) Add no-results and loading states to menu
+    const {
+      maxDropdownHeight,
+      multi,
+      noResultsMsg,
+      onItemSelect,
+      options = [],
+      overrides = {},
+      size,
+    } = this.props;
+    const [DropdownContainer, dropdownContainerProps] = getOverrides(
+      overrides.DropdownContainer,
+      StyledDropdownContainer,
     );
-  }, [props.options]);
-
-  return (
-    <DropdownContainer
-      ref={props.innerRef}
-      role="listbox"
-      {...sharedProps}
-      {...dropdownContainerProps}
-    >
-      <OverriddenStatefulMenu
-        noResultsMsg={props.noResultsMsg}
-        onItemSelect={props.onItemSelect}
-        items={groupedOptions}
-        size={props.size}
-        initialState={{
-          isFocused: true,
-          highlightedIndex: highlightedIndex,
-        }}
-        overrides={mergeOverrides(
-          {
-            List: {
-              component: StyledDropdown,
-              style: p => ({
-                maxHeight: p.$maxHeight || null,
-              }),
-              props: {
-                $maxHeight: props.maxDropdownHeight,
-                'aria-multiselectable': props.multi,
+    const [ListItem, listItemProps] = getOverrides(
+      overrides.DropdownListItem,
+      StyledDropdownListItem,
+    );
+    const [
+      OverriddenStatefulMenu,
+      // $FlowFixMe
+      {overrides: statefulMenuOverrides = {}, ...restStatefulMenuProps},
+    ] = getOverrides(overrides.StatefulMenu, StatefulMenu);
+    const highlightedIndex = this.getHighlightedIndex();
+    const groupedOptions = groupOptions(options);
+    return (
+      <DropdownContainer
+        ref={this.props.innerRef}
+        role="listbox"
+        {...this.getSharedProps()}
+        {...dropdownContainerProps}
+      >
+        <OverriddenStatefulMenu
+          noResultsMsg={noResultsMsg}
+          onItemSelect={onItemSelect}
+          items={groupedOptions}
+          size={size}
+          initialState={{
+            isFocused: true,
+            highlightedIndex: highlightedIndex,
+          }}
+          overrides={mergeOverrides(
+            {
+              List: {
+                component: StyledDropdown,
+                style: p => ({
+                  maxHeight: p.$maxHeight || null,
+                }),
+                props: {
+                  $maxHeight: maxDropdownHeight,
+                  'aria-multiselectable': multi,
+                },
               },
-            },
-            Option: {
-              props: {
-                getItemLabel,
-                // figure out why the onClick handler is not
-                // triggered without this temporary fix
-                onMouseDown: onMouseDown,
-                overrides: {
-                  ListItem: {
-                    component: ListItem,
-                    props: {...listItemProps, role: 'option'},
-                    // slightly hacky way to handle the list item style overrides
-                    // since the menu component doesn't provide a top level overrides for it
-                    // $FlowFixMe
-                    style: listItemProps.$style,
+              Option: {
+                props: {
+                  getItemLabel: this.getItemLabel,
+                  // figure out why the onClick handler is not
+                  // triggered without this temporary fix
+                  onMouseDown: this.onMouseDown,
+                  overrides: {
+                    ListItem: {
+                      component: ListItem,
+                      props: {...listItemProps, role: 'option'},
+                      // slightly hacky way to handle the list item style overrides
+                      // since the menu component doesn't provide a top level overrides for it
+                      // $FlowFixMe
+                      style: listItemProps.$style,
+                    },
                   },
                 },
               },
             },
-          },
-          {
-            List: overrides.Dropdown || {},
-            Option: overrides.DropdownOption || {},
-            ...statefulMenuOverrides,
-          },
-        )}
-        {...restStatefulMenuProps}
-      />
-    </DropdownContainer>
-  );
+            {
+              List: overrides.Dropdown || {},
+              Option: overrides.DropdownOption || {},
+              ...statefulMenuOverrides,
+            },
+          )}
+          {...restStatefulMenuProps}
+        />
+      </DropdownContainer>
+    );
+  }
 }
