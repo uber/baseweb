038c0ba4231f4faa86a873ed680ff2cb8799c36c
Graham Murdoch <murdoch@uber.com>
2019-12-17 11:12:07 -0800

    fix: missing typography types

---
+++ src/typography/types.js
@@ -0,0 +1,51 @@
+/*
+Copyright (c) 2018-2019 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+import * as React from 'react';
+import type {BlockPropsT} from '../block/types.js';
+
+export type DisplayLarge = React.StatelessFunctionalComponent<BlockPropsT>;
+export type DisplayMedium = React.StatelessFunctionalComponent<BlockPropsT>;
+export type DisplaySmall = React.StatelessFunctionalComponent<BlockPropsT>;
+export type DisplayXSmall = React.StatelessFunctionalComponent<BlockPropsT>;
+export type HeadingXXLarge = React.StatelessFunctionalComponent<BlockPropsT>;
+export type HeadingXLarge = React.StatelessFunctionalComponent<BlockPropsT>;
+export type HeadingLarge = React.StatelessFunctionalComponent<BlockPropsT>;
+export type HeadingMedium = React.StatelessFunctionalComponent<BlockPropsT>;
+export type HeadingSmall = React.StatelessFunctionalComponent<BlockPropsT>;
+export type HeadingXSmall = React.StatelessFunctionalComponent<BlockPropsT>;
+export type LabelLarge = React.StatelessFunctionalComponent<BlockPropsT>;
+export type LabelMedium = React.StatelessFunctionalComponent<BlockPropsT>;
+export type LabelSmall = React.StatelessFunctionalComponent<BlockPropsT>;
+export type LabelXSmall = React.StatelessFunctionalComponent<BlockPropsT>;
+export type ParagraphLarge = React.StatelessFunctionalComponent<BlockPropsT>;
+export type ParagraphMedium = React.StatelessFunctionalComponent<BlockPropsT>;
+export type ParagraphSmall = React.StatelessFunctionalComponent<BlockPropsT>;
+export type ParagraphXSmall = React.StatelessFunctionalComponent<BlockPropsT>;
+
+// TODO(v10): remove
+export type Display = React.StatelessFunctionalComponent<BlockPropsT>;
+export type Display1 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type Display2 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type Display3 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type Display4 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type H1 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type H2 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type H3 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type H4 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type H5 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type H6 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type Paragraph1 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type Paragraph2 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type Paragraph3 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type Paragraph4 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type Label1 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type Label2 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type Label3 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type Label4 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type Caption1 = React.StatelessFunctionalComponent<BlockPropsT>;
+export type Caption2 = React.StatelessFunctionalComponent<BlockPropsT>;
