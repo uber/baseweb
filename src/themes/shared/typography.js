a4458e98760f2ca24f40f292eac422faa1380503
Gergely Nemeth <gergelyke@users.noreply.github.com>
2019-12-18 10:31:54 -0800

    fix(theme): remove duplicate code (#2509)

--- src/themes/shared/typography.js
+++ src/themes/shared/typography.js
@@ -13,221 +13,152 @@ export const fontTokens: FontTokensT = {
 };
 
 // color constants
-export default (themePrimitives: FontTokensT = fontTokens): TypographyT => ({
-  font100: {
+export default (themePrimitives: FontTokensT = fontTokens): TypographyT => {
+  const font100 = {
     fontFamily: themePrimitives.primaryFontFamily,
     fontSize: '12px',
     fontWeight: 'normal',
     lineHeight: '20px',
-  },
-  font150: {
+  };
+  const font150 = {
     fontFamily: themePrimitives.primaryFontFamily,
     fontSize: '12px',
     fontWeight: 500,
     lineHeight: '20px',
-  },
-  font200: {
+  };
+  const font200 = {
     fontFamily: themePrimitives.primaryFontFamily,
     fontSize: '14px',
     fontWeight: 'normal',
     lineHeight: '20px',
-  },
-  font250: {
+  };
+  const font250 = {
     fontFamily: themePrimitives.primaryFontFamily,
     fontSize: '14px',
     fontWeight: 500,
     lineHeight: '20px',
-  },
-  font300: {
+  };
+  const font300 = {
     fontFamily: themePrimitives.primaryFontFamily,
     fontSize: '16px',
     fontWeight: 'normal',
     lineHeight: '24px',
-  },
-  font350: {
+  };
+  const font350 = {
     fontFamily: themePrimitives.primaryFontFamily,
     fontSize: '16px',
     fontWeight: 500,
     lineHeight: '24px',
-  },
-  font400: {
+  };
+  const font400 = {
     fontFamily: themePrimitives.primaryFontFamily,
     fontSize: '18px',
     fontWeight: 'normal',
     lineHeight: '28px',
-  },
-  font450: {
+  };
+  const font450 = {
     fontFamily: themePrimitives.primaryFontFamily,
     fontSize: '18px',
     fontWeight: 500,
     lineHeight: '28px',
-  },
-  font550: {
+  };
+  const font550 = {
     fontFamily: themePrimitives.primaryFontFamily,
     fontSize: '20px',
     fontWeight: 500,
     lineHeight: '28px',
-  },
-  font650: {
+  };
+  const font650 = {
     fontFamily: themePrimitives.primaryFontFamily,
     fontSize: '24px',
     fontWeight: 500,
     lineHeight: '32px',
-  },
-  font750: {
+  };
+  const font750 = {
     fontFamily: themePrimitives.primaryFontFamily,
     fontSize: '28px',
     fontWeight: 500,
     lineHeight: '36px',
-  },
-  font850: {
+  };
+  const font850 = {
     fontFamily: themePrimitives.primaryFontFamily,
     fontSize: '32px',
     fontWeight: 500,
     lineHeight: '40px',
-  },
-  font950: {
+  };
+  const font950 = {
     fontFamily: themePrimitives.primaryFontFamily,
     fontSize: '36px',
     fontWeight: 500,
     lineHeight: '44px',
-  },
-  font1050: {
+  };
+  const font1050 = {
     fontFamily: themePrimitives.primaryFontFamily,
     fontSize: '40px',
     fontWeight: 500,
     lineHeight: '52px',
-  },
-  font1150: {
+  };
+  const font1150 = {
     fontFamily: themePrimitives.primaryFontFamily,
     fontSize: '36px',
     fontWeight: 500,
     lineHeight: '44px',
-  },
-  font1250: {
+  };
+  const font1250 = {
     fontFamily: themePrimitives.primaryFontFamily,
     fontSize: '44px',
     fontWeight: 500,
     lineHeight: '52px',
-  },
-  font1350: {
+  };
+  const font1350 = {
     fontFamily: themePrimitives.primaryFontFamily,
     fontSize: '52px',
     fontWeight: 500,
     lineHeight: '64px',
-  },
-  font1450: {
+  };
+  const font1450 = {
     fontFamily: themePrimitives.primaryFontFamily,
     fontSize: '96px',
     fontWeight: 500,
     lineHeight: '112px',
-  },
-  ParagraphXSmall: {
-    fontFamily: themePrimitives.primaryFontFamily,
-    fontSize: '12px',
-    fontWeight: 'normal',
-    lineHeight: '20px',
-  },
-  ParagraphSmall: {
-    fontFamily: themePrimitives.primaryFontFamily,
-    fontSize: '14px',
-    fontWeight: 'normal',
-    lineHeight: '20px',
-  },
-  ParagraphMedium: {
-    fontFamily: themePrimitives.primaryFontFamily,
-    fontSize: '16px',
-    fontWeight: 'normal',
-    lineHeight: '24px',
-  },
-  ParagraphLarge: {
-    fontFamily: themePrimitives.primaryFontFamily,
-    fontSize: '18px',
-    fontWeight: 'normal',
-    lineHeight: '28px',
-  },
-  LabelXSmall: {
-    fontFamily: themePrimitives.primaryFontFamily,
-    fontSize: '12px',
-    fontWeight: 500,
-    lineHeight: '20px',
-  },
-  LabelSmall: {
-    fontFamily: themePrimitives.primaryFontFamily,
-    fontSize: '14px',
-    fontWeight: 500,
-    lineHeight: '20px',
-  },
-  LabelMedium: {
-    fontFamily: themePrimitives.primaryFontFamily,
-    fontSize: '16px',
-    fontWeight: 500,
-    lineHeight: '24px',
-  },
-  LabelLarge: {
-    fontFamily: themePrimitives.primaryFontFamily,
-    fontSize: '18px',
-    fontWeight: 500,
-    lineHeight: '28px',
-  },
-  HeadingXSmall: {
-    fontFamily: themePrimitives.primaryFontFamily,
-    fontSize: '20px',
-    fontWeight: 500,
-    lineHeight: '28px',
-  },
-  HeadingSmall: {
-    fontFamily: themePrimitives.primaryFontFamily,
-    fontSize: '24px',
-    fontWeight: 500,
-    lineHeight: '32px',
-  },
-  HeadingMedium: {
-    fontFamily: themePrimitives.primaryFontFamily,
-    fontSize: '28px',
-    fontWeight: 500,
-    lineHeight: '36px',
-  },
-  HeadingLarge: {
-    fontFamily: themePrimitives.primaryFontFamily,
-    fontSize: '32px',
-    fontWeight: 500,
-    lineHeight: '40px',
-  },
-  HeadingXLarge: {
-    fontFamily: themePrimitives.primaryFontFamily,
-    fontSize: '36px',
-    fontWeight: 500,
-    lineHeight: '44px',
-  },
-  HeadingXXLarge: {
-    fontFamily: themePrimitives.primaryFontFamily,
-    fontSize: '40px',
-    fontWeight: 500,
-    lineHeight: '52px',
-  },
-  DisplayXSmall: {
-    fontFamily: themePrimitives.primaryFontFamily,
-    fontSize: '36px',
-    fontWeight: 500,
-    lineHeight: '44px',
-  },
-  DisplaySmall: {
-    fontFamily: themePrimitives.primaryFontFamily,
-    fontSize: '44px',
-    fontWeight: 500,
-    lineHeight: '52px',
-  },
-  DisplayMedium: {
-    fontFamily: themePrimitives.primaryFontFamily,
-    fontSize: '52px',
-    fontWeight: 500,
-    lineHeight: '64px',
-  },
-  DisplayLarge: {
-    fontFamily: themePrimitives.primaryFontFamily,
-    fontSize: '96px',
-    fontWeight: 500,
-    lineHeight: '112px',
-  },
-});
+  };
+
+  return {
+    font100,
+    font150,
+    font200,
+    font250,
+    font300,
+    font350,
+    font400,
+    font450,
+    font550,
+    font650,
+    font750,
+    font850,
+    font950,
+    font1050,
+    font1150,
+    font1250,
+    font1350,
+    font1450,
+    ParagraphXSmall: font100,
+    ParagraphSmall: font200,
+    ParagraphMedium: font300,
+    ParagraphLarge: font400,
+    LabelXSmall: font150,
+    LabelSmall: font250,
+    LabelMedium: font350,
+    LabelLarge: font450,
+    HeadingXSmall: font550,
+    HeadingSmall: font650,
+    HeadingMedium: font750,
+    HeadingLarge: font850,
+    HeadingXLarge: font950,
+    HeadingXXLarge: font1050,
+    DisplayXSmall: font1150,
+    DisplaySmall: font1250,
+    DisplayMedium: font1350,
+    DisplayLarge: font1450,
+  };
+};
