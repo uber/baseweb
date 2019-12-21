f0acd056b0b017b5e96b627888bd32b63236364e
Graham Murdoch <grahammurdoch8@gmail.com>
2019-12-20 20:36:05 -0800

    fix: ts

--- documentation-site/examples/spinner/next-custom.tsx
+++ documentation-site/examples/spinner/next-custom.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import {StyledSpinnerNext} from 'baseui/spinner';
+import {Theme} from 'baseui/theme';
 
 export default () => (
   <StyledSpinnerNext
-    $style={({$theme}) => ({
+    $style={({$theme}: {$theme: Theme}) => ({
       width: $theme.sizing.scale2400, // 96px
       height: $theme.sizing.scale2400, // 96px
       borderWidth: $theme.sizing.scale500, // 12px
