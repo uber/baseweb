989fbf3dacc54c27bc50592bb249761eec38147e
Graham Murdoch <murdoch@uber.com>
2019-12-23 14:18:14 -0800

    docs(spinner): update example to use withstyle rather than $style

--- documentation-site/examples/spinner/next-custom.js
+++ documentation-site/examples/spinner/next-custom.js
@@ -1,15 +1,17 @@
 // @flow
 
 import * as React from 'react';
+import {withStyle} from 'baseui';
 import {StyledSpinnerNext} from 'baseui/spinner';
 
-export default () => (
-  <StyledSpinnerNext
-    $style={({$theme}) => ({
-      width: $theme.sizing.scale2400, // 96px
-      height: $theme.sizing.scale2400, // 96px
-      borderWidth: $theme.sizing.scale500, // 12px
-      borderTopColor: $theme.colors.negative,
-    })}
-  />
+const ExtraLargeSpinner = withStyle<typeof StyledSpinnerNext, {}>(
+  StyledSpinnerNext,
+  ({$theme}) => ({
+    width: $theme.sizing.scale2400, // 96px
+    height: $theme.sizing.scale2400, // 96px
+    borderWidth: $theme.sizing.scale500, // 12px
+    borderTopColor: $theme.colors.negative,
+  }),
 );
+
+export default () => <ExtraLargeSpinner />;
