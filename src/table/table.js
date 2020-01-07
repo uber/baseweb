0794ecd8c0758a8b250c8b91738c284194d78b40
afzal.s <afzal.s@media.net>
2020-01-07 22:38:20 +0530

    feat(table): implement loading state

--- src/table/table.js
+++ src/table/table.js
@@ -17,7 +17,20 @@ import {
   StyledCell,
 } from './styled-components.js';
 
+import {ProgressBar} from '../progress-bar/index.js';
+import {styled} from '../styles/index.js';
+
 import type {TablePropsT} from './types.js';
+
+const EmptyWrapper = styled('div', props => {
+  const {$theme} = props;
+  return {
+    ...$theme.typography.font100,
+    marginTop: $theme.sizing.scale600,
+    marginBottom: $theme.sizing.scale600,
+    textAlign: 'center',
+  };
+});
 
 export default class Table extends React.Component<TablePropsT> {
   static defaultProps = {
@@ -27,26 +40,45 @@ export default class Table extends React.Component<TablePropsT> {
   };
 
   render() {
+    const {isLoading, columns, data, horizontalScrollWidth} = this.props;
     return (
       <StyledTable
         data-baseweb="table"
-        aria-colcount={this.props.columns.length}
-        aria-rowcount={this.props.data.length}
+        aria-colcount={columns.length}
+        aria-rowcount={data.length}
       >
-        <StyledHead $width={this.props.horizontalScrollWidth}>
-          {this.props.columns.map((column, index) => (
+        {isLoading && (
+          <ProgressBar
+            infinite
+            overrides={{
+              Bar: {
+                style: {
+                  marginBottom: 0,
+                  marginLeft: 0,
+                  marginRight: 0,
+                  marginTop: 0,
+                },
+              },
+            }}
+          />
+        )}
+        <StyledHead $width={horizontalScrollWidth}>
+          {columns.map((column, index) => (
             <StyledHeadCell key={index}>{column}</StyledHeadCell>
           ))}
         </StyledHead>
-
-        <StyledBody $width={this.props.horizontalScrollWidth}>
-          {this.props.data.map((row, index) => (
-            <StyledRow key={index}>
-              {row.map((cell, cellIndex) => (
-                <StyledCell key={cellIndex}>{cell}</StyledCell>
-              ))}
-            </StyledRow>
-          ))}
+        <StyledBody $width={horizontalScrollWidth}>
+          {data.length > 0 ? (
+            data.map((row, index) => (
+              <StyledRow key={index}>
+                {row.map((cell, cellIndex) => (
+                  <StyledCell key={cellIndex}>{cell}</StyledCell>
+                ))}
+              </StyledRow>
+            ))
+          ) : (
+            <EmptyWrapper>{isLoading ? 'Loading...' : 'No data'}</EmptyWrapper>
+          )}
         </StyledBody>
       </StyledTable>
     );
