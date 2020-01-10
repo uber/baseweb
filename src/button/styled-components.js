19d173dfb9c64440fe9d8949ff2675081a70c1d5
Graham Murdoch <grahammurdoch8@gmail.com>
2020-01-10 10:58:20 -0800

    fix(typography): update label line heights to spec (#2598)
    * fix(typography): update label line heights to spec
    
    * fix(button): adjust vertical padding to align to spec
    
    * fix(button): adjust padding for icon shapes
    
    * test(vrt): update visual snapshots for 765f001 [skip ci] (#2600)
    
    Co-authored-by: UberOpenSourceBot <33560951+UberOpenSourceBot@users.noreply.github.com>

--- src/button/styled-components.js
+++ src/button/styled-components.js
@@ -169,35 +169,35 @@ function getPaddingStyles({$theme, $size, $shape}) {
   switch ($size) {
     case SIZE.compact:
       return {
-        paddingTop: $theme.sizing.scale300,
-        paddingBottom: $theme.sizing.scale300,
+        paddingTop: $theme.sizing.scale400,
+        paddingBottom: $theme.sizing.scale400,
         paddingLeft: iconShape
-          ? $theme.sizing.scale200
+          ? $theme.sizing.scale400
           : $theme.sizing.scale500,
         paddingRight: iconShape
-          ? $theme.sizing.scale200
+          ? $theme.sizing.scale400
           : $theme.sizing.scale500,
       };
     case SIZE.large:
+      return {
+        paddingTop: $theme.sizing.scale600,
+        paddingBottom: $theme.sizing.scale600,
+        paddingLeft: iconShape
+          ? $theme.sizing.scale600
+          : $theme.sizing.scale700,
+        paddingRight: iconShape
+          ? $theme.sizing.scale600
+          : $theme.sizing.scale700,
+      };
+    default:
       return {
         paddingTop: $theme.sizing.scale550,
         paddingBottom: $theme.sizing.scale550,
         paddingLeft: iconShape
           ? $theme.sizing.scale550
-          : $theme.sizing.scale700,
-        paddingRight: iconShape
-          ? $theme.sizing.scale550
-          : $theme.sizing.scale700,
-      };
-    default:
-      return {
-        paddingTop: $theme.sizing.scale500,
-        paddingBottom: $theme.sizing.scale500,
-        paddingLeft: iconShape
-          ? $theme.sizing.scale500
           : $theme.sizing.scale600,
         paddingRight: iconShape
-          ? $theme.sizing.scale500
+          ? $theme.sizing.scale550
           : $theme.sizing.scale600,
       };
   }
