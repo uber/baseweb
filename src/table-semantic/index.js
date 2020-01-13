c4df8d2eddf61a2cccd45bea1abeb1b02af7b9c1
Gergely Nemeth <gnemeth@uber.com>
2020-01-13 10:27:18 -0800

    docs(semantic-table): warning for removing unstable exports

--- src/table-semantic/index.js
+++ src/table-semantic/index.js
@@ -6,8 +6,13 @@ LICENSE file in the root directory of this source tree.
 */
 // @flow
 export {default as Table} from './table.js';
+export {default as Unstable_Table} from './table.js';
 export {default as TableBuilder} from './table-builder.js';
+export {default as Unstable_TableBuilder} from './table-builder.js';
 export {default as TableBuilderColumn} from './table-builder-column.js';
+export {
+  default as Unstable_TableBuilderColumn,
+} from './table-builder-column.js';
 // Styled elements
 export {
   StyledRoot,
@@ -25,3 +30,9 @@ export {
 } from './styled-components.js';
 // Flow
 export type * from './types.js';
+
+if (__DEV__) {
+  console.warn(
+    'We have stabized the Semantic Table component, so you can drop the Unstable_ imports. We will remove the Unstable_ exports soon, so please make these changes as soon as possible!',
+  );
+}
