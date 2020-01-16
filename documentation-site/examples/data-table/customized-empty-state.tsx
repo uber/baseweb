67c37b24beb1dcc6fc895087dc2b5721ba5013b4
Ryan Wong <r25wong@gmail.com>
2020-01-16 09:07:53 -0800

    fix(data-table): removed unnecessary data in emptyState example

--- documentation-site/examples/data-table/customized-empty-state.tsx
+++ documentation-site/examples/data-table/customized-empty-state.tsx
@@ -11,67 +11,6 @@ import {
   NUMERICAL_FORMATS,
 } from 'baseui/data-table';
 
-// https://gist.github.com/6174/6062387
-function pseudoRandomString(rowIdx: any, columnIdx: any) {
-  return (
-    (0.88 * rowIdx)
-      .toString(36)
-      .replace('.', '')
-      .substring(2) +
-    (0.99 * columnIdx).toString(36).replace('.', '')
-  ).slice(0, 10);
-}
-
-function makeRowsFromColumns(columns: any, rowCount: number) {
-  const rows = [];
-  for (let i = 0; i < rowCount; i++) {
-    rows.push({
-      id: i,
-      data: columns.map((column: any, j: number) => {
-        switch (column.kind) {
-          case COLUMNS.CATEGORICAL:
-            switch (i % 5) {
-              case 4:
-                return 'A';
-              case 3:
-                return 'B';
-              case 2:
-                return 'C';
-              case 1:
-                return 'D';
-              case 0:
-              default:
-                return 'F';
-            }
-          case COLUMNS.NUMERICAL:
-            return i % 2 ? i - 1 : i + 3;
-          case COLUMNS.BOOLEAN:
-            return i % 2 === 0;
-          case COLUMNS.STRING:
-            return pseudoRandomString(i, j);
-          case COLUMNS.CUSTOM:
-            switch (i % 5) {
-              case 4:
-                return {color: 'red'};
-              case 3:
-                return {color: 'green'};
-              case 2:
-                return {color: 'blue'};
-              case 1:
-                return {color: 'purple'};
-              case 0:
-              default:
-                return {color: 'yellow'};
-            }
-          default:
-            return 'default' + pseudoRandomString(i, j);
-        }
-      }),
-    });
-  }
-  return rows;
-}
-
 type RowDataT = [
   string,
   string,
@@ -142,15 +81,13 @@ const columns = [
   }),
 ];
 
-const rows = makeRowsFromColumns(columns, 2000);
-
 export default () => {
   const [css] = useStyletron();
   return (
     <div className={css({height: '800px'})}>
       <Unstable_StatefulDataTable
         columns={columns}
-        rows={rows}
+        rows={[]}
         overrides={{
           EmptyState: {
             style: ({$theme}) => {
