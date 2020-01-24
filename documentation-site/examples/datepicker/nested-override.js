70fd7ed4f78a116747dd09874109a31ada27efc4
Nadiia Dmytrenko <nadiia@uber.com>
2020-01-24 14:07:25 -0800

    fix(MaskedInput): pass style overrides, add datepicker example with readonly input attr

--- documentation-site/examples/datepicker/nested-override.js
+++ documentation-site/examples/datepicker/nested-override.js
@@ -1,28 +1,46 @@
 // @flow
 import * as React from 'react';
-import {StatefulCalendar} from 'baseui/datepicker';
+import {Datepicker} from 'baseui/datepicker';
 
-export default () => (
-  <StatefulCalendar
-    overrides={{
-      // MonthYearSelectStatefulMenu is a Menu component
-      // We can use any overrides available to Menu
-      MonthYearSelectStatefulMenu: {
-        props: {
-          overrides: {
-            ListItem: {
-              style: ({$isHighlighted}) => {
-                if ($isHighlighted) {
-                  return {
-                    color: 'white',
-                    backgroundColor: 'salmon',
-                  };
-                }
+export default () => {
+  const [singleDate, setSingleDate] = React.useState();
+  return (
+    <Datepicker
+      value={singleDate || null}
+      onChange={({date}) => setSingleDate(date)}
+      overrides={{
+        // MonthYearSelectStatefulMenu is a Menu component
+        // We can use any overrides available to Menu
+        MonthYearSelectStatefulMenu: {
+          props: {
+            overrides: {
+              ListItem: {
+                style: ({$isHighlighted}) => {
+                  if ($isHighlighted) {
+                    return {
+                      color: 'white',
+                      backgroundColor: 'salmon',
+                    };
+                  }
+                },
               },
             },
           },
         },
-      },
-    }}
-  />
-);
+        // Input override is the MaskedInput Base Web component.
+        // We can use any overrides available to MaskedInput
+        Input: {
+          props: {
+            overrides: {
+              Input: {
+                props: {
+                  readonly: 'readonly',
+                },
+              },
+            },
+          },
+        },
+      }}
+    />
+  );
+};
