ced365f8089073f3b50f0a18c91678e03181be7d
Gergely Nemeth <gnemeth@uber.com>
2020-01-24 10:04:27 -0800

    fix(vscode): do not expose PAT

--- deploy.sh
+++ deploy.sh
@@ -56,7 +56,7 @@ if [ "$this_commit" = "$latest_tagged_commit" ]; then
   cd packages/baseweb-vscode-extension
   node "$BASEDIR/scripts/sync-package-versions.js" package.json
   yarn
-  yarn deploy -p $AZURE_TOKEN
+  yarn vsce publish --yarn -p $AZURE_TOKEN
 else
   echo current commit does not match latest tagged commit
   echo exited without deploying to now
