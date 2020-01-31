1e92e9da797bc2140aac3b402e761cac9017b405
Vojtech Miksu <vojtech@miksu.cz>
2020-01-31 15:10:27 -0800

    fix(vscode): fix the build (#2782)
    * fix(vscode): fix the build
    
    * fix(vscode): add header
    
    * fix(vscode): add ignore

--- packages/baseweb-vscode-extension/scripts/build-non-split.js
+++ packages/baseweb-vscode-extension/scripts/build-non-split.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
+/*
+Copyright (c) 2018-2020 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
 
 // Disables code splitting into chunks
 // See https://github.com/facebook/create-react-app/issues/5306#issuecomment-433425838
