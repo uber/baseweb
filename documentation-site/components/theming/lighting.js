bc688031c3788c6c105fb7f6928a9a094d008896
Graham Murdoch <murdoch@uber.com>
2020-02-03 15:40:11 -0800

    docs(theming): update lighting to show dark as well

--- documentation-site/components/theming/lighting.js
+++ documentation-site/components/theming/lighting.js
@@ -8,19 +8,58 @@ LICENSE file in the root directory of this source tree.
 
 import * as React from 'react';
 import {useStyletron} from 'baseui';
-import {Property} from './common.js';
+import {Title, Value} from './common.js';
+import {LightTheme, DarkTheme} from 'baseui/themes';
+
+function Swatch({value, mode, left = false}) {
+  const [css, theme] = useStyletron();
+  return (
+    <div className={css({flexBasis: '50%', flexGrow: 1})}>
+      <div
+        className={css({
+          backgroundColor: mode.colors.backgroundPrimary,
+          paddingTop: theme.sizing.scale800,
+          paddingBottom: theme.sizing.scale800,
+          display: 'flex',
+          justifyContent: 'center',
+          borderTopStyle: 'solid',
+          borderRightStyle: left ? null : 'solid',
+          borderBottomStyle: 'solid',
+          borderLeftStyle: left ? 'solid' : null,
+          borderTopWidth: '1px',
+          borderBottomWidth: '1px',
+          borderRightWidth: left ? null : '1px',
+          borderLeftWidth: left ? '1px' : null,
+          borderTopColor: theme.colors.borderOpaque,
+          borderBottomColor: theme.colors.borderOpaque,
+          borderRightColor: theme.colors.borderOpaque,
+          borderLeftColor: theme.colors.borderOpaque,
+        })}
+      >
+        <div
+          className={css({
+            height: '40px',
+            width: '40px',
+            boxShadow: mode.lighting[value],
+          })}
+        ></div>
+      </div>
+      <Value $style={{fontSize: '13px', letterSpacing: '-0.1px'}}>
+        ↪ {mode.lighting[value]}
+      </Value>
+    </div>
+  );
+}
 
 export function Lighting({value}: {value: string}) {
   const [css, theme] = useStyletron();
   return (
-    <Property title={value} value={theme.lighting[value]}>
-      <div
-        className={css({
-          backgroundColor: theme.colors.contentInversePrimary,
-          height: theme.sizing.scale1200,
-          boxShadow: theme.lighting[value],
-        })}
-      ></div>
-    </Property>
+    <div className={css({marginBottom: theme.sizing.scale600})}>
+      <Title>{value}</Title>
+      <div className={css({display: 'flex'})}>
+        <Swatch mode={LightTheme} value={value} left />
+        <Swatch mode={DarkTheme} value={value} />
+      </div>
+    </div>
   );
 }
