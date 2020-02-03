7434fabd6f58c81b3420cd64c44954164d873158
Karan Verma <karan.verma@uber.com>
2020-02-03 13:14:34 +0530

    chore: update grid layout docs to user grid porps

--- documentation-site/examples/layout-grid/custom-css-unit.tsx
+++ documentation-site/examples/layout-grid/custom-css-unit.tsx
@@ -1,67 +1,52 @@
 import * as React from 'react';
-import {
-  useStyletron,
-  createTheme,
-  lightThemePrimitives,
-  ThemeProvider,
-} from 'baseui';
+import {useStyletron} from 'baseui';
 import {Grid, Cell} from 'baseui/layout-grid';
-const themeWithCustomCSSUnits = createTheme(
-  {
-    ...lightThemePrimitives,
-  },
-  {
-    grid: {
-      unit: 'em',
-      gutters: [1, 2, 2],
-      margins: [1, 2, 4],
-      gridMaxWidth: 40,
-    },
-  },
-);
+
 export default () => (
-  <ThemeProvider theme={themeWithCustomCSSUnits}>
-    <Outer>
-      <Grid>
-        <Cell>
-          <Inner>1</Inner>
-        </Cell>
-        <Cell>
-          <Inner>2</Inner>
-        </Cell>
-        <Cell>
-          <Inner>3</Inner>
-        </Cell>
-        <Cell>
-          <Inner>4</Inner>
-        </Cell>
-        <Cell>
-          <Inner>5</Inner>
-        </Cell>
-        <Cell>
-          <Inner>6</Inner>
-        </Cell>
-        <Cell>
-          <Inner>7</Inner>
-        </Cell>
-        <Cell>
-          <Inner>8</Inner>
-        </Cell>
-        <Cell>
-          <Inner>9</Inner>
-        </Cell>
-        <Cell>
-          <Inner>10</Inner>
-        </Cell>
-        <Cell>
-          <Inner>11</Inner>
-        </Cell>
-        <Cell>
-          <Inner>12</Inner>
-        </Cell>
-      </Grid>
-    </Outer>
-  </ThemeProvider>
+  <Outer>
+    <Grid
+      gridUnit="em"
+      gridGutters={[1, 2, 2]}
+      gridMargins={[1, 2, 4]}
+    >
+      <Cell>
+        <Inner>1</Inner>
+      </Cell>
+      <Cell>
+        <Inner>2</Inner>
+      </Cell>
+      <Cell>
+        <Inner>3</Inner>
+      </Cell>
+      <Cell>
+        <Inner>4</Inner>
+      </Cell>
+      <Cell>
+        <Inner>5</Inner>
+      </Cell>
+      <Cell>
+        <Inner>6</Inner>
+      </Cell>
+      <Cell>
+        <Inner>7</Inner>
+      </Cell>
+      <Cell>
+        <Inner>8</Inner>
+      </Cell>
+      <Cell>
+        <Inner>9</Inner>
+      </Cell>
+      <Cell>
+        <Inner>10</Inner>
+      </Cell>
+      <Cell>
+        <Inner>11</Inner>
+      </Cell>
+      <Cell>
+        <Inner>12</Inner>
+      </Cell>
+    </Grid>
+  </Outer>
 );
 
 const Outer: React.FunctionComponent<{}> = ({children}) => {
