6c3fbe57e1d37a068f402a27ef1dbc1e96d81fc2
Ashish Kale <ashish.kale@uber.com>
2020-02-04 13:39:45 +0530

    feat: remove copyright from example

--- documentation-site/examples/table-grid/sortable.tsx
+++ documentation-site/examples/table-grid/sortable.tsx
@@ -1,10 +1,3 @@
-/*
-Copyright (c) 2018-2020 Uber Technologies, Inc.
-
-This source code is licensed under the MIT license found in the
-LICENSE file in the root directory of this source tree.
-*/
-
 import * as React from 'react';
 
 import {
