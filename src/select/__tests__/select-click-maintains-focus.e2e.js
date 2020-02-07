9735c0e1c8c39b7f0e43b23516c67a978f2f3ce9
Chase Starr <starr@uber.com>
2020-02-07 13:12:53 -0800

    fix(select): lint

--- src/select/__tests__/select-click-maintains-focus.e2e.js
+++ src/select/__tests__/select-click-maintains-focus.e2e.js
@@ -4,10 +4,11 @@ Copyright (c) 2018-2020 Uber Technologies, Inc.
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
+/* global document */
 /* eslint-env node */
 /* eslint-disable flowtype/require-valid-file-annotation */
 
-const {mount, analyzeAccessibility} = require('../../../e2e/helpers');
+const {mount} = require('../../../e2e/helpers');
 
 const SELECT_INPUT = 'div[data-baseweb="select"] input';
 
