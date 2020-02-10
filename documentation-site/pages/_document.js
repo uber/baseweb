74ccf16a72206c3db7d79073c13d46848f6c1bac
Gergely Nemeth <gergelyke@users.noreply.github.com>
2020-02-10 16:12:21 -0500

    docs(delighted): make the csat survey recurring (#2829)

--- documentation-site/pages/_document.js
+++ documentation-site/pages/_document.js
@@ -43,7 +43,7 @@ export default class MyDocument extends Document {
     return {
       __html: `
         !function(e,t,r,n,a){if(!e[a]){for(var i=e[a]=[],s=0;s<r.length;s++){var c=r[s];i[c]=i[c]||function(e){return function(){var t=Array.prototype.slice.call(arguments);i.push([e,t])}}(c)}i.SNIPPET_VERSION="1.0.1";var o=t.createElement("script");o.type="text/javascript",o.async=!0,o.src="https://d2yyd1h5u9mauk.cloudfront.net/integrations/web/v1/library/"+n+"/"+a+".js";var p=t.getElementsByTagName("script")[0];p.parentNode.insertBefore(o,p)}}(window,document,["survey","reset","config","init","set","get","event","identify","track","page","screen","group","alias"],"5ey7SgIHHblqnpdL","delighted");
-        delighted.survey();
+        delighted.survey({recurringPeriod: 1});
       `,
     };
   }
