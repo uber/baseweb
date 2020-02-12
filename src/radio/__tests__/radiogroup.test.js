d1b7949fec3484fc07bd388991caa08381913260
Vojtech Miksu <vojtech@uber.com>
2020-02-11 18:22:31 -0800

    fix(radio): wip tests

--- src/radio/__tests__/radiogroup.test.js
+++ src/radio/__tests__/radiogroup.test.js
@@ -8,8 +8,11 @@ LICENSE file in the root directory of this source tree.
 
 import * as React from 'react';
 import {shallow} from 'enzyme';
+import {render, fireEvent} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 
-import {RadioGroup, Radio} from '../index.js';
+import {StatefulRadioGroup, RadioGroup, Radio} from '../index.js';
 
 describe('radio-group', () => {
   it('sets expected child radio checked', () => {
@@ -52,5 +55,77 @@ describe('radio-group', () => {
     wrapper.children().forEach((child, index) => {
       expect(child).toHaveProp('disabled', index === 0);
     });
+  });
+});
+
+describe('radio-group focus and a11y management', () => {
+  it('sets the initial state', () => {
+    const {getByDisplayValue} = render(
+      <RadioGroup name="numbers" value="3">
+        <Radio value="1" />
+        <Radio value="2" />
+        <Radio value="3" />
+      </RadioGroup>,
+    );
+    expect(getByDisplayValue('1')).not.toBeChecked();
+    expect(getByDisplayValue('2')).not.toBeChecked();
+    expect(getByDisplayValue('3')).toBeChecked();
+
+    expect(getByDisplayValue('1')).toHaveAttribute('tabindex', '-1');
+    expect(getByDisplayValue('2')).toHaveAttribute('tabindex', '-1');
+    expect(getByDisplayValue('3')).toHaveAttribute('tabindex', '0');
+
+    expect(getByDisplayValue('1')).not.toHaveFocus();
+    expect(getByDisplayValue('2')).not.toHaveFocus();
+    expect(getByDisplayValue('3')).not.toHaveFocus();
+  });
+
+  it('hits tab', () => {
+    const {getByDisplayValue, container} = render(
+      <StatefulRadioGroup name="numbersz" initialState={{value: '3'}}>
+        <Radio value="1" />
+        <Radio value="2" />
+        <Radio value="3" />
+      </StatefulRadioGroup>,
+    );
+
+    const one = getByDisplayValue('1');
+    const two = getByDisplayValue('2');
+    const three = getByDisplayValue('3');
+
+    userEvent.tab();
+
+    fireEvent(
+      getByDisplayValue('2'),
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      }),
+    );
+    // getByDisplayValue('3').focus();
+    // fireEvent(
+    //   getByDisplayValue('3'),
+    //   new KeyboardEvent('keydown', {
+    //     key: 'ArrowUp',
+    //     code: 'ArrowUp',
+    //     keyCode: 38,
+    //     charCode: 38,
+    //   }),
+    // );
+    // fireEvent.keyDown(container, {key: 'ArrowUp', code: 38});
+    // fireEvent.keyDown(three, {key: 'ArrowUp', code: 38});
+    // fireEvent.keyDown(document.body, {key: 'ArrowUp', code: 38});
+
+    // expect(one).not.toHaveFocus();
+    // expect(two).toHaveFocus();
+    // expect(three).not.toHaveFocus();
+
+    expect(getByDisplayValue('1')).not.toBeChecked();
+    expect(getByDisplayValue('2')).toBeChecked();
+    expect(getByDisplayValue('3')).not.toBeChecked();
+
+    expect(getByDisplayValue('1')).toHaveAttribute('tabindex', '-1');
+    expect(getByDisplayValue('2')).toHaveAttribute('tabindex', '0');
+    expect(getByDisplayValue('3')).toHaveAttribute('tabindex', '-1');
   });
 });
