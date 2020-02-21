ffec7e091cc8f43d1a0a4f9d2ec676e13f1409ba
Chase Starr <starr@uber.com>
2020-02-21 08:31:38 -0800

    fix(flow): lint build deps output

--- packages/flow-check-build/build-dependencies.js
+++ packages/flow-check-build/build-dependencies.js
@@ -31,8 +31,10 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 // @flow
+/* eslint-disable */
 ${imports}
 import * as baseui from 'baseui';
+/* eslint-enable */
 `;
 
 fs.writeFileSync(path.resolve(__dirname, 'index.js'), source);
--- packages/flow-check-build/index.js
+++ packages/flow-check-build/index.js
@@ -5,6 +5,7 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 // @flow
+/* eslint-disable */
 import * as $0 from 'baseui/accordion';
 import * as $1 from 'baseui/aspect-ratio-box';
 import * as $2 from 'baseui/avatar';
@@ -62,3 +63,4 @@ import * as $53 from 'baseui/tooltip';
 import * as $54 from 'baseui/tree-view';
 import * as $55 from 'baseui/typography';
 import * as baseui from 'baseui';
+/* eslint-enable */
