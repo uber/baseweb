0c21a98ae0d517b72f14cbcadd7b65d7e64c0c66
Chase Starr <starr@uber.com>
2020-02-21 13:21:15 -0800

    fix(flow): only check flow 110 and 111

--- scripts/flow-check-build.js
+++ scripts/flow-check-build.js
@@ -35,13 +35,14 @@ spawnSync('node', ['build-dependencies.js'], spawn_args);
 const versions = [
   '0.110',
   '0.111',
-  '0.112',
-  '0.113',
-  '0.114',
-  '0.115',
-  '0.116',
-  '0.117',
-  '0.118',
+  // save additional versions for follow up prs
+  // '0.112',
+  // '0.113',
+  // '0.114',
+  // '0.115',
+  // '0.116',
+  // '0.117',
+  // '0.118',
 ];
 
 const exit_codes = versions.map(flow_check_version);
