uber/baseweb

commit 7c8aa99cd5f1ca53baf9f0fd0d090b1cf155fa53

feat(input):  make "clear input" keyboard interactive (#2936)
* feat(input):  make clear keyboard interactive

* test(input): add keyboard clear tests

* fix: remove focus pseudo

* fix: scrolling on space

src/input/base-input.js
@@ -23,6 +23,7 @@ import {Button, KIND} from '../button/index.js';
 import Hide from '../icon/hide.js';
 import Show from '../icon/show.js';
 import createEvent from '../utils/create-event.js';
+import {isFocusVisible, forkFocus, forkBlur} from '../utils/focusVisible.js';
 
 const NullComponent = () => null;
 
@@ -66,6 +67,7 @@ class BaseInput<T: EventTarget> extends React.Component<
     isFocused: this.props.autoFocus || false,
     isMasked: this.props.type === 'password',
     initialType: this.props.type,
+    isFocusVisibleForClear: false,
   };
 
   componentDidMount() {
@@ -207,6 +209,18 @@ class BaseInput<T: EventTarget> extends React.Component<
     );
   }
 
+  handleFocusForClear = (event: SyntheticEvent<>) => {
+    if (isFocusVisible(event)) {
+      this.setState({isFocusVisibleForClear: true});
+    }
+  };
+
+  handleBlurForClear = (event: SyntheticEvent<>) => {
+    if (this.state.isFocusVisibleForClear !== false) {
+      this.setState({isFocusVisibleForClear: false});
+    }
+  };
+
   renderClear() {
     const {clearable, value, disabled, overrides = {}} = this.props;
     if (!clearable || !value || !value.length || disabled) {
@@ -230,12 +244,22 @@ class BaseInput<T: EventTarget> extends React.Component<
       >
         <ClearIcon
           size={16}
+          tabindex={0}
           title={ariaLabel}
           aria-label={ariaLabel}
           onClick={this.onClearIconClick}
+          onKeyDown={event => {
+            if (event.key && (event.key === 'Enter' || event.key === ' ')) {
+              event.preventDefault();
+              this.onClearIconClick();
+            }
+          }}
           role="button"
+          $isFocusVisible={this.state.isFocusVisibleForClear}
           {...sharedProps}
           {...clearIconProps}
+          onFocus={forkFocus(clearIconProps, this.handleFocusForClear)}
+          onBlur={forkBlur(clearIconProps, this.handleBlurForClear)}
         />
       </ClearIconContainer>
     );

src/input/__tests__/input.e2e.js
@@ -85,6 +85,42 @@ describe('input', () => {
       });
     });
 
+    it('with delete icon via enter', async () => {
+      await mount(page, 'input-clearable');
+      await page.waitFor(selectors.input);
+
+      let inputValue = await page.$eval(selectors.input, input => input.value);
+      expect(inputValue).toBe('Thing');
+
+      await page.focus(selectors.clearIcon);
+      await page.keyboard.press('Enter');
+
+      inputValue = await page.$eval(selectors.input, input => input.value);
+      expect(inputValue).toBe('');
+
+      await page.waitFor(selectors.clearIcon, {
+        hidden: true,
+      });
+    });
+
+    it('with delete icon via space', async () => {
+      await mount(page, 'input-clearable');
+      await page.waitFor(selectors.input);
+
+      let inputValue = await page.$eval(selectors.input, input => input.value);
+      expect(inputValue).toBe('Thing');
+
+      await page.focus(selectors.clearIcon);
+      await page.keyboard.press(' ');
+
+      inputValue = await page.$eval(selectors.input, input => input.value);
+      expect(inputValue).toBe('');
+
+      await page.waitFor(selectors.clearIcon, {
+        hidden: true,
+      });
+    });
+
     // regression test for https://github.com/uber/baseweb/issues/1643
     // verify that the input receiving the clear event is cleared and not another input
     it('clears the correct input', async () => {

src/input/styled-components.js
@@ -26,9 +26,13 @@ export const StyledClearIconContainer = styled<{
   };
 });
 
-export const StyledClearIcon = styled<typeof DeleteAlt, {}>(DeleteAlt, {
+export const StyledClearIcon = styled<
+  typeof DeleteAlt,
+  {$isFocusVisible: boolean},
+>(DeleteAlt, ({$theme, $isFocusVisible}) => ({
   cursor: 'pointer',
-});
+  outline: $isFocusVisible ? `solid 3px ${$theme.colors.accent}` : 'none',
+}));
 
 function getInputPadding(size, sizing) {
   return {

src/input/types.js
@@ -25,6 +25,8 @@ export type InternalStateT = {
   isFocused?: boolean,
   /** Renders input in 'masked' state if type equals "password" */
   isMasked?: boolean,
+  /** Tracks if focus should be visible on the clear button. */
+  isFocusVisibleForClear?: boolean,
 };
 
 export type StateT = {

commit 7df3c580736fcce5176312c3255c7c7705372081

fix(input): tabIndex typo
Fix a "tabindex" typo on the input clearable button introduced in #2936.

src/input/base-input.js
@@ -244,7 +244,7 @@ class BaseInput<T: EventTarget> extends React.Component<
       >
         <ClearIcon
           size={16}
-          tabindex={0}
+          tabIndex={0}
           title={ariaLabel}
           aria-label={ariaLabel}
           onClick={this.onClearIconClick}