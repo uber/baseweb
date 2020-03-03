60dbfcadb6c684cc61b4fce8b0156d6008a2f425
Chase Starr <chasestarr@gmail.com>
2020-03-03 11:15:24 -0800

    fix(menu): improve for screen reader compatibility (#2950)
    * fix(menu): tell screenreader menu item is disabled
    
    * feat(menu): add activedescendant change handler
    
    * fix(select): apply active descendant to select input
    
    * fix(select): apply listbox id to input controls
    
    * fix(select): applies aria label with selected values
    
    * fix(menu): update menu tests
    
    * fix(select): conditionally apply listbox id
    
    * fix(select): update snapshot
    
    * fix(menu): adds message for how to operate child menu
    
    * fix(menu): communicate empty state to screen reader

--- src/menu/locale.js
+++ src/menu/locale.js
@@ -8,10 +8,12 @@ LICENSE file in the root directory of this source tree.
 
 export type MenuLocaleT = {|
   noResultsMsg: string,
+  parentMenuItemAriaLabel: string,
 |};
 
-const locale = {
+const locale: MenuLocaleT = {
   noResultsMsg: 'No results',
+  parentMenuItemAriaLabel: `You are currently at an item than opens a nested listbox. Press right arrow to enter that element and left arrow to return.`,
 };
 
 export default locale;
