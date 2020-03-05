0c67f4445482c56acceb4dd3a7c3b6752c161121
Graham Murdoch <murdoch@uber.com>
2020-03-05 14:10:41 -0800

    fix(side-navigation): fix withStyle extensions

--- src/side-navigation/styled-components.js
+++ src/side-navigation/styled-components.js
@@ -7,8 +7,8 @@ LICENSE file in the root directory of this source tree.
 // @flow
 import * as React from 'react';
 import {styled, hexToRgb} from '../styles/index.js';
-import type {Item, SharedPropsT} from './types.js';
-import type {StyletronComponent} from '../styles/styled.js';
+import type {SharedPropsT} from './types.js';
+import {withWrapper} from '../styles/styled.js';
 
 export const StyledRoot = styled<SharedPropsT>('nav', props => {
   const {
@@ -95,18 +95,13 @@ export const StyledNavItemElement = styled<SharedPropsT>('div', props => {
   }: {});
 });
 
-export const StyledNavItem = ((React.forwardRef<
-  {item: Item, ...$Exact<SharedPropsT>},
-  // eslint-disable-next-line flowtype/no-weak-types
-  any,
->(
-  ({item, ...restProps}, ref) => (
-    <StyledNavItemElement ref={ref} {...restProps} />
-  ),
-  // eslint-disable-next-line flowtype/no-weak-types
-): any): StyletronComponent<SharedPropsT>);
-StyledNavItem.__STYLETRON__ = StyledNavItemElement.__STYLETRON__;
-StyledNavItem.displayName = 'StyledNavItem';
+export const StyledNavItem = withWrapper(
+  StyledNavItemElement,
+  Styled =>
+    function StyledNav({item, ...restProps}) {
+      return <Styled {...restProps} />;
+    },
+);
 
 export const StyledSubNavContainer = styled('ul', {
   listStyleType: 'none',
