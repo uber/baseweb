bfb5d0acf69b0021c80be54dd157eab97fba38a4
Graham Murdoch <murdoch@uber.com>
2020-03-19 17:06:57 -0700

    refactor(input): remove Button dependency

--- src/input/styled-components.js
+++ src/input/styled-components.js
@@ -11,6 +11,30 @@ import {ADJOINED, ENHANCER_POSITION, SIZE} from './constants.js';
 import type {SharedPropsT, SizeT} from './types.js';
 import DeleteAlt from '../icon/delete-alt.js';
 
+export const StyledMaskToggleButton = styled<{
+  $size: SizeT,
+  $isFocusVisible: boolean,
+  $theme: ThemeT,
+}>('button', ({$theme, $size, $isFocusVisible}) => {
+  const pad = {
+    [SIZE.mini]: $theme.sizing.scale300,
+    [SIZE.compact]: $theme.sizing.scale400,
+    [SIZE.default]: $theme.sizing.scale500,
+    [SIZE.large]: $theme.sizing.scale600,
+  }[$size];
+  return {
+    display: 'flex',
+    borderTop: 'none',
+    borderBottom: 'none',
+    borderLeft: 'none',
+    borderRight: 'none',
+    background: 'none',
+    paddingLeft: pad,
+    paddingRight: pad,
+    outline: $isFocusVisible ? `solid 3px ${$theme.colors.accent}` : 'none',
+  };
+});
+
 export const StyledClearIconContainer = styled<{
   $alignTop: boolean,
   $theme: ThemeT,
