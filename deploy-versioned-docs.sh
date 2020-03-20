1346e261e01b00b0d8e3dcdd5b6b04b6218284bc
Chase Starr <chasestarr@gmail.com>
2020-03-20 15:17:31 -0700

    fix(build): correct versioned doc site cname config (#3097)

--- deploy-versioned-docs.sh
+++ deploy-versioned-docs.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+
 # deploy to now the versioned docs site
 FORCE_EXTRACT_REACT_TYPES=true yarn documentation:build
 now --scope=uber-ui-platform --token=$ZEIT_NOW_TOKEN --public --no-clipboard deploy ./public > deployment.txt
 deployment=`cat deployment.txt`
-cname="${BUILDKITE_TAG//./-}"
+version=$(echo $BUILDKITE_MESSAGE | cut -d' ' -f 2)
+cname="${version//./-}"
 curl -X POST "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/dns_records" \
     -H "Authorization: Bearer $CF_API_KEY" \
     -H "Content-Type: application/json" \
