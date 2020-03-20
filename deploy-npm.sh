e8ba8dc08b3439f942743436b8b89b74742b54cd
Chase Starr <chasestarr@gmail.com>
2020-03-20 14:10:57 -0700

    fix(build): correct some publish script errors (#3094)

--- deploy-npm.sh
+++ deploy-npm.sh
@@ -9,8 +9,9 @@ echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
 yarn build
 npm publish dist
 
-echo deploying eslint-plugin-baseui to npm
 # publish eslint-plugin-baseui
+echo deploying eslint-plugin-baseui to npm
+BASEDIR=$(pwd)
 cd $BASEDIR
 cd packages/eslint-plugin-baseui
 node "$BASEDIR/scripts/sync-package-versions.js" package.json
--- deploy-vscode.sh
+++ deploy-vscode.sh
@@ -3,6 +3,7 @@
 set -e
 
 # publish the vscode extension
+BASEDIR=$(pwd)
 cd $BASEDIR
 cd packages/baseweb-vscode-extension
 node "$BASEDIR/scripts/sync-package-versions.js" package.json
