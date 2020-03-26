ad8d932187978073b2126c7dbd822cabf6572564
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-03-26 23:11:51 +0530

    Fixed Accordion stateful missing ul warning (#3119)

--- documentation-site/examples/accordion/stateful.tsx
+++ documentation-site/examples/accordion/stateful.tsx
@@ -5,5 +5,7 @@ const content =
   'Praesent condimentum ante ac ipsum aliquam, ac scelerisque velit sagittis. Ut sit amet libero scelerisque, accumsan ante vitae, hendrerit tellus. Nullam metus est, vehicula a aliquet id, lobortis in mauris.';
 
 export default () => (
-  <StatefulPanel title="Expandable panel">{content}</StatefulPanel>
+  <ul>
+    <StatefulPanel title="Expandable panel">{content}</StatefulPanel>
+  </ul>
 );
