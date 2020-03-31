478ca987483796f00cdff1d47e54809b93c58a2c
Chase Starr <chasestarr@gmail.com>
2020-03-30 17:00:10 -0700

    fix(spinner): only warn on usage outside of baseui (#3143)

--- src/spinner/spinner.js
+++ src/spinner/spinner.js
@@ -28,11 +28,13 @@ class Spinner extends React.Component<SpinnerPropsT> {
   componentDidMount() {
     // TODO(v10): remove warning when switching default Spinner
     if (__DEV__) {
-      console.warn(
-        `❖ [baseui] Please consider using "StyledSpinnerNext" instead of "Spinner". ` +
-          `In v10, "StyledSpinnerNext" will become the default "Spinner"` +
-          ` and the current SVG based implementation will be deprecated.`,
-      );
+      if (!this.props.silenceV10DeprecationWarning) {
+        console.warn(
+          `❖ [baseui] Please consider using "StyledSpinnerNext" instead of "Spinner". ` +
+            `In v10, "StyledSpinnerNext" will become the default "Spinner"` +
+            ` and the current SVG based implementation will be deprecated.`,
+        );
+      }
     }
   }
 
