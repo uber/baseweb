f3b7df1cca5df39b0bfae490161e36647bbe68d5
Graham Murdoch <murdoch@uber.com>
2020-04-02 14:03:39 -0700

    Move getColors out of creator.js

--- src/themes/creator.js
+++ src/themes/creator.js
@@ -6,27 +6,20 @@ LICENSE file in the root directory of this source tree.
 */
 // @flow
 
-import deepMerge from '../utils/deep-merge.js';
-import lightColorTokens from './light-theme/color-tokens.js';
-import getLightSemanticColorTokens from './light-theme/color-semantic-tokens.js';
-import getLightComponentColorTokens from './light-theme/color-component-tokens.js';
-import getLightDeprecatedSemanticColorTokens from './light-theme/color-deprecated-semantic-tokens.js';
-import darkColorTokens from './dark-theme/color-tokens.js';
-import getDarkSemanticColorTokens from './dark-theme/color-semantic-tokens.js';
-import getDarkComponentColorTokens from './dark-theme/color-component-tokens.js';
-import getDarkDeprecatedSemanticColorTokens from './dark-theme/color-deprecated-semantic-tokens.js';
-import darkBorders from './dark-theme/borders.js';
-import lighting from './shared/lighting.js';
-import borders from './shared/borders.js';
-import getTypography from './shared/typography.js';
 import animation from './shared/animation.js';
+import borders from './shared/borders.js';
 import breakpoints from './shared/breakpoints.js';
+import darkBorders from './dark-theme/borders.js';
+import deepMerge from '../utils/deep-merge.js';
+import getColors from './shared/colors.js';
+import getTypography from './shared/typography.js';
 import grid from './shared/grid.js';
+import lighting from './shared/lighting.js';
 import mediaQuery from './shared/media-query.js';
 import sizing from './shared/sizing.js';
 
-import type {ThemeT} from '../styles/types.js';
 import type {PrimitivesT} from './types.js';
+import type {ThemeT} from '../styles/types.js';
 
 export default function createTheme(
   // Used to derive various theme properties.
@@ -36,12 +29,13 @@ export default function createTheme(
   // Options for deriving the final theme object.
   options?: {isDark?: boolean} = {},
 ): ThemeT {
-  const {primaryFontFamily, ...colorTokens} = primitives;
+  const {primaryFontFamily, ...customColorTokens} = primitives;
+  const {isDark = false} = options;
   const theme = {
     animation,
     borders: options.isDark ? darkBorders : borders,
     breakpoints,
-    colors: getColors(colorTokens, options.isDark),
+    colors: getColors(customColorTokens, isDark),
     direction: 'auto',
     grid,
     lighting,
@@ -56,32 +50,4 @@ export default function createTheme(
   };
 
   return deepMerge(theme, overrides);
-}
-
-function getColors(colorTokens, isDark = false) {
-  // The following vary depending on if the theme is light or dark.
-  let defaultColorTokens;
-  let getComponentColorTokens;
-  let getDeprecatedSemanticColorTokens;
-  let getSemanticColorTokens;
-
-  if (isDark) {
-    defaultColorTokens = darkColorTokens;
-    getComponentColorTokens = getDarkComponentColorTokens;
-    getDeprecatedSemanticColorTokens = getDarkDeprecatedSemanticColorTokens;
-    getSemanticColorTokens = getDarkSemanticColorTokens;
-  } else {
-    defaultColorTokens = lightColorTokens;
-    getComponentColorTokens = getLightComponentColorTokens;
-    getDeprecatedSemanticColorTokens = getLightDeprecatedSemanticColorTokens;
-    getSemanticColorTokens = getLightSemanticColorTokens;
-  }
-
-  return {
-    ...defaultColorTokens,
-    ...colorTokens,
-    ...getComponentColorTokens(colorTokens),
-    ...getDeprecatedSemanticColorTokens(colorTokens),
-    ...getSemanticColorTokens(colorTokens),
-  };
 }
