c8e4e2bec188a01a7d19ebcf8782d478325e7b31
Nadiia Dmytrenko <nadiia.dmytrenko@gmail.com>
2020-04-15 15:19:05 -0700

    fix(docs): fix app navigation bar codesandbox example

--- documentation-site/examples/app-nav-bar/basic.tsx
+++ documentation-site/examples/app-nav-bar/basic.tsx
@@ -118,6 +118,13 @@ export default () => {
   const [isNavBarVisible, setIsNavBarVisible] = React.useState(
     false,
   );
+  const containerStyles = css({
+    boxSizing: 'border-box',
+    width: '100vw',
+    position: 'fixed',
+    top: '0',
+    left: '0',
+  });
   return (
     <React.Fragment>
       <Button onClick={() => setIsNavBarVisible(!isNavBarVisible)}>
@@ -125,15 +132,7 @@ export default () => {
       </Button>
       {isNavBarVisible ? (
         <Layer>
-          <div
-            className={css({
-              boxSizing: 'border-box',
-              width: '100vw',
-              position: 'fixed',
-              top: '0',
-              left: '0',
-            })}
-          >
+          <div className={containerStyles}>
             <AppNavBar
               appDisplayName="App Something"
               mainNav={MAIN_NAV}
