b47f2b704668bd9d7948eade3b4d9c12331ca723
Clay Branch <clay.branch@gmail.com>
2020-04-22 10:32:47 -0500

    fix: re-wrap direct date-fns exports
    The functions that used to be exported from utils/index used to have
    two pretty different sets of behavior.  The functions that were created
    directly inside the file would only accept dates, where as the functions
    that were being re-exported from date-fns would accept either a date
    or a number(and then date-fns would convert them into a date).
    
    This difference needed to be maintained for the types not to break.
    Rather than roughly half of the functions in DateHelpers acceptingeither
    a number or date, and the other half just a date, I decided to re-wrap
    the date-helpers functions to maintain parity.

--- src/datepicker/utils/index.js
+++ src/datepicker/utils/index.js
@@ -12,32 +12,14 @@ import DateHelpers from './date-helpers';
 
 const defaultDateHelpers = new DateHelpers<Date>(defaultAdapter);
 
+type DirtyDate = Date | number;
+const {date: wrapDate} = defaultAdapter;
+
 export const formatDate = defaultDateHelpers.formatDate;
-export const setSeconds = defaultDateHelpers.setSeconds;
-export const setMinutes = defaultDateHelpers.setMinutes;
-export const setHours = defaultDateHelpers.setHours;
-export const setMonth = defaultDateHelpers.setMonth;
-export const setYear = defaultDateHelpers.setYear;
-export const getMinutes = defaultDateHelpers.getMinutes;
-export const getHours = defaultDateHelpers.getHours;
-export const getMonth = defaultDateHelpers.getMonth;
-export const getYear = defaultDateHelpers.getYear;
-export const getDay = defaultDateHelpers.getDay;
-export const getDate = defaultDateHelpers.getDate;
 export const getStartOfWeek = defaultDateHelpers.getStartOfWeek;
 export const getEndOfWeek = defaultDateHelpers.getEndOfWeek;
 export const getStartOfMonth = defaultDateHelpers.getStartOfMonth;
 export const getEndOfMonth = defaultDateHelpers.getEndOfMonth;
-export const addDays = defaultDateHelpers.addDays;
-export const addWeeks = defaultDateHelpers.addWeeks;
-export const addMonths = defaultDateHelpers.addMonths;
-export const addYears = defaultDateHelpers.addYears;
-export const subDays = defaultDateHelpers.subDays;
-export const subWeeks = defaultDateHelpers.subWeeks;
-export const subMonths = defaultDateHelpers.subMonths;
-export const subYears = defaultDateHelpers.subYears;
-export const isBefore = defaultDateHelpers.isBefore;
-export const isAfter = defaultDateHelpers.isAfter;
 export const isSameYear = defaultDateHelpers.isSameYear;
 export const isSameMonth = defaultDateHelpers.isSameMonth;
 export const isSameDay = defaultDateHelpers.isSameDay;
@@ -55,3 +37,53 @@ export const getEffectiveMinDate = defaultDateHelpers.getEffectiveMinDate;
 export const getEffectiveMaxDate = defaultDateHelpers.getEffectiveMaxDate;
 export const applyTimeToDate = defaultDateHelpers.applyTimeToDate;
 export const applyDateToTime = defaultDateHelpers.applyDateToTime;
+
+const createDirtySetter = (setter: (Date, number) => Date) => {
+  return (dirtyDate: DirtyDate, number: number) =>
+    setter(wrapDate(dirtyDate), number);
+};
+
+const createDirtyGetter = (getter: Date => number) => {
+  return (dirtyDate: DirtyDate) => getter(wrapDate(dirtyDate));
+};
+
+const createDirtyCompare = (compare: (Date, Date) => boolean) => {
+  return (fromDirty: DirtyDate, toDirty: DirtyDate) => {
+    return compare(wrapDate(fromDirty), wrapDate(toDirty));
+  };
+};
+
+// ** Re-exported from date-fns **
+
+// these need to be able to accept either number or date
+// to maintain parity with the old exports
+
+// ** Date Setters **
+export const setSeconds = createDirtySetter(defaultDateHelpers.setSeconds);
+export const setMinutes = createDirtySetter(defaultDateHelpers.setMinutes);
+export const setHours = createDirtySetter(defaultDateHelpers.setHours);
+export const setMonth = createDirtySetter(defaultDateHelpers.setMonth);
+export const setYear = createDirtySetter(defaultDateHelpers.setYear);
+
+// ** Date Getters **
+export const getMinutes = createDirtyGetter(defaultDateHelpers.getMinutes);
+export const getHours = createDirtyGetter(defaultDateHelpers.getHours);
+export const getDay = createDirtyGetter(defaultDateHelpers.getDay);
+export const getDate = createDirtyGetter(defaultDateHelpers.getDate);
+export const getMonth = createDirtyGetter(defaultDateHelpers.getMonth);
+export const getYear = createDirtyGetter(defaultDateHelpers.getYear);
+
+// ** Date Math
+export const addDays = createDirtySetter(defaultDateHelpers.addDays);
+export const addWeeks = createDirtySetter(defaultDateHelpers.addWeeks);
+export const addMonths = createDirtySetter(defaultDateHelpers.addMonths);
+export const addYears = createDirtySetter(defaultDateHelpers.addYears);
+export const subDays = createDirtySetter(defaultDateHelpers.subDays);
+export const subWeeks = createDirtySetter(defaultDateHelpers.subWeeks);
+export const subMonths = createDirtySetter(defaultDateHelpers.subMonths);
+export const subYears = createDirtySetter(defaultDateHelpers.subYears);
+
+// ** Date Comparison
+
+export const isBefore = createDirtyCompare(defaultDateHelpers.isBefore);
+export const isAfter = createDirtyCompare(defaultDateHelpers.isAfter);
