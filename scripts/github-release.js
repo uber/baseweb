eb966f09982d1b6b52c911a586f249a3f0c1bb52
Chase Starr <starr@uber.com>
2020-04-24 15:46:58 -0700

    fix(build): lint

--- scripts/github-release.js
+++ scripts/github-release.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
+/*
+Copyright (c) 2018-2020 Uber Technologies, Inc.
 
-/** Copyright (c) 2017 Uber Technologies, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
 
 // @flow
 /* eslint-env node */
