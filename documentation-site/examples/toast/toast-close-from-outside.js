b32f4e806fc0e7fcd676fe964473413a9c915af9
alanoliv <alan.oliveira@gympass.com>
2020-05-07 14:54:22 -0300

    docs(toast): adding typescript example

--- documentation-site/examples/toast/toast-close-from-outside.js
+++ documentation-site/examples/toast/toast-close-from-outside.js
@@ -1,25 +1,26 @@
+// @flow
 import * as React from 'react';
+
 import {toaster, ToasterContainer} from 'baseui/toast';
 import {Button} from 'baseui/button';
 
 export default () => {
   const [toastKey, setToastKey] = React.useState(null);
-  function showToast() {
-    const key = toaster.info('This is the message.');
-    setToastKey(key);
-  }
 
-  function closeToast() {
-    if (toastKey) {
-      toaster.clear(toastKey);
-      setToastKey(null);
-    }
-  }
+  const showToast = () =>
+    setToastKey(toaster.info('This is the message.'));
+
+  const closeToast = () => {
+    toaster.clear(toastKey);
+    setToastKey(null);
+  };
 
   return (
     <ToasterContainer>
       <Button onClick={showToast}>Show notification</Button>
-      <Button onClick={closeToast}>Close notification</Button>
+      <Button disabled={!toastKey} onClick={closeToast}>
+        Close notification
+      </Button>
     </ToasterContainer>
   );
 };
