uber/baseweb

commit 1aab1e6372be78f0a5c4b733ba9bde277ba87dfa

fix(focus-styles): the rest of components and doc site (#2864)
* fix(link): improve focus-ring styles

* fix(pagination): improve focus styles

* fix(side-nav): improve focus styles

* fix(tabs): improve focus styles

* fix(accordion): improve focus-ring styles

* fix(dnd-list): improve focus-ring styles

* fix(tag): improve focus-ring styles

* fix(button): focus-ring style round buttons

* fix(data-table): focus-styles

* fix(toast): focus-ring styles

* fix(drawer): focus-ring styles

* fix(modal): focus-ring styles

* docs: improve focus-ring styles in docs

* fix(a11y): small improvements

* fix(foucs): update snaps

* fix(focus): eslint fix

* test(vrt): update visual snapshots for 74018af [skip ci] (#2865)

Co-authored-by: UberOpenSourceBot <33560951+UberOpenSourceBot@users.noreply.github.com>

src/toast/toast.js
@@ -23,6 +23,7 @@ import type {
   SharedStylePropsArgT,
 } from './types.js';
 import type {OverridesT} from '../icon/index.js';
+import {isFocusVisible, forkFocus, forkBlur} from '../utils/focusVisible.js';
 
 class Toast extends React.Component<ToastPropsT, ToastPrivateStateT> {
   static defaultProps: ToastPropsShapeT = {
@@ -47,6 +48,7 @@ class Toast extends React.Component<ToastPropsT, ToastPrivateStateT> {
   state = {
     isVisible: false,
     isRendered: true,
+    isFocusVisible: false,
   };
 
   componentDidMount() {
@@ -66,6 +68,18 @@ class Toast extends React.Component<ToastPropsT, ToastPrivateStateT> {
   componentWillUnmount() {
     this.clearTimeout();
   }
+
+  handleFocus = (event: SyntheticEvent<>) => {
+    if (isFocusVisible(event)) {
+      this.setState({isFocusVisible: true});
+    }
+  };
+
+  handleBlur = (event: SyntheticEvent<>) => {
+    if (this.state.isFocusVisible !== false) {
+      this.setState({isFocusVisible: false});
+    }
+  };
 
   startTimeout() {
     if (this.props.autoHideDuration) {
@@ -183,7 +197,6 @@ class Toast extends React.Component<ToastPropsT, ToastPrivateStateT> {
           <Body
             role="alert"
             data-baseweb={this.props['data-baseweb'] || 'toast'}
-            tabIndex={-1}
             {...sharedProps}
             {...bodyProps}
             // the properties below have to go after overrides
@@ -201,6 +214,7 @@ class Toast extends React.Component<ToastPropsT, ToastPrivateStateT> {
               <DeleteIcon
                 role="button"
                 tabIndex={0}
+                $isFocusVisible={this.state.isFocusVisible}
                 onClick={this.dismiss}
                 onKeyPress={event => {
                   if (event.key === 'Enter') {
@@ -210,6 +224,8 @@ class Toast extends React.Component<ToastPropsT, ToastPrivateStateT> {
                 title={locale.toast.close}
                 {...sharedProps}
                 {...closeIconProps}
+                onFocus={forkFocus(closeIconProps, this.handleFocus)}
+                onBlur={forkBlur(closeIconProps, this.handleBlur)}
                 overrides={closeIconOverrides}
               />
             ) : null}

documentation-site/components/anchor.js
@@ -10,9 +10,13 @@ import * as React from 'react';
 
 import {themedStyled} from '../pages/_app';
 
-const Wrapper = themedStyled('a', ({$isVisible}) => ({
+const Wrapper = themedStyled('a', ({$isVisible, $theme}) => ({
   visibility: $isVisible ? 'visible' : 'hidden',
-  color: '#000',
+  color: $theme.colors.primary,
+  ':focus': {
+    outline: `3px solid ${$theme.colors.accent}`,
+    outlineOffset: '1px',
+  },
 }));
 
 const elementToSize = element => {

documentation-site/components/blog.js
@@ -60,6 +60,11 @@ const AuthorLink = themedStyled('a', ({$theme}) => ({
   ':hover': {
     color: $theme.colors.contentPrimary,
   },
+  ':focus': {
+    outline: `3px solid ${$theme.colors.accent}`,
+    textDecoration: 'none',
+    outlineOffset: '2px',
+  },
 }));
 
 const ArticleDate = themedStyled('span', ({$theme}) => ({

documentation-site/components/contributors.js
@@ -39,6 +39,19 @@ function Contributors(props: {contributors: Contributor[]}) {
             marginRight="scale200"
             key={contributor.login}
             title={contributor.login}
+            overrides={{
+              Block: {
+                style: ({$theme}) => ({
+                  ':focus > div > img': {
+                    boxShadow: `0 0 0 3px ${$theme.colors.accent}`,
+                    outline: 'none',
+                  },
+                  ':focus': {
+                    outline: 'none',
+                  },
+                }),
+              },
+            }}
           >
             <Avatar
               name={contributor.login}
@@ -48,11 +61,12 @@ function Contributors(props: {contributors: Contributor[]}) {
               overrides={{
                 Root: {
                   style: ({$theme}) => ({
-                    ...$theme.borders.border300,
-                    borderColor: $theme.colors.mono100,
+                    margin: $theme.sizing.scale100,
+                    transitionProperty: 'all',
+                    transitionDuration: $theme.animation.timing100,
+                    transitionTimingFunction: $theme.animation.easeInOutCurve,
                     ':hover': {
-                      borderColor: $theme.colors.primary,
-                      cursor: 'pointer',
+                      transform: 'scale(1.2)',
                     },
                   }),
                 },

documentation-site/components/footer.js
@@ -53,6 +53,10 @@ const StyledLink = themedStyled('a', ({$theme}) => ({
   ':first-child': {
     marginLeft: '0',
   },
+  ':focus': {
+    outline: `3px solid ${$theme.colors.accent}`,
+    outlineOffset: '3px',
+  },
   ':hover': {
     color: $theme.colors.primary,
     textDecoration: 'none',

documentation-site/components/header-navigation.js
@@ -29,12 +29,16 @@ import Bulb from './bulb';
 import {StatefulTooltip} from 'baseui/tooltip';
 import {Button, KIND, SIZE} from 'baseui/button';
 
-const Hamburger = themedStyled<{}>('div', ({$theme}) => ({
+const Hamburger = themedStyled<{}>('a', ({$theme}) => ({
   display: 'block',
   userSelect: 'none',
   height: '32px',
-  marginLeft: $theme.sizing.scale600,
+  marginLeft: $theme.sizing.scale400,
   cursor: 'pointer',
+  ':focus': {
+    outline: `3px solid ${$theme.colors.accent}`,
+    outlineOffset: '1px',
+  },
   [$theme.mediaQuery.medium]: {
     display: 'none',
   },
@@ -65,6 +69,7 @@ const Navigation = ({toggleSidebar, toggleTheme, toggleDirection}: PropsT) => {
     <ThemeContext.Consumer>
       {theme => (
         <HeaderNavigation
+          $as={'header'}
           overrides={{
             Root: {
               style: ({$theme}) => ({
@@ -82,12 +87,27 @@ const Navigation = ({toggleSidebar, toggleTheme, toggleDirection}: PropsT) => {
             <Block display="flex" alignItems="center">
               <Link href="/">
                 <Block
-                  as="img"
-                  height="40px"
-                  src={theme.name.startsWith('dark') ? LogoWhite : Logo}
-                  width="101px"
-                  overrides={{Block: {style: {cursor: 'pointer'}}}}
-                />
+                  $as="a"
+                  href="#"
+                  overrides={{
+                    Block: {
+                      style: ({$theme}) => ({
+                        ':focus': {
+                          outline: `3px solid ${$theme.colors.accent}`,
+                          outlineOffset: '5px',
+                        },
+                      }),
+                    },
+                  }}
+                >
+                  <Block
+                    as="img"
+                    height="40px"
+                    src={theme.name.startsWith('dark') ? LogoWhite : Logo}
+                    width="101px"
+                    overrides={{Block: {style: {cursor: 'pointer'}}}}
+                  />
+                </Block>
               </Link>
               <Block marginLeft="scale300">
                 <VersionSelector />
@@ -101,7 +121,7 @@ const Navigation = ({toggleSidebar, toggleTheme, toggleDirection}: PropsT) => {
                       BaseButton: {
                         style: {
                           display: 'none',
-                          '@media screen and (min-width: 820px)': {
+                          '@media screen and (min-width: 928px)': {
                             display: 'inline-block',
                           },
                         },
@@ -147,6 +167,10 @@ const Navigation = ({toggleSidebar, toggleTheme, toggleDirection}: PropsT) => {
                   Block: {
                     style: ({$theme}) => ({
                       display: 'none',
+                      ':focus': {
+                        outline: `3px solid ${$theme.colors.accent}`,
+                        outlineOffset: '5px',
+                      },
                       height: $theme.sizing.scale800,
                       [$theme.mediaQuery.small]: {
                         display: 'block',
@@ -155,8 +179,8 @@ const Navigation = ({toggleSidebar, toggleTheme, toggleDirection}: PropsT) => {
                   },
                 }}
                 href="https://join.slack.com/t/baseui/shared_invite/enQtNDI0NTgwMjU0NDUyLWQ0M2RhZWNiMDAwNDA4MDFiOWQyNmViODNkMzFmZDczYzM4MDliNjU3MmZhYWE5YjZhZmJjZWY0MDIxZjdkYzE"
-                marginLeft="scale400"
-                marginRight="scale400"
+                marginLeft="scale600"
+                marginRight="scale300"
                 $style={{textDecoration: 'none'}}
                 target="_blank"
               >
@@ -168,6 +192,10 @@ const Navigation = ({toggleSidebar, toggleTheme, toggleDirection}: PropsT) => {
                   Block: {
                     style: ({$theme}) => ({
                       display: 'none',
+                      ':focus': {
+                        outline: `3px solid ${$theme.colors.accent}`,
+                        outlineOffset: '5px',
+                      },
                       height: $theme.sizing.scale800,
                       [$theme.mediaQuery.small]: {
                         display: 'block',
@@ -177,22 +205,28 @@ const Navigation = ({toggleSidebar, toggleTheme, toggleDirection}: PropsT) => {
                 }}
                 href="https://github.com/uber/baseweb"
                 marginLeft="scale300"
+                marginRight="scale300"
                 $style={{textDecoration: 'none'}}
                 target="_blank"
               >
                 <GithubLogo size={24} color={theme.colors.contentPrimary} />
               </Block>
               <Block
-                as="span"
+                as="a"
                 font="font300"
-                marginLeft="scale400"
-                marginRight="scale400"
+                marginLeft="scale300"
+                marginRight="scale300"
                 onClick={toggleDirection}
+                href="#"
                 overrides={{
                   Block: {
                     style: ({$theme}) => ({
                       cursor: 'pointer',
                       display: 'none',
+                      ':focus': {
+                        outline: `3px solid ${$theme.colors.accent}`,
+                        outlineOffset: '5px',
+                      },
                       height: $theme.sizing.scale800,
                       [$theme.mediaQuery.small]: {
                         display: 'block',
@@ -219,6 +253,10 @@ const Navigation = ({toggleSidebar, toggleTheme, toggleDirection}: PropsT) => {
                   Block: {
                     style: ({$theme}) => ({
                       height: $theme.sizing.scale800,
+                      ':focus': {
+                        outline: `3px solid ${$theme.colors.accent}`,
+                        outlineOffset: '5px',
+                      },
                       [$theme.mediaQuery.small]: {
                         display: 'block',
                       },
@@ -227,6 +265,7 @@ const Navigation = ({toggleSidebar, toggleTheme, toggleDirection}: PropsT) => {
                 }}
                 $style={{textDecoration: 'none'}}
                 href="#"
+                marginLeft="scale300"
                 onClick={toggleTheme}
               >
                 <StatefulTooltip
@@ -238,8 +277,8 @@ const Navigation = ({toggleSidebar, toggleTheme, toggleDirection}: PropsT) => {
                   </Block>
                 </StatefulTooltip>
               </Block>
-              <Hamburger role="button" onClick={toggleSidebar}>
-                <Menu size={32} color={theme.colors.contentSecondary} />
+              <Hamburger role="button" onClick={toggleSidebar} href="#">
+                <Menu size={32} color={theme.colors.contentPrimary} />
               </Hamburger>
             </Block>
           </NavigationList>

documentation-site/components/layout.js
@@ -22,6 +22,7 @@ import Routes from '../routes';
 import DirectionContext from '../components/direction-context';
 import ComponentSizes from '../../component-sizes.json';
 import Help from './help';
+import SkipToContent from './skip-to-content';
 
 const GH_URL =
   'https://github.com/uber/baseweb/edit/master/documentation-site/pages';
@@ -63,7 +64,7 @@ const TOCWrapper = themedStyled<{}>('div', ({$theme}) => ({
 const SidebarWrapper = themedStyled<{
   $isOpen: boolean,
   $hideSideNavigation: boolean,
-}>('div', ({$theme, $isOpen, $hideSideNavigation}) => ({
+}>('nav', ({$theme, $isOpen, $hideSideNavigation}) => ({
   display: $isOpen ? 'block' : 'none',
   paddingTop: $theme.sizing.scale700,
   marginLeft: $theme.sizing.scale800,
@@ -77,13 +78,14 @@ const SidebarWrapper = themedStyled<{
 const ContentWrapper = themedStyled<{
   $isSidebarOpen: boolean,
   $maxWidth?: string,
-}>('div', ({$theme, $isSidebarOpen, $maxWidth}) => ({
+}>('main', ({$theme, $isSidebarOpen, $maxWidth}) => ({
   position: 'relative',
   boxSizing: 'border-box',
   display: $isSidebarOpen ? 'none' : 'block',
   paddingLeft: $theme.sizing.scale800,
   paddingRight: $theme.sizing.scale800,
   width: '100%',
+  outline: 'none',
   maxWidth: $maxWidth ? $maxWidth : '40em',
   [$theme.mediaQuery.medium]: {
     display: 'block',
@@ -131,6 +133,7 @@ class Layout extends React.Component<PropsT, {sidebarOpen: boolean}> {
       <DirectionContext.Consumer>
         {direction => (
           <React.Fragment>
+            <SkipToContent />
             <HeaderNavigation
               toggleSidebar={() =>
                 this.setState(prevState => ({
@@ -149,6 +152,7 @@ class Layout extends React.Component<PropsT, {sidebarOpen: boolean}> {
               justifyContent="center"
             >
               <SidebarWrapper
+                aria-label="primary"
                 $isOpen={sidebarOpen}
                 $hideSideNavigation={!!this.props.hideSideNavigation}
                 onClick={() =>
@@ -160,6 +164,7 @@ class Layout extends React.Component<PropsT, {sidebarOpen: boolean}> {
               <ContentWrapper
                 id="docSearch-content"
                 role="main"
+                tabIndex="-1"
                 $isSidebarOpen={sidebarOpen}
                 $maxWidth={this.props.maxContentWidth}
               >

documentation-site/components/skip-to-content.js
@@ -1,0 +1,39 @@
+/*
+Copyright (c) 2018-2020 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+
+import * as React from 'react';
+import {StyledLink} from 'baseui/link';
+import {useStyletron} from 'baseui';
+
+const SkipToContent = () => {
+  const [, theme] = useStyletron();
+  return (
+    <StyledLink
+      href="#docSearch-content"
+      $style={{
+        top: '-80px',
+        left: theme.sizing.scale600,
+        position: 'fixed',
+        paddingTop: theme.sizing.scale600,
+        paddingBottom: theme.sizing.scale600,
+        paddingLeft: theme.sizing.scale1000,
+        paddingRight: theme.sizing.scale1000,
+        backgroundColor: theme.colors.backgroundSecondary,
+        ':focus': {
+          top: theme.sizing.scale600,
+          outlineOffset: '-3px',
+          transition: `top ${theme.animation.timing100} ${theme.animation.easeInCurve} 0ms`,
+        },
+      }}
+    >
+      Skip to content
+    </StyledLink>
+  );
+};
+
+export default SkipToContent;

commit d7135f3e981f90dfca8e2da9d67e7da1025bc835

fix(toast): add tabIndex back to Toast body so it can be focused
This was previously fixed in https://github.com/uber/baseweb/commit/34748e94615a962de7a6e83083b3a36d61ae532c
but then subsequently broken in https://github.com/uber/baseweb/commit/1aab1e6372be78f0a5c4b733ba9bde277ba87dfa

src/toast/toast.js
@@ -225,6 +225,7 @@ class Toast extends React.Component<ToastPropsT, ToastPrivateStateT> {
           <Body
             role="alert"
             data-baseweb={this.props['data-baseweb'] || 'toast'}
+            tabIndex={-1}
             {...sharedProps}
             {...bodyProps}
             // the properties below have to go after overrides