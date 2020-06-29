210e30de85c11e8797f4538e6b14056947e3f84c
Yang Jiao <jiaoy14@uber.com>
2020-06-29 11:53:53 -0700

    feat(radio):add unique id

--- src/radio/__tests__/radio.scenario.js
+++ src/radio/__tests__/radio.scenario.js
@@ -10,17 +10,25 @@ import * as React from 'react';
 
 import {StatefulRadioGroup, Radio} from '../index.js';
 
+import {FormControl} from '../../form-control/index.js';
+
 export default function Scenario() {
   return (
-    <StatefulRadioGroup initialState={{value: '2'}} aria-label="choose item">
-      <Radio value="1">First</Radio>
-      <Radio
-        value="2"
-        description="This is a radio description, it gives a little more in-yo-face context about what this is."
+    <FormControl label="Test-label">
+      <StatefulRadioGroup
+        id="test-id"
+        initialState={{value: '2'}}
+        aria-label="choose item"
       >
-        Second
-      </Radio>
-      <Radio value="3">Third</Radio>
-    </StatefulRadioGroup>
+        <Radio value="1">First</Radio>
+        <Radio
+          value="2"
+          description="This is a radio description, it gives a little more in-yo-face context about what this is."
+        >
+          Second
+        </Radio>
+        <Radio value="3">Third</Radio>
+      </StatefulRadioGroup>
+    </FormControl>
   );
 }
