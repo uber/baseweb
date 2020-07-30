20171924e4e49740b676b9fdbe2c94d7f40099af
Graham Murdoch <murdoch@uber.com>
2020-07-30 12:41:56 -0700

    feat: add primitve colors

--- src/tag/constants.js
+++ src/tag/constants.js
@@ -20,4 +20,15 @@ export const KIND = {
   warning: 'warning',
   negative: 'negative',
   custom: 'custom',
+  // Primitive color tags
+  // These are the Base approved tags going forwards
+  // TODO(major): remove semantic tags
+  black: 'black',
+  red: 'red',
+  orange: 'orange',
+  yellow: 'yellow',
+  green: 'green',
+  blue: 'blue',
+  purple: 'purple',
+  brown: 'brown',
 };
