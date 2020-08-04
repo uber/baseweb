7d5693929c99854415c2d73bc4d001a20a9a17d0
Gergely Nemeth <gergelyke@users.noreply.github.com>
2020-08-04 10:41:54 -0700

    feat(button-group): add pill shape support (#3574)

---
+++ src/button-group/__tests__/button-group-pill.scenario.js
@@ -0,0 +1,26 @@
+/*
+Copyright (c) 2018-2020 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+
+import * as React from 'react';
+
+import {Button} from '../../button/index.js';
+import {StatefulButtonGroup, MODE, SHAPE} from '../index.js';
+
+export default function Scenario() {
+  return (
+    <StatefulButtonGroup
+      shape={SHAPE.pill}
+      mode={MODE.radio}
+      initialState={{selected: 0}}
+    >
+      <Button>Some label</Button>
+      <Button>Label</Button>
+      <Button>Some longer label</Button>
+    </StatefulButtonGroup>
+  );
+}
--- src/button-group/index.js
+++ src/button-group/index.js
@@ -11,7 +11,7 @@ export {default as StatefulButtonGroup} from './stateful-button-group.js';
 export {default as StatefulContainer} from './stateful-container.js';
 
 // Constants
-export {SIZE} from '../button/constants.js';
+export {SIZE, SHAPE} from '../button/constants.js';
 export {MODE, STATE_CHANGE_TYPE} from './constants.js';
 
 // Styled elements
