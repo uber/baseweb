b1a91d87c439429731c9def170c1d1e84b0f5afd
Graham Murdoch <murdoch@uber.com>
2020-08-11 22:02:38 -0700

    docs: improvements

--- documentation-site/examples/tag/primitive.js
+++ documentation-site/examples/tag/primitive.js
@@ -1,5 +1,4 @@
 // @flow
-
 import * as React from 'react';
 import {Tag, KIND, VARIANT} from 'baseui/tag';
 
@@ -7,64 +6,32 @@ export default function Scenario() {
   return (
     <React.Fragment>
       {[
-        KIND.black,
-        KIND.blue,
-        KIND.red,
-        KIND.orange,
-        KIND.yellow,
-        KIND.green,
-        KIND.purple,
-        KIND.brown,
+        'black',
+        'white',
+        'blue',
+        'green',
+        'red',
+        'yellow',
+        'orange',
+        'purple',
+        'brown',
       ].map(kind => (
-        <div key={kind}>
-          <div>
-            <Tag kind={kind} closeable={false}>
-              Label
-            </Tag>
-            <Tag
-              kind={kind}
-              onClick={() => alert('click')}
-              closeable={false}
-            >
-              Label
-            </Tag>
-            <Tag
-              kind={kind}
-              variant={VARIANT.solid}
-              closeable={false}
-            >
-              Label
-            </Tag>
-            <Tag kind={kind} disabled closeable={false}>
-              Label
-            </Tag>
-          </div>
-          <div>
-            <Tag kind={kind} onActionClick={() => alert('action')}>
-              Label
-            </Tag>
-            <Tag
-              kind={kind}
-              onClick={() => alert('click')}
-              onActionClick={() => alert('action')}
-            >
-              Label
-            </Tag>
-            <Tag
-              kind={kind}
-              variant={VARIANT.solid}
-              onActionClick={() => alert('action')}
-            >
-              Label
-            </Tag>
-            <Tag
-              kind={kind}
-              disabled
-              onActionClick={() => alert('action')}
-            >
-              Label
-            </Tag>
-          </div>
+        <div>
+          <Tag
+            kind={kind}
+            onClick={() => alert(`click ${kind}`)}
+            onActionClick={() => alert(`action ${kind}`)}
+          >
+            {kind}
+          </Tag>
+          <Tag
+            kind={kind}
+            onClick={() => alert(`click ${kind}`)}
+            onActionClick={() => alert(`action ${kind}`)}
+            variant={VARIANT.solid}
+          >
+            {kind}
+          </Tag>
         </div>
       ))}
     </React.Fragment>
