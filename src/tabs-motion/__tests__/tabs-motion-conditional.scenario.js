d3daf442074a420871b4c0be3b5e5fe0e28c6873
Graham Murdoch <murdoch@uber.com>
2020-08-12 16:18:02 -0700

    add replication scenario

---
+++ src/tabs-motion/__tests__/tabs-motion-conditional.scenario.js
@@ -0,0 +1,42 @@
+/*
+Copyright (c) 2018-2020 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+// @flow
+
+/* eslint-disable jsx-a11y/accessible-emoji */
+
+import * as React from 'react';
+import {Tab, Tabs} from '../index.js';
+import {Button, KIND} from '../../button/index.js';
+
+export default function Scenario() {
+  const [activeKey, setActiveKey] = React.useState(0);
+  const [show, setShow] = React.useState(false);
+  return (
+    <React.Fragment>
+      <button type="button" onClick={() => setShow(true)}>
+        Show Robot Tab
+      </button>
+      <Tabs
+        activeKey={activeKey}
+        onChange={({activeKey}) => setActiveKey(activeKey)}
+      >
+        {show && (
+          <Tab title="Robot">
+            <Button kind={KIND.secondary}>🤖</Button>
+          </Tab>
+        )}
+        <Tab title="Monster">
+          <Button kind={KIND.secondary}>👺</Button>
+        </Tab>
+        <Tab title="Watermelon">
+          <Button kind={KIND.secondary}>🍉</Button>
+        </Tab>
+      </Tabs>
+    </React.Fragment>
+  );
+}
