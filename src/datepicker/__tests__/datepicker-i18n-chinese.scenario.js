ddfdb7ab08d0fc79d89561b53cdba71c20bf9d8e
Chase Starr <chasestarr@gmail.com>
2020-08-18 14:48:55 -0700

    fix(datepicker): use short weekday format for calendar header (#3652)
    * fix(datepicker): use short weekday format for calendar header
    
    * fix(datepicker): separate weekdaymin
    
    * fix(datepicker): improve chinese calendar vrt
    
    * test(vrt): update visual snapshots for b7f048d [skip ci] (#3653)
    
    Co-authored-by: UberOpenSourceBot <UberOpenSourceBot@users.noreply.github.com>
    
    Co-authored-by: UberOpenSourceBot <33560951+UberOpenSourceBot@users.noreply.github.com>
    Co-authored-by: UberOpenSourceBot <UberOpenSourceBot@users.noreply.github.com>

---
+++ src/datepicker/__tests__/datepicker-i18n-chinese.scenario.js
@@ -0,0 +1,21 @@
+/*
+Copyright (c) 2018-2020 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+
+import * as React from 'react';
+
+import {StatefulCalendar} from '../index.js';
+import {zhCN} from 'date-fns/locale/index.js';
+
+export default function Scenario() {
+  return (
+    <StatefulCalendar
+      highlightedDate={new Date('2019-02-14T10:00:00Z')}
+      locale={zhCN}
+    />
+  );
+}
