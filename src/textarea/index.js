3381cf7a4f3561aac8c20a481d0884aa8bf8350c
Saagar Takhi <saagar.takhi@sprinklr.com>
2020-08-20 03:08:24 +0530

    fix(textarea): removes ADJOINED enum

--- src/textarea/index.js
+++ src/textarea/index.js
@@ -10,5 +10,5 @@ export {default as StatefulTextarea} from './stateful-textarea.js';
 export {default as StatefulContainer} from './stateful-container.js';
 // Styled elements
 export {StyledTextareaContainer, StyledTextarea} from './styled-components.js';
-export {ADJOINED, STATE_CHANGE_TYPE, SIZE} from './constants.js';
+export {STATE_CHANGE_TYPE, SIZE} from './constants.js';
 export type * from './types.js';
