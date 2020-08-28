0c9d0ca4231acbd6bc9c776c560326e4371ef2eb
Yang Jiao <jiaoy14@uber.com>
2020-08-27 19:27:31 -0700

    fix typos

--- documentation-site/examples/progress-steps/dotted.tsx
+++ documentation-site/examples/progress-steps/dotted.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import {useStyletron} from 'baseui';
 import {ProgressSteps, Step} from 'baseui/progress-steps';
-import {Button, SHAPE} from 'baseui/button';
+import {Button, SHAPE, ButtonProps} from 'baseui/button';
 import {StatefulSelect, TYPE} from 'baseui/select';
 
-function SpacedButton(props) {
+function SpacedButton(props: ButtonProps) {
   return (
     <Button
       {...props}
