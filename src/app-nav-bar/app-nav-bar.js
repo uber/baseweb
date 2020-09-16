2bd69a5790f0a3dbf52697ad7a1d34fc4a0e52bb
refrigeratorstory <69975329+refrigeratorstory@users.noreply.github.com>
2020-09-16 11:37:39 +0000

    Update app-nav-bar.js
    i have no idea how this happened. reverted back to original.

--- src/app-nav-bar/app-nav-bar.js
+++ src/app-nav-bar/app-nav-bar.js
@@ -64,7 +64,7 @@ export default function AppNavBar(props: AppNavBarPropsT) {
   });
 
   return (
-    <StyledRoot>
+    <StyledRoot data-baseweb="app-nav-bar">
       {/* Mobile Nav Experience */}
       <div
         className={css({
