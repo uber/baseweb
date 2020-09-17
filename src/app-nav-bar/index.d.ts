90fdaba02ac9134ad3c4914071de51dcb3c3ce64
refrigeratorstory <69975329+refrigeratorstory@users.noreply.github.com>
2020-09-17 09:11:18 +0000

    fix(app-nav-bar): fixed typescript definitions to match flow definitions

--- src/app-nav-bar/index.d.ts
+++ src/app-nav-bar/index.d.ts
@@ -4,6 +4,15 @@ type ItemT = any;
 type mapItemToNode = (item: ItemT) => React.ReactNode;
 type mapItemToString = (item: ItemT) => string;
 
+type isNavItemActiveT = (params: {
+  item: MainNavItemT | UserNavItemT,
+}) => boolean;
+
+type onNavItemClickT = (params: {item: MainNavItemT | UserNavItemT, event: Event}) => any;
+type onNavItemAuxClickT = (params: {item: MainNavItemT | UserNavItemT, event: Event}) => any;
+type onNavItemKeyDownT = (params: {item: MainNavItemT | UserNavItemT, event: KeyboardEvent}) => any;
+type onNavItemSelectT = (params: {item: MainNavItemT | UserNavItemT}) => any;
+
 export type MainNavItemT = {
   active?: boolean;
   icon?: React.ComponentType<any>;
@@ -12,6 +21,10 @@ export type MainNavItemT = {
   mapItemToString: mapItemToString;
   nav?: MainNavItemT[];
   navExitIcon?: React.ComponentType<any>;
+  navPosition?: {
+    desktop?: POSITION[keyof POSITION];
+    mobile?: POSITION[keyof POSITION];
+  };
 };
 
 export type UserNavItemT = {
@@ -20,18 +33,31 @@ export type UserNavItemT = {
   item: ItemT;
   mapItemToNode?: mapItemToNode;
   mapItemToString: mapItemToString;
+  nav?: UserNavItemT[];
 };
 
 export interface AppNavBarPropsT {
   appDisplayName?: React.ReactNode;
   // eslint-disable-next-line flowtype/no-weak-type;
   mainNav?: MainNavItemT[];
-  isNavItemActive: (params: {item: MainNavItemT | UserNavItemT}) => boolean;
-  onNavItemSelect: (params: {item: MainNavItemT | UserNavItemT}) => any;
+  isNavItemActive: isNavItemActiveT;
+  onNavItemClick?: onNavItemClickT;
+  onNavItemAuxClick?: onNavItemAuxClickT;
+  onNavItemKeyDown?: onNavItemKeyDownT;
+  onNavItemSelect: onNavItemSelectT;
   userNav?: UserNavItemT[];
   username?: string;
   usernameSubtitle?: React.ReactNode;
   userImgUrl?: string;
+}
+
+export interface SecondaryMenuT {
+  nav?: MainNavItemT[];
+  isNavItemActive?: isNavItemActiveT;
+  onNavItemClick?: onNavItemClickT;
+  onNavItemAuxClick?: onNavItemAuxClickT;
+  onNavItemKeyDown?: onNavItemKeyDownT;
+  onNavItemSelect: onNavItemSelectT;
 }
 
 export interface POSITION {
