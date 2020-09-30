fbd7d84ca2041a241a001ce7f12ed777a3015b96
Chase Starr <starr@uber.com>
2020-09-30 15:17:12 -0700

    fix(input): lint

--- src/input/__tests__/input-disabled-matches-select.scenario.js
+++ src/input/__tests__/input-disabled-matches-select.scenario.js
@@ -1,7 +1,16 @@
+/*
+Copyright (c) 2018-2020 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+// @flow
+
 import * as React from 'react';
 
 import {Input} from '../../input/index.js';
-import {Select, TYPE} from '../../select/index.js';
+import {Select} from '../../select/index.js';
 
 export default function Scenario() {
   return (
