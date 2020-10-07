7060701ff5a887b37b09416e62ce015f1570593c
Chase Starr <starr@uber.com>
2020-10-07 15:58:18 -0700

    chore(test): add snackbar unit-test

---
+++ src/snackbar/__tests__/snackbar-element.test.js
@@ -0,0 +1,32 @@
+/*
+Copyright (c) 2018-2020 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+// @flow
+
+import React from 'react';
+import {render, getByText} from '@testing-library/react';
+
+import {SnackbarElement} from '../index.js';
+
+describe('snackbar-element', () => {
+  it('renders text content', () => {
+    const resizeObserver = window.ResizeObserver;
+    window.ResizeObserver = jest.fn(() => ({
+      observe() {},
+      unobserve() {},
+      disconnect() {},
+    }));
+
+    const {container} = render(
+      <SnackbarElement focus={false} message="message" />,
+    );
+    const message = getByText(container, 'message');
+    expect(message).not.toBeNull();
+
+    window.ResizeObserver = resizeObserver;
+  });
+});
