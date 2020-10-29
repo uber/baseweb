ffa3728e19fb9a56ad5fb26b6e6b2e97c06b09a0
Chase Starr <starr@uber.com>
2020-10-29 15:14:16 -0700

    fix(docs): lint

--- documentation-site/components/code-sandboxer.js
+++ documentation-site/components/code-sandboxer.js
@@ -5,7 +5,9 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 
-import {fetchFiles, finaliseCSB, sendFilesToCSB} from 'codesandboxer';
+// @flow
+
+import {finaliseCSB, sendFilesToCSB} from 'codesandboxer';
 
 import packageJson from '../../package.json';
 import {trackEvent} from '../helpers/ga';
