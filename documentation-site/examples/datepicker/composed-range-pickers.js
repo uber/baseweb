35b4134b847cd91b684a3514f3734dbf829a3469
Chase Starr <chasestarr@gmail.com>
2020-11-20 16:30:19 -0800

    docs(datepicker): composed datetime range pickers render well on mobile (#3946)
    * docs(datepicker): composed datetime range pickers render well on mobile
    
    * test(vrt): update visual snapshots for 1a337cf [skip ci] (#3947)
    
    Co-authored-by: UberOpenSourceBot <UberOpenSourceBot@users.noreply.github.com>
    
    Co-authored-by: UberOpenSourceBot <33560951+UberOpenSourceBot@users.noreply.github.com>
    Co-authored-by: UberOpenSourceBot <UberOpenSourceBot@users.noreply.github.com>

--- documentation-site/examples/datepicker/composed-range-pickers.js
+++ documentation-site/examples/datepicker/composed-range-pickers.js
@@ -16,94 +16,109 @@ export default () => {
   const [dates, setDates] = useState<any>([START_DATE, END_DATE]);
 
   return (
-    <div className={css({display: 'flex', alignItems: 'center'})}>
-      <div
-        className={css({
-          width: '120px',
-          marginRight: theme.sizing.scale300,
-        })}
-      >
-        <FormControl label="Start Date" caption="YYYY/MM/DD">
-          <DatePicker
-            value={dates}
-            onChange={({date}) => setDates(date)}
-            timeSelectStart
-            range
-            mask="9999/99/99"
-            displayValueAtRangeIndex={0}
-            placeholder="Start Date"
-          />
-        </FormControl>
-      </div>
+    <div
+      className={css({
+        [theme.mediaQuery.medium]: {
+          display: 'flex',
+          alignItems: 'center',
+        },
+      })}
+    >
+      <div className={css({display: 'flex'})}>
+        <div
+          className={css({
+            width: '120px',
+            marginRight: theme.sizing.scale300,
+          })}
+        >
+          <FormControl label="Start Date" caption="YYYY/MM/DD">
+            <DatePicker
+              value={dates}
+              onChange={({date}) => setDates(date)}
+              timeSelectStart
+              range
+              mask="9999/99/99"
+              displayValueAtRangeIndex={0}
+              placeholder="Start Date"
+            />
+          </FormControl>
+        </div>
 
-      <div
-        className={css({
-          width: '120px',
-          marginRight: theme.sizing.scale300,
-        })}
-      >
-        <FormControl label="Start Time" caption="HH:MM">
-          <TimePicker
-            value={dates[0]}
-            onChange={time => {
-              if (time) {
-                if (isAfter(time, dates[1])) {
-                  setDates([time, time]);
-                } else {
-                  setDates([time, dates[1]]);
+        <div
+          className={css({
+            width: '120px',
+            marginRight: theme.sizing.scale300,
+          })}
+        >
+          <FormControl label="Start Time" caption="HH:MM">
+            <TimePicker
+              value={dates[0]}
+              onChange={time => {
+                if (time) {
+                  if (isAfter(time, dates[1])) {
+                    setDates([time, time]);
+                  } else {
+                    setDates([time, dates[1]]);
+                  }
                 }
-              }
-            }}
-          />
-        </FormControl>
+              }}
+            />
+          </FormControl>
+        </div>
       </div>
 
       <div
         className={css({
+          display: 'none',
           marginRight: theme.sizing.scale300,
+          [theme.mediaQuery.medium]: {
+            display: 'block',
+          },
         })}
       >
         <ArrowRight size={24} />
       </div>
 
-      <div
-        className={css({
-          width: '120px',
-          marginRight: theme.sizing.scale300,
-        })}
-      >
-        <FormControl label="End Date" caption="yyyy/MM/DD">
-          <DatePicker
-            value={dates}
-            onChange={({date}) => setDates(date)}
-            timeSelectEnd
-            range
-            mask="9999/99/99"
-            displayValueAtRangeIndex={1}
-            placeholder="End Date"
-          />
-        </FormControl>
-      </div>
+      <div className={css({display: 'flex'})}>
+        <div
+          className={css({
+            width: '120px',
+            marginRight: theme.sizing.scale300,
+          })}
+        >
+          <FormControl label="End Date" caption="yyyy/MM/DD">
+            <DatePicker
+              value={dates}
+              onChange={({date}) => setDates(date)}
+              timeSelectEnd
+              range
+              mask="9999/99/99"
+              displayValueAtRangeIndex={1}
+              placeholder="End Date"
+            />
+          </FormControl>
+        </div>
 
-      <div
-        className={css({
-          width: '120px',
-        })}
-      >
-        <FormControl label="End Time" caption="HH:MM">
-          <TimePicker
-            value={dates[1]}
-            onChange={time => {
-              if (time) {
-                if (isBefore(time, dates[0])) {
-                  setDates([time, time]);
-                } else {
-                  setDates([dates[0], time]);
+        <div
+          className={css({
+            width: '120px',
+          })}
+        >
+          <FormControl label="End Time" caption="HH:MM">
+            <TimePicker
+              value={dates[1]}
+              onChange={time => {
+                if (time) {
+                  if (isBefore(time, dates[0])) {
+                    setDates([time, time]);
+                  } else {
+                    setDates([dates[0], time]);
+                  }
                 }
-              }
-            }}
-          />
-        </FormControl>
+              }}
+            />
+          </FormControl>
+        </div>
       </div>
     </div>
   );
