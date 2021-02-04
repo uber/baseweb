7c904eefdfda525c08d794d918ddb040c3e829e2
Chase Starr <starr@uber.com>
2021-02-04 15:39:46 -0800

    chore(e2e): update puppeteer

--- jest.e2e.setup.js
+++ jest.e2e.setup.js
@@ -12,32 +12,3 @@ const mkdirp = require('mkdirp');
 
 jest.setTimeout(20 * 1000);
 jest.retryTimes(10);
-
-global.it = async function(name, func, timeout) {
-  return await test(
-    name,
-    async () => {
-      try {
-        await func();
-      } catch (e) {
-        mkdirp.sync('__artifacts__');
-        try {
-          await page.screenshot({
-            type: 'png',
-            path: `__artifacts__/${name}.png`,
-          });
-        } catch (er) {
-          console.log('There was an issue taking a test failure screenshot.');
-          console.log(er);
-        }
-        if (process.env.CI) {
-          console.log(
-            `\u001B]1338;url="artifact://__artifacts__/${name}.png";alt="Screenshot"\u0007`,
-          );
-        }
-        throw e;
-      }
-    },
-    timeout,
-  );
-};
