b06411e8c91b0dfaf8f37849d44aaacb6d142829
Vojtech Miksu <vojtech@uber.com>
2021-02-06 21:41:44 -0700

    docs: add stories files

---
+++ src/checkbox/__tests__/checkbox.stories.js
@@ -0,0 +1,24 @@
+/*
+Copyright (c) 2018-2020 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+
+import React from 'react';
+import CheckboxIndeterminate from './checkbox-indeterminate.scenario.js';
+import CheckboxPlacement from './checkbox-placement.scenario.js';
+import CheckboxStates from './checkbox-states.scenario.js';
+import CheckboxToggle from './checkbox-toggle.scenario.js';
+import CheckboxToggleRound from './checkbox-toggle-round.scenario.js';
+import CheckboxUnlabeled from './checkbox-unlabeled.scenario.js';
+import CheckboxDefault from './checkbox.scenario.js';
+
+export const Indeterminate = () => <CheckboxIndeterminate />;
+export const Placement = () => <CheckboxPlacement />;
+export const States = () => <CheckboxStates />;
+export const Toggle = () => <CheckboxToggle />;
+export const ToggleRound = () => <CheckboxToggleRound />;
+export const Unlabeled = () => <CheckboxUnlabeled />;
+export const Checkbox = () => <CheckboxDefault />;
