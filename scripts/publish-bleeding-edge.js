f7fc1e3cc9e5ccc0dfd47e7cb95f015920b92d6b
Stephen Cook <stephen@stephencookdev.co.uk>
2021-02-23 10:02:08 +0000

    feature(publish) Publish a 'next' tag on every master release

---
+++ scripts/publish-bleeding-edge.js
@@ -0,0 +1,14 @@
+/*
+Copyright (c) 2018-2020 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+// Sets local package.json to current baseui version.
+
+/* eslint-env node */
+// @flow
+
+const publishToNpm = require('./publish-to-npm.js');
+publishToNpm({tag: 'next'});
