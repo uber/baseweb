d923b679153261615269f2cb1efb3e798b0b7ba5
Stephen Cook <stephen@stephencookdev.co.uk>
2021-02-25 21:14:16 +0000

    fix(publish): create unique semver before attempting next publish (#4094)

--- scripts/publish-bleeding-edge.js
+++ scripts/publish-bleeding-edge.js
@@ -11,4 +11,7 @@ LICENSE file in the root directory of this source tree.
 // @flow
 
 const publishToNpm = require('./publish-to-npm.js');
-publishToNpm({tag: 'next'});
+publishToNpm({
+  tag: 'next',
+  commit: process.env.BUILDKITE_COMMIT,
+});
