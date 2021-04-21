f57482947c5cf827f0d2768bf788d0d452226d02
William Ernest <williame@uber.com>
2021-04-20 17:21:32 -0700

    fix(tooltip): update comment

--- src/tooltip/__tests__/tooltip-complex.scenario.js
+++ src/tooltip/__tests__/tooltip-complex.scenario.js
@@ -31,9 +31,9 @@ export default function Scenario() {
         accessibilityType={'tooltip'}
         content={ComplexContent}
         showArrow
-        focusLock
         /* eslint-disable-next-line jsx-a11y/no-autofocus */
         autoFocus
+        focusLock
       >
         <span
           className={css({
