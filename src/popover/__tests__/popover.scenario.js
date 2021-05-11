1d99412e3bb008b685a59363b7e9827996a61a19
William Ernest <williame@uber.com>
2021-05-11 12:13:16 -0700

    fix(popover): fix types

--- src/popover/__tests__/popover.scenario.js
+++ src/popover/__tests__/popover.scenario.js
@@ -9,12 +9,24 @@ LICENSE file in the root directory of this source tree.
 import * as React from 'react';
 
 import {Button} from '../../button/index.js';
-import {Popover} from '../index.js';
+import {StatefulPopover} from '../index.js';
+import {Block} from '../../block/index.js';
 
 export default function Scenario() {
   return (
-    <Popover accessibilityType={'tooltip'} isOpen content={<div>content</div>}>
-      <Button>Open</Button>
-    </Popover>
+    <div>
+      {new Array(222).fill('').map((_, idx) => (
+        <StatefulPopover
+          key={idx}
+          content={() => <Block padding={'20px'}>🎉</Block>}
+          onMouseEnterDelay={0}
+          onMouseLeaveDelay={0}
+          showArrow={false}
+          triggerType={'hover'}
+        >
+          <Button>👋</Button>
+        </StatefulPopover>
+      ))}
+    </div>
   );
 }
