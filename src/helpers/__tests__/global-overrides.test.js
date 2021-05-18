f66f9f9170c6986240de18af383661ca4e4bd3d4
Vojtech Miksu <vojtech@uber.com>
2021-05-17 17:49:23 -0700

    fix: fixes?

--- src/helpers/__tests__/global-overrides.test.js
+++ src/helpers/__tests__/global-overrides.test.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2018-2020 Uber Technologies, Inc.
+Copyright (c) Uber Technologies, Inc.
 
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
@@ -15,23 +15,26 @@ import {
   render,
 } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect.js';
+import {LightTheme} from '../../themes/index.js';
 import {withOverrides} from '../overrides.js';
 import type {OverrideObjectT} from '../overrides.js';
 import ThemeProvider from '../../styles/theme-provider.js';
 import BaseProvider from '../base-provider.js';
 
 type withOverridesT = {
-  overrides: OverrideObjectT,
+  overrides?: OverrideObjectT,
   forwardedRef?: React.Ref<any>,
+  'data-testid'?: string,
 };
 
 describe('Global Overrides', () => {
   test('passes global overrides through ThemeProvider', () => {
     // Mock behavior of baseui component
     const TestElement = (props: withOverridesT) => {
-      return (
-        <div {...props.overrides.props} style={props.overrides.style}></div>
-      );
+      const overrides = props.overrides
+        ? props.overrides
+        : {props: {}, style: {}};
+      return <div {...overrides.props} style={overrides.style}></div>;
     };
     const TestComponent = withOverrides(TestElement, 'TestComponent');
     const {container} = render(
@@ -48,7 +51,6 @@ describe('Global Overrides', () => {
           },
         }}
       >
-        {/* $FlowFixMe */}
         <TestComponent />
       </ThemeProvider>,
     );
@@ -57,6 +59,39 @@ describe('Global Overrides', () => {
     expect(testComponent).toHaveStyle('color: red');
   });
   test('passes global overrides through BaseProvider', () => {
+    // Mock behavior of baseui component
+    const TestElement = (props: withOverridesT) => {
+      const overrides = props.overrides
+        ? props.overrides
+        : {props: {}, style: {}};
+      return <div {...overrides.props} style={overrides.style}></div>;
+    };
+    const TestComponent = withOverrides<withOverridesT, any>(
+      TestElement,
+      'TestComponent',
+    );
+    const {container} = render(
+      <BaseProvider
+        theme={LightTheme}
+        overrides={{
+          TestComponent: {
+            props: {
+              'data-testid': 'global-overrides',
+            },
+            style: {
+              color: 'red',
+            },
+          },
+        }}
+      >
+        <TestComponent />
+      </BaseProvider>,
+    );
+    const testComponent = getByTestId(container, 'global-overrides');
+    expect(testComponent).toBeTruthy();
+    expect(testComponent).toHaveStyle('color: red');
+  });
+  test('local overrides replaces global overrides', () => {
     // Mock behavior of baseui component
     const TestElement = (props: withOverridesT) => {
       return (
@@ -78,13 +113,21 @@ describe('Global Overrides', () => {
           },
         }}
       >
-        {/* $FlowFixMe */}
-        <TestComponent />
+        <TestComponent
+          overrides={{
+            props: {
+              'data-testid': 'local-overrides',
+            },
+            style: {
+              color: 'green',
+            },
+          }}
+        />
       </BaseProvider>,
     );
-    const testComponent = getByTestId(container, 'global-overrides');
+    const testComponent = getByTestId(container, 'local-overrides');
     expect(testComponent).toBeTruthy();
-    expect(testComponent).toHaveStyle('color: red');
+    expect(testComponent).toHaveStyle('color: green');
   });
   test('passes ref successfully', () => {
     /** Create nested forwardRefs */
