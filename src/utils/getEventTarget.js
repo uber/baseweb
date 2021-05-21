3dfb706fee647ca987a52ad65c908b520196f1c8
gevgeny <e.gluhotorenko@gmail.com>
2021-05-21 16:21:29 +0200

    add getEventTarget util

---
+++ src/utils/getEventTarget.js
@@ -0,0 +1,18 @@
+/*
+Copyright (c) Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// @flow
+
+/**
+ * Given a event, returns its target: object from which the event was dispatched.
+ * This implementation also works for events dispatched from shadow DOM with mode === 'open'.
+ */
+export default function getEventTarget(event: Event): EventTarget {
+  if (event.composedPath) {
+    return event.composedPath()[0];
+  }
+  return event.target;
+}
