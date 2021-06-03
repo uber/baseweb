d56b79d11e07c6b6d44e19e24ef2dc387ab2404a
William Ernest <williame@uber.com>
2021-06-03 09:50:26 -0700

    chore(deploy): update script

--- deploy-versioned-docs.sh
+++ deploy-versioned-docs.sh
@@ -11,9 +11,9 @@ deployment=`cat deployment.txt`
 version=$(echo $BUILDKITE_MESSAGE | cut -d' ' -f 2)
 cname="${version//./-}"
 echo $deployment
-#curl -X POST "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/dns_records" \
-#    -H "Authorization: Bearer $CF_API_KEY" \
-#    -H "Content-Type: application/json" \
-#    --data "{\"type\":\"CNAME\",\"name\":\"$cname.baseweb.design\",\"content\":\"cname.vercel-dns.com\",\"ttl\":1,\"priority\":10,\"proxied\":false}"
-#now --scope=uber-ui-platform --token=$ZEIT_NOW_TOKEN alias $deployment "$cname.baseweb.design"
+curl -X POST "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/dns_records" \
+    -H "Authorization: Bearer $CF_API_KEY" \
+    -H "Content-Type: application/json" \
+    --data "{\"type\":\"CNAME\",\"name\":\"$cname.baseweb.design\",\"content\":\"cname.vercel-dns.com\",\"ttl\":1,\"priority\":10,\"proxied\":false}"
+now --scope=uber-ui-platform --token=$ZEIT_NOW_TOKEN alias $deployment "$cname.baseweb.design"
 
