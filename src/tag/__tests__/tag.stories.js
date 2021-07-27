f421a6515e37872d59ac1e484a5fb7143db57460
Luke Brennan <luke.brennan@uber.com>
2021-07-27 12:47:08 -0700

    fix(tag): respect delete icon override from theme

--- src/tag/__tests__/tag.stories.js
+++ src/tag/__tests__/tag.stories.js
@@ -8,11 +8,13 @@ LICENSE file in the root directory of this source tree.
 
 import React from 'react';
 import TagLongText from './tag-long-text.scenario.js';
+import TagOverrides from './tag-overrides.scenario.js';
 import TagSize from './tag-size.scenario.js';
 import TagDefault from './tag.scenario.js';
 import TagStartEnhancer from './tag-start-enhancer.scenario.js';
 
 export const LongText = () => <TagLongText />;
+export const Overrides = () => <TagOverrides />;
 export const Size = () => <TagSize />;
 export const Tag = () => <TagDefault />;
 export const StartEnhancer = () => <TagStartEnhancer />;
