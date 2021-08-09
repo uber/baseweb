1f787464887c717af7bcda0d7308c2a2393a1a6f
William Ernest <williame@uber.com>
2021-08-09 11:13:12 -0700

    docs(button-group): clarify click handler in docs

--- documentation-site/components/yard/config/button.ts
+++ documentation-site/components/yard/config/button.ts
@@ -1,3 +1,9 @@
+/*
+Copyright (c) Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
 import {Button, KIND, SIZE, SHAPE} from 'baseui/button';
 import {PropTypes} from 'react-view';
 import {TConfig} from '../types';
