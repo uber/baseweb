db5cd149fc123ef4581dbc15cac804aa11e5cc9a
Luke Brennan <luke.brennan@uber.com>
2021-09-22 15:02:04 -0700

    docs(list): add react-view for ListHeading

--- src/list/list-heading.js
+++ src/list/list-heading.js
@@ -7,7 +7,7 @@ LICENSE file in the root directory of this source tree.
 // @flow
 
 import React from 'react';
-import ReactIs from 'react-is';
+import * as ReactIs from 'react-is';
 
 import {getOverrides} from '../helpers/overrides.js';
 
