bb29f427062a19335defedf87de780e1d425a842
Dylan Babbs <babbs@uber.com>
2021-10-12 12:28:55 -0400

    chore(map-marker): add example of anchor position in blog

---
+++ documentation-site/pages/blog/introducing-base-map-markers/anchor-position-example.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  FloatingMarker,
+  FLOATING_MARKER_ANCHOR_POSITIONS,
+} from 'baseui/map-marker';
+import {Label3} from 'baseui/typography';
+import {useStyletron} from 'baseui';
+
+const anchors = [
+  FLOATING_MARKER_ANCHOR_POSITIONS.topLeft,
+  FLOATING_MARKER_ANCHOR_POSITIONS.topRight,
+  FLOATING_MARKER_ANCHOR_POSITIONS.bottomRight,
+  FLOATING_MARKER_ANCHOR_POSITIONS.bottomLeft,
+];
+const AnchorPositionExample = () => {
+  const [css] = useStyletron();
+
+  const [index, setIndex] = React.useState(0);
+  React.useEffect(() => {
+    setInterval(() => {
+      setIndex(t => {
+        if (t >= anchors.length - 1) {
+          return 0;
+        } else {
+          return t + 1;
+        }
+      });
+    }, 3000);
+  }, []);
+
+  return (
+    <div
+      className={css({
+        height: '160px',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'relative',
+      })}
+    >
+      <Label3
+        className={css({
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+        })}
+      >
+        Anchor position: {anchors[index]}
+      </Label3>
+      <FloatingMarker label="Uber HQ" anchor={anchors[index]} />
+    </div>
+  );
+};
+
+export default AnchorPositionExample;
