c4295d653c7b4ad87986ae8c88c825ad969cb7c6
Chase Starr <chasestarr@gmail.com>
2021-11-19 16:34:44 -0800

    feat(bundle): query master branch for size data (#4632)
    * feat(bundle): query master branch for size data
    
    * fix(bundle): debug
    
    * fix(bundle): use new token
    
    * fix(bundle): proxy env var to docker container
    
    * chore(bundle): mock failure case
    
    * fix(bundle): revert failing change

--- scripts/collect-bundle-sizes.js
+++ scripts/collect-bundle-sizes.js
@@ -54,6 +54,66 @@ async function measurePageBytesReceived(browser, url) {
   return bytesReceived;
 }
 
+async function downloadMasterBundleSizeData() {
+  const BK_API_URL = 'https://api.buildkite.com/v2';
+  const BK_BASEWEB_URL = `${BK_API_URL}/organizations/uberopensource/pipelines/baseweb`;
+
+  const buildkiteToken = process.env.BUILDKITE_UBEROPENSOURCE_API_TOKEN;
+  if (!buildkiteToken) {
+    throw new Error('BUILDKITE_UBEROPENSOURCE_API_TOKEN env var not set');
+  }
+
+  function bkfetch(url, options = {}) {
+    return fetch(url, {
+      headers: {Authorization: `Bearer ${buildkiteToken}`},
+      ...options,
+    });
+  }
+
+  const buildsResponse = await bkfetch(
+    `${BK_BASEWEB_URL}/builds?branch=master`,
+  );
+  if (buildsResponse.status !== 200) {
+    throw new Error('failed to list buildkite builds');
+  }
+  const builds = await buildsResponse.json();
+  for (const build of builds) {
+    const artifactsResponse = await bkfetch(
+      `${BK_BASEWEB_URL}/builds/${build.number}/artifacts`,
+    );
+    if (artifactsResponse.status !== 200) {
+      throw new Error('failed to list artifacts');
+    }
+    const artifacts = await artifactsResponse.json();
+
+    // We could be in a situation where there is an in-progress build on master that will not have
+    // the bundle size data available. Below I've implemented it so that we will continue on to
+    // next available master build where bundle size data is available. This has the potential to
+    // have improper comparisons on stale data, but should be quite rare. The in-progress build
+    // and current build will need to have significantly changed the size.
+    for (const artifact of artifacts) {
+      if (artifact.filename === 'bundle-size.json') {
+        // Artifact download_url provided by buildkite is a redirect to an s3 url, and is protected
+        // by bk authentication. By default, node-fetch will follow all redirects and when it goes
+        // to request from the s3 url it fails due to an invalid auth header. So below we will get
+        // the redirected url, then make a second request without auth headers.
+        const downloadRedirectResponse = await bkfetch(artifact.download_url, {
+          redirect: 'manual',
+        });
+        if (downloadRedirectResponse.status !== 302) {
+          throw new Error('failed to request artifact download url');
+        }
+        const redirectUrl = downloadRedirectResponse.headers.get('location');
+        const dataResponse = await fetch(redirectUrl);
+        if (dataResponse.status !== 200) {
+          throw new Error('failed to download artifact data');
+        }
+        return dataResponse.json();
+      }
+    }
+  }
+}
+
 async function main() {
   execSync(
     'yarn ladle build --out build-ladle --stories src/**/*.scenario.js',
@@ -106,6 +166,33 @@ async function main() {
   }
   writeFileSync(bundleSizeJsonPath, JSON.stringify(sizes));
   console.log(`bundle-size data written to ${bundleSizeJsonPath}`);
+
+  const significantDeltas = {};
+  const masterBundleSizes = await downloadMasterBundleSizeData();
+  for (const storyTitle in sizes) {
+    const masterSize = masterBundleSizes[storyTitle];
+    const currentSize = sizes[storyTitle];
+    if (masterBundleSizes[storyTitle]) {
+      const delta = currentSize - masterSize;
+      if (Math.abs(delta) > masterSize / 100) {
+        significantDeltas[storyTitle] = delta;
+      }
+    }
+  }
+
+  if (Object.keys(significantDeltas).length) {
+    console.log(
+      'The following stories had bundle size changes greater than 1%',
+    );
+    for (const storyTitle in significantDeltas) {
+      console.log(
+        `${storyTitle} master: ${masterBundleSizes[storyTitle]}b, current: ${sizes[storyTitle]}b, delta: ${significantDeltas[storyTitle]}b`,
+      );
+    }
+
+    // soft fail bk job
+    process.exit(2);
+  }
 
   process.exit(0);
 }
