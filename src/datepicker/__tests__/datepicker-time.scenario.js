83b03e8d80fcc1f2f018e583c357dff0af7bae4e
William Ernest <williame@uber.com>
2021-12-09 15:09:13 -0800

    test(datepicker): add scenario to verify am/pm shows in datepicker

---
+++ src/datepicker/__tests__/datepicker-time.scenario.js
@@ -0,0 +1,27 @@
+// @flow
+/*
+Copyright (c) Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+import * as React from 'react';
+
+import {Datepicker} from '../index.js';
+
+export function Scenario() {
+  const [value, setValue] = React.useState([
+    new Date('2021-06-02T20:49:53.000Z'),
+    new Date('2021-06-10T06:30:00.000Z'),
+  ]);
+  return (
+    <Datepicker
+      value={value}
+      onChange={({date}) => setValue(Array.isArray(date) ? date : [date])}
+      formatString="yyyy/MM/dd h:mm aa"
+      range
+      timeSelectStart
+      timeSelectEnd
+    />
+  );
+}
