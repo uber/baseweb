0bf1ad5810d65f74d03c28e6897718bef36c459a
William Ernest <williame@uber.com>
2021-12-13 12:05:40 -0800

    fix(eslint): update

--- packages/eslint-plugin-baseui/.eslintrc.js
+++ packages/eslint-plugin-baseui/.eslintrc.js
@@ -5,13 +5,8 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 
-// @flow
-
 module.exports = {
   env: {
     node: true,
-  },
-  rules: {
-    'header/header': [2, '../../LICENSE-HEAD'],
   },
 };
