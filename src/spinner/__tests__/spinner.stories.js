bd7a33f930aa088901fc89f4b17eafed0fcd23cd
Chase Starr <chasestarr@gmail.com>
2022-02-18 13:01:03 -0800

    feat(spinner): deprecate current default and replace with next version (#4793)
    * feat(spinner): promote StyledSpinnerNext to Spinner
    
    * fix(spinner): correct file name
    
    * test(vrt): update visual snapshots for 01500a7 [skip ci] (#4785)
    
    Co-authored-by: UberOpenSourceBot <UberOpenSourceBot@users.noreply.github.com>
    
    Co-authored-by: UberOpenSourceBot <33560951+UberOpenSourceBot@users.noreply.github.com>
    Co-authored-by: UberOpenSourceBot <UberOpenSourceBot@users.noreply.github.com>

--- src/spinner/__tests__/spinner.stories.js
+++ src/spinner/__tests__/spinner.stories.js
@@ -7,10 +7,6 @@ LICENSE file in the root directory of this source tree.
 // @flow
 
 import React from 'react';
-import {Scenario as SpinnerNegative} from './spinner-negative.scenario.js';
-import {Scenario as SpinnerNext} from './spinner-next.scenario.js';
 import {Scenario as SpinnerDefault} from './spinner.scenario.js';
 
-export const Negative = () => <SpinnerNegative />;
-export const Next = () => <SpinnerNext />;
 export const Spinner = () => <SpinnerDefault />;
