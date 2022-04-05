dce856d1baf64cd8c97f49d54cee7a90caab5904
Luke Brennan <luke.brennan@uber.com>
2022-04-05 11:58:21 -0700

    feat(accordion): support arrow keys, home key, end key (#4884)
    * feat(accordion): support arrow keys, home key, end key
    
    Arrow Up and Arrow Down shifts focus to previous/next panel header.
    Home key shifts focus to the first/top panel header.
    End key shifts focus to the last/bottom panel header.
    These keys do not change the expanded state of the panel.
    
    * chore(accordion): move itemRefs out of state
    
    * chore(accordion): add e2e tests
    
    * chore(accordion): update docs
    
    * feat(accordion): get nav keys working for stateless accordion
    
    * chore(accordion): add e2e tests for stateless accordion
    
    * chore(accordion): prevent default on nav keys
    
    * chore(accordion): remove nav functionality from stateless accordion

--- src/accordion/accordion.js
+++ src/accordion/accordion.js
@@ -31,6 +31,8 @@ export default class Accordion extends React.Component<AccordionPropsT, Accordio
     ...this.props.initialState,
   };
 
+  itemRefs = [];
+
   //flowlint-next-line unclear-type:off
   onPanelChange(key: React.Key, onChange: () => {}, ...args: Array<any>) {
     let activeKeys = this.state.expanded;
@@ -61,12 +63,56 @@ export default class Accordion extends React.Component<AccordionPropsT, Accordio
     typeof onChange === 'function' && onChange(newState);
   }
 
+  handleKeyDown(e: KeyboardEvent) {
+    if (this.props.disabled) {
+      return;
+    }
+
+    const itemRefs = this.itemRefs;
+
+    const HOME = 36;
+    const END = 35;
+    const ARROW_UP = 38;
+    const ARROW_DOWN = 40;
+
+    if (e.keyCode === HOME) {
+      e.preventDefault();
+      const firstItem = itemRefs[0];
+      firstItem.current && firstItem.current.focus();
+    }
+    if (e.keyCode === END) {
+      e.preventDefault();
+      const lastItem = itemRefs[itemRefs.length - 1];
+      lastItem.current && lastItem.current.focus();
+    }
+    if (e.keyCode === ARROW_UP) {
+      e.preventDefault();
+      const activeItemIdx = itemRefs.findIndex((item) => item.current === document.activeElement);
+      if (activeItemIdx > 0) {
+        const prevItem = itemRefs[activeItemIdx - 1];
+        prevItem.current && prevItem.current.focus();
+      }
+    }
+    if (e.keyCode === ARROW_DOWN) {
+      e.preventDefault();
+      const activeItemIdx = itemRefs.findIndex((item) => item.current === document.activeElement);
+      if (activeItemIdx < itemRefs.length - 1) {
+        const nextItem = itemRefs[activeItemIdx + 1];
+        nextItem.current && nextItem.current.focus();
+      }
+    }
+  }
+
   getItems() {
     const { expanded } = this.state;
     const { accordion, disabled, children, renderPanelContent, renderAll, overrides } = this.props;
     // flowlint-next-line unclear-type:off
     return React.Children.map(children, (child: any, index) => {
       if (!child) return;
+
+      const itemRef = React.createRef<HTMLDivElement>();
+      this.itemRefs.push(itemRef);
+
       // If there is no key provided use the panel order as a default key
       const key = child.key || String(index);
       let isExpanded = false;
@@ -78,6 +124,7 @@ export default class Accordion extends React.Component<AccordionPropsT, Accordio
 
       const props = {
         key,
+        ref: itemRef,
         expanded: isExpanded || child.props.expanded,
         accordion,
         renderPanelContent,
@@ -108,6 +155,7 @@ export default class Accordion extends React.Component<AccordionPropsT, Accordio
         data-baseweb="accordion"
         $disabled={this.props.disabled}
         $isFocusVisible={false}
+        onKeyDown={this.handleKeyDown.bind(this)}
         {...rootProps}
       >
         {this.getItems()}
