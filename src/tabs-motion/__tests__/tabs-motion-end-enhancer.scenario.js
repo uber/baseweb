66a0384fe29ff788589e22a6739accdcea285d75
Luke Brennan <luke.brennan@uber.com>
2022-06-29 14:49:38 -0700

    chore(tabs-motion): expand end enhancer scenario

--- src/tabs-motion/__tests__/tabs-motion-end-enhancer.scenario.js
+++ src/tabs-motion/__tests__/tabs-motion-end-enhancer.scenario.js
@@ -10,18 +10,22 @@ LICENSE file in the root directory of this source tree.
 /* eslint-disable jsx-a11y/accessible-emoji */
 
 import * as React from 'react';
-import { Tab, Tabs, ORIENTATION } from '../index.js';
+import { Tab, Tabs, ORIENTATION, FILL } from '../index.js';
 import { Button, KIND, SIZE, SHAPE } from '../../button/index.js';
 import Menu from '../../icon/menu.js';
 
 export function Scenario() {
-  const [activeKey, setActiveKey] = React.useState(0);
+  const [activeKey1, setActiveKey1] = React.useState(0);
+  const [activeKey5, setActiveKey5] = React.useState(0);
+  const [activeKey2, setActiveKey2] = React.useState(0);
+  const [activeKey3, setActiveKey3] = React.useState(0);
+  const [activeKey4, setActiveKey4] = React.useState(0);
   return (
-    <div style={{ maxWidth: '600px' }}>
+    <>
       <div style={{ marginBottom: '100px' }}>
         <Tabs
-          activeKey={activeKey}
-          onChange={({ activeKey }) => setActiveKey(activeKey)}
+          activeKey={activeKey1}
+          onChange={({ activeKey }) => setActiveKey1(activeKey)}
           endEnhancer={() => <Menu size={24} />}
         >
           <Tab title="Robot">
@@ -53,8 +57,42 @@ export function Scenario() {
 
       <div style={{ marginBottom: '100px' }}>
         <Tabs
-          activeKey={activeKey}
-          onChange={({ activeKey }) => setActiveKey(activeKey)}
+          activeKey={activeKey5}
+          onChange={({ activeKey }) => setActiveKey5(activeKey)}
+          endEnhancer={() => <Menu size={24} />}
+          fill={FILL.fixed}
+        >
+          <Tab title="Robot">
+            <Button kind={KIND.secondary}>🤖</Button>
+          </Tab>
+          <Tab title="Monster">
+            <Button kind={KIND.secondary}>👺</Button>
+          </Tab>
+          <Tab title="Watermelon">
+            <Button kind={KIND.secondary}>🍉</Button>
+          </Tab>
+          <Tab title="Heart">
+            <Button kind={KIND.secondary}>❤️</Button>
+          </Tab>
+          <Tab title="Flame">
+            <Button kind={KIND.secondary}>🔥</Button>
+          </Tab>
+          <Tab title="Chocolate">
+            <Button kind={KIND.secondary}>🍫</Button>
+          </Tab>
+          <Tab title="Butterfly">
+            <Button kind={KIND.secondary}>🦋</Button>
+          </Tab>
+          <Tab title="Volcano">
+            <Button kind={KIND.secondary}>🌋</Button>
+          </Tab>
+        </Tabs>
+      </div>
+
+      <div style={{ maxWidth: '600px', marginBottom: '100px' }}>
+        <Tabs
+          activeKey={activeKey2}
+          onChange={({ activeKey }) => setActiveKey2(activeKey)}
           endEnhancer={
             <Button size={SIZE.compact} shape={SHAPE.pill}>
               Button
@@ -88,10 +126,10 @@ export function Scenario() {
         </Tabs>
       </div>
 
-      <div style={{ marginBottom: '100px' }}>
+      <div style={{ maxWidth: '600px', marginBottom: '100px' }}>
         <Tabs
-          activeKey={activeKey}
-          onChange={({ activeKey }) => setActiveKey(activeKey)}
+          activeKey={activeKey3}
+          onChange={({ activeKey }) => setActiveKey3(activeKey)}
           endEnhancer="String w/ min-width style override"
           overrides={{
             EndEnhancerContainer: {
@@ -130,8 +168,8 @@ export function Scenario() {
 
       <div style={{ marginBottom: '100px' }}>
         <Tabs
-          activeKey={activeKey}
-          onChange={({ activeKey }) => setActiveKey(activeKey)}
+          activeKey={activeKey4}
+          onChange={({ activeKey }) => setActiveKey4(activeKey)}
           endEnhancer={() => <span>Will not appear</span>}
           orientation={ORIENTATION.vertical}
         >
@@ -161,6 +199,6 @@ export function Scenario() {
           </Tab>
         </Tabs>
       </div>
-    </div>
+    </>
   );
 }
