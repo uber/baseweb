0d6737e659ebd727b9d330832d4470617bed05f8
Bogdan Savluk <savluk.bogdan@gmail.com>
2022-07-14 21:11:53 +0200

    fix(types): compatibility fix for input

--- src/payment-card/types.ts
+++ src/payment-card/types.ts
@@ -4,12 +4,12 @@ Copyright (c) Uber Technologies, Inc.
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
-import type { InputComponents, InputProps, StInputPropsDiff } from '../input';
+import type { InputOverrides, InputProps, StInputPropsDiff } from '../input';
 import type { Override } from '../helpers/overrides';
 
 export type PaymentCardOverrides = {
   IconWrapper?: Override;
-} & InputComponents;
+} & InputOverrides;
 
 export type PaymentCardProps = {
   overrides: PaymentCardOverrides;
