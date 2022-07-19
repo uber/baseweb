ae3da01e0ca384794ada1e7529068125beaeef50
tajo <vojtech@miksu.cz>
2022-07-19 15:51:46 -0700

    fix: css sync

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -1,17 +1,44 @@
 import * as React from 'react';
 import { Provider as StyletronProvider } from 'styletron-react';
-import { Client as Styletron } from 'styletron-engine-monolithic';
+import { Client as Styletron } from 'styletron-engine-atomic';
 import { LightTheme, DarkTheme } from '../src/themes/index.js';
 import BaseProvider from '../src/helpers/base-provider.js';
 
-const engine = new Styletron();
+let prevWidth = 0;
+
+// finds the closest document.head (one in iframe or top-level)
+const getDocument = (story: string) => {
+  const iframe = document.querySelector(`[title='Story ${story}']`) as HTMLIFrameElement;
+  return iframe && iframe.contentDocument ? iframe.contentDocument : document;
+};
 
 export const Provider: React.FC<{
   globalState: {
     theme: string;
     rtl: string;
+    story: string;
+    width: number;
   };
 }> = ({ children, globalState }) => {
+  const [engine, setEngine] = React.useState(
+    new Styletron({ container: getDocument(globalState.story).head })
+  );
+
+  React.useEffect(() => {
+    // change engine target container when adding/removing iframe
+    if ((prevWidth > 0 && globalState.width === 0) || (prevWidth === 0 && globalState.width > 0)) {
+      const targetHead = getDocument(globalState.story).head;
+      // clear out previously created styles first to prevent any naming clashes
+      [...targetHead.children].forEach((child) => {
+        if (child.tagName === 'STYLE' && child.innerHTML === '') {
+          child.remove();
+        }
+      });
+      setEngine(new Styletron({ container: getDocument(globalState.story).head }));
+    }
+    prevWidth = globalState.width;
+  }, [globalState.width]);
+
   return (
     <StyletronProvider value={engine}>
       <BaseProvider
