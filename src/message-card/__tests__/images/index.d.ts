4ff32a7bdfd2517eaf5eafd80ac2ce4be81f4af8
Luke Brennan <lhbrennan@gmail.com>
2022-07-29 14:34:27 -0700

    chore(message-card): fix svg import issue

---
+++ src/message-card/__tests__/images/index.d.ts
@@ -0,0 +1,12 @@
+declare module '*.svg' {
+  import React = require('react');
+
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
