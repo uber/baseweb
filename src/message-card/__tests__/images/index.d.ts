a56a6bed451eca1a68428950c0a94451a9b8f496
Luke Brennan <luke.brennan@uber.com>
2022-08-08 11:58:39 -0700

    chore(message-card): add header to index.d.ts file

--- src/message-card/__tests__/images/index.d.ts
+++ src/message-card/__tests__/images/index.d.ts
@@ -1,3 +1,9 @@
+/*
+Copyright (c) Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
 declare module '*.svg' {
   import React = require('react');
 
