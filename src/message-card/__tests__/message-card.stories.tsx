a6a728148d0f123f2636fde61aacb284506372d7
Luke Brennan <luke.brennan@uber.com>
2022-08-11 09:52:07 -0700

    feat(message-card): build message card (#5068)
    * feat(message-card): build message card
    
    * chore(message-card): replace StyledButtonContainer with Button
    
    * chore(message-card): fix svg import issue
    
    * chore(message-card): add flow types
    
    * chore(message-card): cleanup
    
    * chore(message-card): more cleanup
    
    * chore(message-card): change heading, paragraph, buttonLabel to ReactNode
    
    * chore(message-card): add oversized jpg image to trailing scenario
    
    * chore(message-card): convert img to background img
    
    * chore(message-card): add dark mode overlay
    
    * chore(message-card): make call-to-action button always light theme
    
    * chore(message-card): fix rtl and StyledContentContainer overflow
    
    * chore(message-card): add docs
    
    * chore(message-card): add header to index.d.ts file
    
    * chore(color-tokens): add flow types for new color primitives
    
    * chore(message-card): fix change type.js.flow to index.js.flow
    
    * chore(message-card): remove unused imports from docs examples
    
    * chore(docs): double max_old_space_size size
    
    Cloudflare was failing to build due to node running out of memory
    
    * test(vrt): update visual snapshots for 58804cf [skip ci] (#5070)
    
    Co-authored-by: UberOpenSourceBot <UberOpenSourceBot@users.noreply.github.com>
    
    * fix(message-card): change button size to compact
    
    * fix(message-card): button text is sometimes blue on mobile
    
    * test(vrt): update visual snapshots for 5a290bd (#5085)
    
    Co-authored-by: UberOpenSourceBot <UberOpenSourceBot@users.noreply.github.com>
    
    * chore(message-card): use sets for light/dark/contrast color lists
    
    * chore(message-card): add aria-label to image
    
    Co-authored-by: UberOpenSourceBot <33560951+UberOpenSourceBot@users.noreply.github.com>
    Co-authored-by: UberOpenSourceBot <UberOpenSourceBot@users.noreply.github.com>

---
+++ src/message-card/__tests__/message-card.stories.tsx
@@ -0,0 +1,16 @@
+/*
+Copyright (c) Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+import React from 'react';
+import { Scenario as MessageCardBasic } from './message-card.scenario';
+import { Scenario as MessageCardTrailingImage } from './message-card-trailing-image.scenario';
+import { Scenario as MessageCardSizes } from './message-card-sizes.scenario';
+import { Scenario as MessageCardImagePositions } from './message-card-image-positions.scenario';
+
+export const MessageCard = () => <MessageCardBasic />;
+export const TrailingImage = () => <MessageCardTrailingImage />;
+export const Sizes = () => <MessageCardSizes />;
+export const ImagePositions = () => <MessageCardImagePositions />;
