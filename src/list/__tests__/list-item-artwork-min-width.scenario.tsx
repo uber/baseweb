a2f385f3ce1c8b1052b794abfdbb2169d17f081c
Chase <starr@uber.com>
2022-08-25 16:58:53 -0700

    fix(list-item): artwork-container should not shrink width

---
+++ src/list/__tests__/list-item-artwork-min-width.scenario.tsx
@@ -0,0 +1,62 @@
+/*
+Copyright (c) Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+import * as React from 'react';
+
+import Search from '../../icon/search';
+import { ListItem, ListItemLabel, ARTWORK_SIZES } from '..';
+
+export function Scenario() {
+  return (
+    <div style={{ width: '100px', padding: '24px', backgroundColor: 'lightgreen' }}>
+      <ListItem>
+        <ListItemLabel>No Artwork</ListItemLabel>
+      </ListItem>
+
+      <ListItem artwork={Search} artworkSize={ARTWORK_SIZES.SMALL}>
+        <ListItemLabel>Small Artwork</ListItemLabel>
+      </ListItem>
+
+      <ListItem artwork={Search} artworkSize={ARTWORK_SIZES.MEDIUM}>
+        <ListItemLabel>Medium Artwork</ListItemLabel>
+      </ListItem>
+
+      <ListItem artwork={Search} artworkSize={ARTWORK_SIZES.LARGE}>
+        <ListItemLabel>Large Artwork</ListItemLabel>
+      </ListItem>
+
+      <ListItem
+        artwork={() => (
+          <div
+            style={{
+              backgroundColor: 'lightskyblue',
+              width: '64px',
+              height: '24px',
+            }}
+          />
+        )}
+        artworkSize={64}
+      >
+        <ListItemLabel>64px Artwork</ListItemLabel>
+      </ListItem>
+
+      <ListItem
+        artwork={() => (
+          <div
+            style={{
+              backgroundColor: 'lightskyblue',
+              width: '96px',
+              height: '24px',
+            }}
+          />
+        )}
+        artworkSize={96}
+      >
+        <ListItemLabel>96px Artwork</ListItemLabel>
+      </ListItem>
+    </div>
+  );
+}
