d0f81c488f2a91ffcba5d592b9b5a3be3193b990
Chase <starr@uber.com>
2022-08-26 09:47:46 -0700

    chore(lint): lint suppress csstype no-unresolved

--- src/textarea/types.ts
+++ src/textarea/types.ts
@@ -5,6 +5,7 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
 import type * as React from 'react';
+// eslint-disable-next-line import/no-unresolved
 import type { Properties } from 'csstype';
 import type { Override } from '../helpers/overrides';
 import type { BaseInputProps, StateReducer, State, ADJOINED, SIZE } from '../input';
