8a19ec248707c9f194d9c23bbfa1ed2bf3d78f7b
Alex Gorbatchev <agorbatchev@squareup.com>
2022-10-03 09:45:50 -0700

    fix(types): fix HeaderNavigation to allow children

--- src/header-navigation/types.ts
+++ src/header-navigation/types.ts
@@ -4,12 +4,13 @@ Copyright (c) Uber Technologies, Inc.
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
+import type { PropsWithChildren } from 'react';
 import type { Override } from '../helpers/overrides';
 
 export type HeaderNavigationOverrides = {
   Root?: Override;
 };
 
-export type HeaderNavigationProps = {
+export type HeaderNavigationProps = PropsWithChildren<{
   overrides: HeaderNavigationOverrides;
-};
+}>;
