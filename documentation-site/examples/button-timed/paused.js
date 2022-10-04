f1fb82909abfa3e53ac93f5128a28044bae25bbe
Luke Brennan <luke.brennan@uber.com>
2022-10-04 16:36:09 -0700

    chore(button-timed): move // @flow to top of file

--- documentation-site/examples/button-timed/paused.js
+++ documentation-site/examples/button-timed/paused.js
@@ -1,10 +1,10 @@
+// @flow
 /*
 Copyright (c) Uber Technologies, Inc.
 
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 */
-// @flow
 import * as React from 'react';
 
 import {ButtonTimed} from 'baseui/button-timed';
