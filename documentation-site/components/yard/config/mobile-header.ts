e9d9218b815eaf77da87246c6c22cba5733cfa04
Luke Brennan <luke.brennan@uber.com>
2022-11-17 13:40:00 -0800

    feat(mobile-header): create component (#5205)
    * feat(mobile-header): implement fixed collapsed version
    
    * chore(mobile-header): add scenario for floating header
    
    * chore(mobile-header): add expanded state and sticky header
    
    * chore(mobile-header): support text content for buttons
    
    * chore(mobile-header): fix grid columns and text overflow
    
    * chore(mobile-header): add index.js.flow
    
    * chore(mobile-header): add docs
    
    * chore(mobile-header): fix positioning for scenarios
    
    * chore(mobile-header): fix docs examples
    
    * chore(mobile-header): rename additionalButtons to actionButtons
    
    * chore(mobile-header): improve docs
    
    * chore(mobile-header): basic a11y tests
    
    * chore(mobile-header): handle RTL
    
    * chore(mobile-header): fix aria-label type for button
    
    * chore(mobile-header): fix a11y issue on overflow divs
    
    * chore(mobile-header): add index.d.ts for png
    
    * chore(mobile-header): fix flow issues
    
    * chore(mobile-header): make fixed example default to expanded
    
    * test(vrt): update visual snapshots for 6209084 [skip ci] (#5206)
    
    Co-authored-by: UberOpenSourceBot <UberOpenSourceBot@users.noreply.github.com>
    
    * chore(mobile-header): change IconButton type
    
    * chore(mobile-header): use renderIcon
    
    * chore(mobile-header): fix navContainer padding issue
    
    * chore(mobile-header): revert accidental rename of nav button
    
    * test(vrt): update visual snapshots for 4d832f8 (#5214)
    
    Co-authored-by: UberOpenSourceBot <UberOpenSourceBot@users.noreply.github.com>
    
    Co-authored-by: UberOpenSourceBot <33560951+UberOpenSourceBot@users.noreply.github.com>
    Co-authored-by: UberOpenSourceBot <UberOpenSourceBot@users.noreply.github.com>
    Co-authored-by: Chase Starr <chasestarr@gmail.com>

---
+++ documentation-site/components/yard/config/mobile-header.ts
@@ -0,0 +1,83 @@
+/*
+Copyright (c) Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+import { MobileHeader, TYPE } from 'baseui/mobile-header';
+import { PropTypes } from 'react-view';
+import type { TConfig } from '../types';
+import { ArrowLeft, Plus, Check } from 'baseui/icon';
+
+const MobileHeaderConfig: TConfig = {
+  componentName: 'MobileHeader',
+  imports: {
+    'baseui/mobile-header': {
+      named: ['MobileHeader'],
+    },
+    'baseui/icon': {
+      named: ['ArrowLeft', 'Plus', 'Check'],
+    },
+  },
+  scope: {
+    MobileHeader,
+    TYPE,
+    ArrowLeft,
+    Plus,
+    Check,
+  },
+  theme: [],
+  props: {
+    type: {
+      value: undefined,
+      defaultValue: undefined,
+      options: TYPE,
+      enumName: 'TYPE',
+      type: PropTypes.Enum,
+      description: 'Determines whether header if fixed or floating',
+      imports: {
+        'baseui/mobile-header': {
+          named: ['TYPE'],
+        },
+      },
+    },
+    title: {
+      value: 'Header title',
+      type: PropTypes.String,
+      description: 'Title to be displayed in MobileHeader. Ignored when using the floating type.',
+    },
+
+    navButton: {
+      value:
+        '{renderIcon: ArrowLeft, onClick: () => alert("nav button click"), label: "Go back to the previous screen"}',
+      type: PropTypes.Object,
+      description:
+        'Determines the content (can be an icon or a string), onClick, and ariaLabel for the nav button',
+    },
+    actionButtons: {
+      value:
+        '[{renderIcon: Plus, onClick: () => alert("plus button click"), label: "Add another entry"}, {renderIcon: Check, onClick: () => alert("check button click"), label: "Approve entries"}]',
+      type: PropTypes.Array,
+      description:
+        'Determines the content (can be an icon or a string), onClick, and ariaLabel for the up to two additional buttons opposite the nav button.',
+    },
+    expanded: {
+      value: false,
+      type: PropTypes.Boolean,
+      description:
+        'Determines whether MobileHeader is expanded. Ignored when using the floating type.',
+      defaultValue: false,
+    },
+    overrides: {
+      value: undefined,
+      type: PropTypes.Custom,
+      description: 'Lets you customize all aspects of the component.',
+      custom: {
+        names: ['Root', 'Title', 'NavContainer', 'HeaderButton', 'ActionButtonsContainer'],
+        sharedProps: {},
+      },
+    },
+  },
+};
+
+export default MobileHeaderConfig;
---
+++ src/mobile-header/types.ts
@@ -0,0 +1,39 @@
+/*
+Copyright (c) Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+// import type * as React from 'react';
+import type { Override } from '../helpers/overrides';
+import type { TYPE } from './constants';
+
+export type Type = keyof typeof TYPE;
+
+export type MobileHeaderOverrides = {
+  Root?: Override;
+  Title?: Override;
+  NavContainer?: Override;
+  HeaderButton?: Override;
+  ActionButtonsContainer?: Override;
+};
+
+export type IconButton = {
+  renderIcon?: React.ComponentType<
+    {
+      size: number;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } & any
+  >;
+  onClick: () => void;
+  label: string;
+};
+
+export type MobileHeaderProps = {
+  title?: string;
+  expanded?: boolean;
+  navButton: IconButton;
+  actionButtons?: [IconButton?, IconButton?];
+  type?: Type;
+  overrides?: MobileHeaderOverrides;
+};
