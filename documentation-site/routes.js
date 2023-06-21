38e25d2bee357d7e44268ab18fe4391d10783d30
Luke Brennan <luke.brennan@uber.com>
2023-06-20 18:22:02 -0700

    chore(docs): add Stepper docs to route.js

--- documentation-site/routes.js
+++ documentation-site/routes.js
@@ -116,6 +116,10 @@ const routes = [
             itemId: '/components/slider',
           },
           {
+            title: 'Stepper',
+            itemId: '/components/stepper',
+          },
+          {
             title: 'Textarea',
             itemId: '/components/textarea',
           },
