e7255b563cef6bac64db1516b2923bff16c24714
tajo <vojtech@miksu.cz>
2023-10-02 19:27:13 -0700

    Update Ladle to v3.2

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -1,44 +1,13 @@
 import * as React from 'react';
 import { Provider as StyletronProvider } from 'styletron-react';
+import type { GlobalProvider } from '@ladle/react';
 import { Client as Styletron } from 'styletron-engine-atomic';
 import { LightTheme, DarkTheme } from '../src/themes/index.js';
 import BaseProvider from '../src/helpers/base-provider.js';
 
-let prevWidth = 0;
+const engine = new Styletron();
 
-// finds the closest document.head (one in iframe or top-level)
-const getDocument = (story: string) => {
-  const iframe = document.querySelector(`[title='Story ${story}']`) as HTMLIFrameElement;
-  return iframe && iframe.contentDocument ? iframe.contentDocument : document;
-};
-
-export const Provider: React.FC<{
-  globalState: {
-    theme: string;
-    rtl: string;
-    story: string;
-    width: number;
-  };
-}> = ({ children, globalState }) => {
-  const [engine, setEngine] = React.useState(
-    () => new Styletron({ container: getDocument(globalState.story).head })
-  );
-
-  React.useEffect(() => {
-    // change engine target container when adding/removing iframe
-    if ((prevWidth > 0 && globalState.width === 0) || (prevWidth === 0 && globalState.width > 0)) {
-      const targetHead = getDocument(globalState.story).head;
-      // clear out previously created styles first to prevent any naming clashes
-      [...targetHead.children].forEach((child) => {
-        if (child.tagName === 'STYLE' && child.innerHTML === '') {
-          child.remove();
-        }
-      });
-      setEngine(new Styletron({ container: getDocument(globalState.story).head }));
-    }
-    prevWidth = globalState.width;
-  }, [globalState.width]);
-
+export const Provider: GlobalProvider = ({ children, globalState }) => {
   return (
     <StyletronProvider value={engine}>
       <BaseProvider
---
+++ .ladle/config.mjs
@@ -0,0 +1,7 @@
+export default {
+  addons: {
+    a11y: {
+      enabled: true,
+    },
+  },
+};
