a3d548dff245f9d89cc03fe332b310b804db13e9
Emerson Pfeiffer <epfeiffe@umich.edu>
2024-06-28 10:09:13 -0700

    File uploader beta upload restrictions example

--- documentation-site/examples/file-uploader-beta/upload-restrictions.tsx
+++ documentation-site/examples/file-uploader-beta/upload-restrictions.tsx
@@ -2,13 +2,7 @@ import * as React from "react";
 import { FileUploaderBeta, type FileRow } from "baseui/file-uploader-beta";
 
 export default function Example() {
-  const [fileRows, setFileRows] = React.useState<Array<FileRow>>([
-    {
-      file: new File(["test file"], "file.txt"),
-      status: "processed",
-      errorMessage: null,
-    },
-  ]);
+  const [fileRows, setFileRows] = React.useState<Array<FileRow>>([]);
 
   const processFileOnDrop = (
     file: File,
@@ -21,16 +15,23 @@ export default function Example() {
           file,
           objectID: "1234",
           uploadID: "1234",
-          uploadStatus: "success",
+          uploadStatus: "error",
         };
-        resolve({ errorMessage: null, fileInfo });
+        resolve({ errorMessage: "Custom user error", fileInfo });
       }, 2000);
     });
   };
 
   return (
     <FileUploaderBeta
+      accept={["image/png", "application/pdf"]}
       fileRows={fileRows}
+      hint={
+        'Try uploading files that break these conditions: 1. accept set to ["image/png", "application/pdf"], 2. minSize set to 20000 bytes (20 KB), 3. maxSize set to 100000 bytes (100 KB), 4. maxFiles set to 3'
+      }
+      maxFiles={3}
+      maxSize={100000}
+      minSize={20000}
       processFileOnDrop={processFileOnDrop}
       setFileRows={setFileRows}
     />
