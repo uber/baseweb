3001671e7d8f41772dbaea5393e65cf0b7436ccf
Luke Brennan <luke.brennan@uber.com>
2024-11-27 12:08:40 -0800

    chore(docs): add docs for dialog component (#5351)
    * chore(docs): add docs for dialog component
    
    * update docs for diloag component

---
+++ documentation-site/examples/dialog/without-overlay.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import { Dialog, SIZE } from "baseui/dialog";
+import { Button, KIND } from "baseui/button";
+
+export default function Example() {
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <div>
+      <div>
+        <Button
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          Open
+        </Button>
+      </div>
+      <Dialog
+        heading="This is the heading"
+        numHeadingLines={1}
+        size={SIZE.medium}
+        hasOverlay={false}
+        isOpen={isOpen}
+        onDismiss={() => setIsOpen(false)}
+        buttonDock={{
+          primaryAction: <Button>Primary Action</Button>,
+          dismissiveAction: <Button kind={KIND.tertiary}>Dismiss</Button>,
+          secondaryActions: [
+            <Button kind={KIND.secondary} key="first">
+              Secondary Action
+            </Button>,
+          ],
+        }}
+      >
+        <p>
+          At vero eos et accusamus et iusto odio dignissimos ducimus qui
+          blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
+          et quas molestias excepturi sint occaecati cupiditate non provident,
+          similique sunt in culpa qui officia deserunt mollitia animi, id est
+          laborum et dolorum fuga.
+        </p>
+      </Dialog>
+    </div>
+  );
+}
