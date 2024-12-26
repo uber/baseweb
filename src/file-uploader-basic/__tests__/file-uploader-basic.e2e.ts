fa775c932f715d38b3692e2539fbdf4d7424fdce
Emerson Pfeiffer <epfeiffe@umich.edu>
2024-12-26 18:00:39 -0500

    Move file uploader basic stories to proper folder

--- src/file-uploader-basic/__tests__/file-uploader-basic.e2e.ts
+++ src/file-uploader-basic/__tests__/file-uploader-basic.e2e.ts
@@ -8,16 +8,16 @@ LICENSE file in the root directory of this source tree.
 import { expect, test } from '@playwright/test';
 import { mount, analyzeAccessibility } from '../../test/integration';
 
-test.describe('file-uploader', () => {
+test.describe('file-uploader-basic', () => {
   test('pre-drop passes basic a11y tests', async ({ page }) => {
-    await mount(page, 'file-uploader--pre-drop');
+    await mount(page, 'file-uploader-basic--pre-drop');
     const accessibilityReport = await analyzeAccessibility(page);
 
     expect(accessibilityReport).toHaveNoAccessibilityIssues();
   });
 
   test('post-drop passes basic a11y tests', async ({ page }) => {
-    await mount(page, 'file-uploader--post-drop');
+    await mount(page, 'file-uploader-basic--post-drop');
     const accessibilityReport = await analyzeAccessibility(page);
 
     expect(accessibilityReport).toHaveNoAccessibilityIssues();
